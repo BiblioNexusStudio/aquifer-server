20d51e9d7330d0d3c8caea2a201c21afe7d1b3ad
Jesse Winston <jesse.winston@biblionexus.org>
2023-08-10 16:04:57 -0400

    Migration files for BnVerse -> VerseId

---
+++ src/Aquifer.API/Data/Migrations/20230810200359_ChangeBnVerseToVerseId.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeBnVerseToVerseId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartBnVerse",
+                table: "Passages",
+                newName: "StartVerseId");
+
+            migrationBuilder.RenameColumn(
+                name: "EndBnVerse",
+                table: "Passages",
+                newName: "EndVerseId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StartVerseId",
+                table: "Passages",
+                newName: "StartBnVerse");
+
+            migrationBuilder.RenameColumn(
+                name: "EndVerseId",
+                table: "Passages",
+                newName: "EndBnVerse");
+        }
+    }
+}
