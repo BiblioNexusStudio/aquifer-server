10a31c599b2375d2a2e0d884cecd29e74a8a7bb0
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-08-15 13:49:10 -0600

    remove bad unique index
    Because we used to have a HasOne relationship from ResourceContentEntity
    to ResourceEntity, the migrations inferred that there should be a unique
    index. Since that was updated, re-generating the migrations removes the
    unique index. This allows us to link one Resource to multiple
    ResourceContents.

---
+++ src/Aquifer.API/Data/Migrations/20230815194851_RemoveBadUniqueIndex.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveBadUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResourceContents_ResourceId",
+                table: "ResourceContents");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceContents_ResourceId",
+                table: "ResourceContents",
+                column: "ResourceId",
+                unique: true);
+        }
+    }
+}
