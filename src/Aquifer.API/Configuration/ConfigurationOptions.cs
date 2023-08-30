4b281348050220a65a358abec65af89b56b6c3a7
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2023-08-30 17:31:06 -0400

    Scaffolding for auth (#54)
    - Add auth for service collection. Currently commented out since we
    don't have any endpoints that need it yet.
    - Note that this setup is specific to Auth0 or similar service as an
    authority. The JWT is validated by the authority, not by the
    application. If the JWT needed to be verified by us instead, we would
    need a slightly different setup.
    - Since this adds new named settings, added logic for typed settings.
    Can be expanded further as needed, and individual Modules can establish
    their own options.
    - Renamed `BiblioNexus` connection string to `BiblioNexusDb` for added
    clarity (made the change in this PR since I was already adding the typed
    config). I've added the new value in Azure, so after deployments can
    delete the old one.
    - Added an example appsettings file and instructions in the readme.

---
+++ src/Aquifer.API/Configuration/ConfigurationOptions.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Aquifer.API.Configuration;
+
+public class ConfigurationOptions
+{
+    public required ConnectionStringOptions ConnectionStrings { get; init; }
+    public JwtSettingOptions? JwtSettings { get; init; }
+}
+
+public class ConnectionStringOptions
+{
+    public required string BiblioNexusDb { get; init; }
+}
+
+public class JwtSettingOptions
+{
+    // Auth isn't needed right now, make required as needed
+    [Url]
+    public required string Authority { get; init; }
+
+    public required string Audience { get; init; }
+}
--- src/Aquifer.API/Program.cs
+++ src/Aquifer.API/Program.cs
@@ -1,17 +1,20 @@
+using Aquifer.API.Configuration;
 using Aquifer.API.Data;
 using Aquifer.API.Modules;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-var configuration = builder.Configuration;
+var configuration = builder.Configuration.Get<ConfigurationOptions>()!;
 
 builder.Services
+    //.AddAuth(configuration)
     .AddApplicationInsightsTelemetry()
     .AddDbContext<AquiferDbContext>(options =>
-        options.UseSqlServer(configuration.GetConnectionString("BiblioNexus")))
+        options.UseSqlServer(configuration.ConnectionStrings.BiblioNexusDb))
     .RegisterModules();
 
 var app = builder.Build();
 
+//app.UseAuth();
 app.MapEndpoints();
 app.Run();
