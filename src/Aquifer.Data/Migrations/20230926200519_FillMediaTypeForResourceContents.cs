473b3b9fffdc696d387c8025e938342600e130d0
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-09-26 14:10:15 -0600

    fix deploys and fill out MediaType (#64)

---
+++ src/Aquifer.Data/Migrations/20230926200519_FillMediaTypeForResourceContents.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FillMediaTypeForResourceContents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE ResourceContents SET MediaType = Resources.MediaType FROM Resources WHERE ResourceContents.ResourceId = Resources.Id"
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE ResourceContents SET MediaType = 0"
+            );
+        }
+    }
+}
