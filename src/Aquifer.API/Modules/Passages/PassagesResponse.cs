ea9628b1f0dd4923b86f830aeafa68ed5412347d
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-09-29 13:35:05 -0600

    return BookCode instead of BookId for passages

--- src/Aquifer.API/Modules/Passages/PassagesResponse.cs
+++ src/Aquifer.API/Modules/Passages/PassagesResponse.cs
@@ -1,28 +1,35 @@
 ﻿using Aquifer.Data.Entities;
+using Aquifer.Data.Enums;
 using System.Text.Json.Serialization;
 
 namespace Aquifer.API.Modules.Passages;
 
 public class PassagesBookResponse
 {
-    public int BookId { get; set; }
+    public string BookCode => BookId.ToCode();
     public required IEnumerable<PassagesResponsePassage> Passages { get; set; }
+
+    [JsonIgnore]
+    public BookId BookId { get; set; }
 }
 
 public class PassagesResponsePassage
 {
     public int Id { get; set; }
-    public int BookId => PassageStartDetails.BookId;
+    public string BookCode => BookId.ToCode();
     public int StartChapter => PassageStartDetails.Chapter;
     public int EndChapter => PassageEndDetails.Chapter;
     public int StartVerse => PassageStartDetails.Verse;
     public int EndVerse => PassageEndDetails.Verse;
 
     [JsonIgnore]
-    public (int BookId, int Chapter, int Verse) PassageStartDetails { get; set; }
+    public BookId BookId => PassageStartDetails.BookId;
 
     [JsonIgnore]
-    public (int BookId, int Chapter, int Verse) PassageEndDetails { get; set; }
+    public (BookId BookId, int Chapter, int Verse) PassageStartDetails { get; set; }
+
+    [JsonIgnore]
+    public (BookId BookId, int Chapter, int Verse) PassageEndDetails { get; set; }
 }
 
 public class PassageDetailsResponse : PassagesResponsePassage
--- src/Aquifer.API/Utilities/BibleUtilities.cs
+++ src/Aquifer.API/Utilities/BibleUtilities.cs
@@ -4,22 +4,22 @@ namespace Aquifer.API.Utilities;
 
 public static class BibleUtilities
 {
-    public static (int bookId, int chapter, int verse) TranslateVerseId(int verseId)
+    public static (BookId bookId, int chapter, int verse) TranslateVerseId(int verseId)
     {
         int verse = verseId % 1000;
         int chapter = verseId / 1000 % 1000;
-        int bookId = verseId / 1000000 % 1000;
+        var bookId = (BookId)(verseId / 1000000 % 1000);
 
         return (bookId, chapter, verse);
     }
 
     public static int LowerBoundOfBook(BookId bookId)
     {
-        return ((int) bookId * 1000000) + 1000000000;
+        return ((int)bookId * 1000000) + 1000000000;
     }
 
     public static int UpperBoundOfBook(BookId bookId)
     {
-        return ((int) bookId * 1000000) + 1000999999;
+        return ((int)bookId * 1000000) + 1000999999;
     }
 }
