d91c080e3a3d7a682d4e44480ca8c2d7038232a6
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-10-02 07:42:07 -0600

    change unique index on resource content to match new schema (#67)

---
+++ src/Aquifer.Data/Migrations/20230929215033_ChangeUniqueIndexOnResourceContent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeUniqueIndexOnResourceContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResourceContents_ResourceId_LanguageId",
+                table: "ResourceContents");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceContents_ResourceId_LanguageId_MediaType",
+                table: "ResourceContents",
+                columns: new[] { "ResourceId", "LanguageId", "MediaType" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResourceContents_ResourceId_LanguageId_MediaType",
+                table: "ResourceContents");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceContents_ResourceId_LanguageId",
+                table: "ResourceContents",
+                columns: new[] { "ResourceId", "LanguageId" },
+                unique: true);
+        }
+    }
+}
