db177a64f671293d728cd7bc90a70b32ca6ccff5
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-10-04 09:10:02 -0600

    drop SupportingResources and switch from resource type enum to entity
    - drop SupportingResources
    - rename Resources.Type to TypeId and reference ResourceTypes.Id
    - switch endpoints to do resource lookups using the ResourceTypes table

--- src/Aquifer.Data/Entities/ResourceTypeEntity.cs
+++ src/Aquifer.Data/Entities/ResourceTypeEntity.cs
@@ -1,14 +1,22 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Aquifer.Data.Entities;
 
 public class ResourceTypeEntity
 {
     public int Id { get; set; }
-    public string ShortName { get; set; } = null!;
+    public string ShortName { get; set; }
     public string DisplayName { get; set; } = null!;
+    public ResourceTypeComplexityLevel ComplexityLevel { get; set; }
 
     [SqlDefaultValue("getutcdate()")]
     public DateTime Created { get; set; } = DateTime.UtcNow;
 
     [SqlDefaultValue("getutcdate()")]
     public DateTime Updated { get; set; } = DateTime.UtcNow;
+}
+
+public enum ResourceTypeComplexityLevel
+{
+    None = 0
 }
