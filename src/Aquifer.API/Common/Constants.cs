fd5c184a7f198939a650fe44d93bd617f0032121
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-10-26 12:07:15 -0600

    fallback to English for images/videos if missing in current language (#81)
    Long term we will probably need to make this opt-in, but getting this
    done quickly for now. This updates the resource queries so that we
    fallback to English for images/videos for both the file manager and
    passages page.

--- src/Aquifer.API/Common/Constants.cs
+++ src/Aquifer.API/Common/Constants.cs
@@ -7,4 +7,9 @@ public static class Constants
 {
     // this represents the resource types that we allow to be accessed as the starting point when viewing content
     public static readonly ReadOnlyCollection<string> RootResourceTypes = new List<string> { "CBBTER" }.AsReadOnly();
+
+    // this represents the media types that we default to English for
+    public static readonly ReadOnlyCollection<ResourceContentMediaType> FallbackToEnglishForMediaTypes =
+        new List<ResourceContentMediaType> { ResourceContentMediaType.Image, ResourceContentMediaType.Video }
+            .AsReadOnly();
 }
