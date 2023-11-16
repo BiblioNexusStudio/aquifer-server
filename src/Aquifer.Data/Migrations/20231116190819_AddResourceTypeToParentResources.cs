e8a4731e58fedec398409e034b1d7e918641cb9f
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-11-16 12:10:37 -0700

    add remaining column

---
+++ src/Aquifer.Data/Migrations/20231116190819_AddResourceTypeToParentResources.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResourceTypeToParentResources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ResourceType",
+                table: "ParentResources",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ResourceType",
+                table: "ParentResources");
+        }
+    }
+}
