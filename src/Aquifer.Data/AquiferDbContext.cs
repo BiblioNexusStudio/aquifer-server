1d363d2d53d6d8b8507faeff839bf917f3da713a
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-11-30 08:53:18 -0700

    add users entity/table

--- src/Aquifer.Data/AquiferDbContext.cs
+++ src/Aquifer.Data/AquiferDbContext.cs
@@ -10,16 +10,17 @@ public class AquiferDbContext : DbContext
     {
     }
 
-    public DbSet<BibleEntity> Bibles { get; set; }
     public DbSet<BibleBookContentEntity> BibleBookContents { get; set; }
-    public DbSet<VerseEntity> Verses { get; set; }
-    public DbSet<PassageEntity> Passages { get; set; }
-    public DbSet<ResourceEntity> Resources { get; set; }
-    public DbSet<VerseResourceEntity> VerseResources { get; set; }
-    public DbSet<PassageResourceEntity> PassageResources { get; set; }
+    public DbSet<BibleEntity> Bibles { get; set; }
     public DbSet<LanguageEntity> Languages { get; set; }
-    public DbSet<ResourceContentEntity> ResourceContents { get; set; }
     public DbSet<ParentResourceEntity> ParentResources { get; set; }
+    public DbSet<PassageEntity> Passages { get; set; }
+    public DbSet<PassageResourceEntity> PassageResources { get; set; }
+    public DbSet<ResourceContentEntity> ResourceContents { get; set; }
+    public DbSet<ResourceEntity> Resources { get; set; }
+    public DbSet<UserEntity> Users { get; set; }
+    public DbSet<VerseEntity> Verses { get; set; }
+    public DbSet<VerseResourceEntity> VerseResources { get; set; }
 
     protected override void OnModelCreating(ModelBuilder builder)
     {
