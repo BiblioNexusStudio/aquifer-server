fe7fec1fd7a3ac3034307db3d4b04c2596cf5fe6
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-11-30 09:14:48 -0700

    add users entity/table (#98)

---
+++ src/Aquifer.Data/Entities/UserEntity.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.Data.Entities;
+
+[Index(nameof(ProviderId), IsUnique = true)]
+[Index(nameof(Email), IsUnique = true)]
+public class UserEntity
+{
+    public int Id { get; set; }
+    public string ProviderId { get; set; } = null!;
+    public string Email { get; set; } = null!;
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
+}
