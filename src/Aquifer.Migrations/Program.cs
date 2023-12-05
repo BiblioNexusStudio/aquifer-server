8e3cc5d8d8be7b031d4ff0306e3f56f18777db75
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2023-12-05 07:38:25 -0700

    add separate migrations project that has 10 minute db timeout (#106)
    This is an idea from a conversation we had a few weeks ago when we ran
    into issues with deploying due to the migration not getting application
    config. We discussed that the long term best solution was to have a
    separate migration project that's used for generating/running the
    migrations, so that non-database config values aren't needed at runtime
    for the migrations.
    
    A recent migration that copies lots of data from one table to another is
    currently timing out and by doing this separate project we can kill two
    birds with one stone. The separate migration project is configured with
    a 10 minute DB timeout while still keeping the default DB timeout for
    the regular API project.j

---
+++ src/Aquifer.Migrations/Program.cs
@@ -0,0 +1,19 @@
+using Aquifer.Data;
+using Aquifer.Migrations.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration.Get<ConfigurationOptions>();
+
+builder.Services
+    .AddDbContext<AquiferDbContext>(options =>
+        options.UseSqlServer(configuration?.ConnectionStrings?.BiblioNexusDb,
+            providerOptions =>
+            {
+                providerOptions.EnableRetryOnFailure(3);
+                providerOptions.CommandTimeout(600);
+            }));
+
+var app = builder.Build();
+
+app.Run();
