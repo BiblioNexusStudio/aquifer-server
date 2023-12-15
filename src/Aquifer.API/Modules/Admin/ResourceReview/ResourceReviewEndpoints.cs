e2a06a12a0c0df8b7b3c375c8fb7c0ae63d1f9ed
Elisabeth <shakespearee04@gmail.com>
2023-12-15 13:53:30 -0600

    also check if the ResourceContent status is AquiferizeReviewPending and add to UserAssignedHistory

--- src/Aquifer.API/Modules/Admin/ResourceReview/ResourceReviewEndpoints.cs
+++ src/Aquifer.API/Modules/Admin/ResourceReview/ResourceReviewEndpoints.cs
@@ -4,6 +4,7 @@ using Aquifer.Data.Entities;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Any;
 using System.Security.Claims;
 
 namespace Aquifer.API.Modules.Admin.ResourceReview;
@@ -16,12 +17,12 @@ public static class ResourceReviewEndpoints
         ClaimsPrincipal claimsPrincipal,
         CancellationToken cancellationToken)
     {
-        (var mostRecentResourceContentVersion, string badRequestResponse) =
+        (var mostRecentResourceContentVersion, var resourceContent, string badRequestResponse) =
             await GetResourceContentVersionValidation(contentId,
                 dbContext,
                 cancellationToken);
 
-        if (mostRecentResourceContentVersion is null)
+        if (mostRecentResourceContentVersion is null || resourceContent is null)
         {
             return TypedResults.BadRequest(badRequestResponse);
         }
@@ -33,11 +34,9 @@ public static class ResourceReviewEndpoints
         mostRecentResourceContentVersion.Updated = DateTime.UtcNow;
         mostRecentResourceContentVersion.AssignedUserId = user.Id;
         
-        var resourceContent =
-            await dbContext.ResourceContents.FirstOrDefaultAsync(x => x.Id == contentId, cancellationToken) ??
-            throw new ArgumentNullException();
         resourceContent.Status = ResourceContentStatus.AquiferizeInReview;
 
+        // update version status history
         var resourceContentVersionStatusHistory = new ResourceContentVersionStatusHistoryEntity
         {
             ResourceContentVersionId = mostRecentResourceContentVersion.Id,
@@ -46,29 +45,42 @@ public static class ResourceReviewEndpoints
             Created = DateTime.UtcNow
         };
         dbContext.ResourceContentVersionStatusHistory.Add(resourceContentVersionStatusHistory);
+
+        // update version assigned user history
+        var resourceContentVersionAssignedUserHistory = new ResourceContentVersionAssignedUserHistoryEntity
+            {
+                ResourceContentVersion = mostRecentResourceContentVersion,
+                AssignedUserId = user.Id,
+                ChangedByUserId = user.Id,
+                Created = DateTime.UtcNow
+            };
+        dbContext.ResourceContentVersionAssignedUserHistory.Add(resourceContentVersionAssignedUserHistory);
         await dbContext.SaveChangesAsync(cancellationToken);
 
         // return a 200
         return TypedResults.Ok("Success");
     }
 
-    private static async Task<(ResourceContentVersionEntity?, string)> GetResourceContentVersionValidation(
+    private static async Task<(ResourceContentVersionEntity?, ResourceContentEntity?, string)> GetResourceContentVersionValidation(
         int contentId,
         AquiferDbContext dbContext,
         CancellationToken cancellationToken)
     {
         var resourceContentVersions = await dbContext.ResourceContentVersions
-            .Where(x => x.ResourceContentId == contentId).ToListAsync(cancellationToken);
+            .Where(x => x.ResourceContentId == contentId && x.IsDraft == true).ToListAsync(cancellationToken);
 
-        // First check that a ResourceContentVersion exists with the given contentId and IsDraft = 1 and ResourceContent.Status is AquiferizationReviewPending. If none exists, return a 400.
-        bool hasResourceContentDraft = resourceContentVersions
-                                           .Any(x => x.ResourceContentId == contentId && 
-                                           x.IsDraft && 
-                                           x.ResourceContent.Status == ResourceContentStatus.AquiferizeReviewPending );
-
-        if (!hasResourceContentDraft)
+        // First check that a ResourceContentVersion exists with the given contentId and IsDraft = 1
+        if (resourceContentVersions.Count == 0)
         {
-            return (null, "This resource either does not have a draft that has the status of Aquaferization Review pending");
+            return (null, null, "This resource does not have a draft");
+        }
+
+        var resourceContent =
+            await dbContext.ResourceContents.FirstOrDefaultAsync(x => x.Id == contentId, cancellationToken) ??
+            throw new ArgumentNullException();
+
+        if (resourceContent.Status != ResourceContentStatus.AquiferizeReviewPending) {
+            return (null, null, "This resource is not in AquiferizeReviewPending status");
         }
 
         // Grab the most recent ResourceContentVersion with given contentId and IsDraft = 1
@@ -77,9 +89,8 @@ public static class ResourceReviewEndpoints
 
         if (mostRecentResourceContentVersion is null)
         {
-            return (null, "There is no draft ResourceContentVersion with the given contentId.");
+            return (null, null, "Draft with the given contentId does not exist.");
         }
-
-        return (mostRecentResourceContentVersion, string.Empty);
+        return (mostRecentResourceContentVersion, resourceContent, string.Empty);
     }
 }
