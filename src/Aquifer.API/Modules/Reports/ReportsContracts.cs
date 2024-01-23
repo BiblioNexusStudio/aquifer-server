80eb5b00e12a6c328773865010ed0c3896f50aa6
Elisabeth <shakespearee04@gmail.com>
2024-01-23 14:47:14 -0600

    add newline

--- src/Aquifer.API/Modules/Reports/ReportsContracts.cs
+++ src/Aquifer.API/Modules/Reports/ReportsContracts.cs
@@ -2,6 +2,7 @@ public record MonthlyAquiferiationStartsAndCompletionsResponse(
     List<StatusCountPerMonth> Starts,
     List<StatusCountPerMonth> Completions
 );
+
 public record StatusCountPerMonth
     (
         DateTime Date,
