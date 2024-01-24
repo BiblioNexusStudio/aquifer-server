9ea676ecfb86cf0e3383c7c60b4cd8f03d7c5108
Jesse Winston <jesse.winston@biblionexus.org>
2024-01-24 14:22:04 -0500

    Fix formatting

--- src/Aquifer.API/Utilities/ReportUtilities.cs
+++ src/Aquifer.API/Utilities/ReportUtilities.cs
@@ -5,7 +5,8 @@ public static class ReportUtilities
     public static List<DateTime> GetLastMonths(int goBackAmount)
     {
         var currentDate = DateTime.UtcNow;
-        return Enumerable.Range(0, goBackAmount).Select(i => new DateTime(currentDate.Year, currentDate.Month, 1).AddMonths(-i))
+        return Enumerable.Range(0, goBackAmount)
+            .Select(i => new DateTime(currentDate.Year, currentDate.Month, 1).AddMonths(-i))
             .ToList();
-    }  
+    }
 }
