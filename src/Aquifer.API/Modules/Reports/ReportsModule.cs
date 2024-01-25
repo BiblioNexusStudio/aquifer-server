6526afa92eb66f24f004cd6960b8a4b0b2929bc3
Elisabeth <shakespearee04@gmail.com>
2024-01-25 13:23:32 -0600

    Change paths of endpoints

--- src/Aquifer.API/Modules/Reports/ReportsModule.cs
+++ src/Aquifer.API/Modules/Reports/ReportsModule.cs
@@ -7,14 +7,14 @@ public class ReportsModule : IModule
     public IEndpointRouteBuilder MapEndpoints(IEndpointRouteBuilder endpoints)
     {
         var group = endpoints.MapGroup("reports");
-        group.MapGet("monthly/aquiferization",
+        group.MapGet("aquiferizations/monthly",
                 MonthlyReportsEndpoints.AquiferizationCompleteAndStart)
             .CacheOutput(x => x.Expire(TimeSpan.FromMinutes(5)))
             .RequireAuthorization();
         group.MapGet("resources/item-totals", ResourceItemTotalsEndpoint.HandleAsync)
             .CacheOutput(x => x.Expire(TimeSpan.FromMinutes(5))).RequireAuthorization();
 
-        group.MapGet("monthly/translation",
+        group.MapGet("translations/monthly",
                 MonthlyReportsEndpoints.TranslationCompleteAndStart)
             .CacheOutput(x => x.Expire(TimeSpan.FromMinutes(5)))
             .RequireAuthorization();
