ed5ea12312a29f98f77a954a1e3a2b0edeff6fae
Elisabeth <shakespearee04@gmail.com>
2024-01-31 10:27:29 -0600

    new utility for getting the last $goBackAmount days

--- src/Aquifer.API/Utilities/ReportUtilities.cs
+++ src/Aquifer.API/Utilities/ReportUtilities.cs
@@ -9,4 +9,12 @@ public static class ReportUtilities
             .Select(i => new DateTime(currentDate.Year, currentDate.Month, 1).AddMonths(-i))
             .ToList();
     }
+
+    public static List<DateTime> GetLastDays(int goBackAmount)
+    {
+        var currentDate = DateTime.UtcNow;
+        return Enumerable.Range(0, goBackAmount)
+            .Select(i => new DateTime(currentDate.Year, currentDate.Month, 1).AddDays(-i))
+            .ToList();
+    }
 }
