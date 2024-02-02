37bc53b227fe0dccc571032daea08d5db67b10b1
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-02-02 16:58:12 -0500

    Reporting column names fix (#171)

--- src/Aquifer.API/Modules/Reports/RequestedResources/MostRequestedResources.cs
+++ src/Aquifer.API/Modules/Reports/RequestedResources/MostRequestedResources.cs
@@ -3,14 +3,11 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.API.Modules.Reports.RequestedResources;
 
-/// <summary>
-///     Handles the retrieval of the most requested resources.
-/// </summary>
 public class MostRequestedResources
 {
     private const string MostRequestedResourcesQuery =
         """
-        SELECT TOP 100 PR.DisplayName, R.EnglishLabel, L.EnglishDisplay, COUNT(RCR.ResourceContentId) AS Count
+        SELECT TOP 100 PR.DisplayName AS Resource, R.EnglishLabel AS Label, L.EnglishDisplay AS Language, COUNT(RCR.ResourceContentId) AS Count
         FROM ResourceContentRequests RCR
                  INNER JOIN ResourceContents RC ON RC.Id = RCR.ResourceContentId
                  INNER JOIN Resources R ON R.Id = RC.ResourceId
@@ -21,12 +18,6 @@ public class MostRequestedResources
         ORDER BY Count DESC;
         """;
 
-    /// <summary>
-    ///     Handles the asynchronous operation of retrieving the most requested resources.
-    /// </summary>
-    /// <param name="dbContext">The database context to use for the operation.</param>
-    /// <param name="cancellationToken">A cancellation token that can be used to cancel the operation.</param>
-    /// <returns>A task that represents the asynchronous operation. The task result contains the most requested resources.</returns>
     public static async Task<IResult> HandleAsync(
         AquiferDbContext dbContext,
         CancellationToken cancellationToken)
--- src/Aquifer.API/Modules/Reports/RequestedResources/MostRequestedResourcesContracts.cs
+++ src/Aquifer.API/Modules/Reports/RequestedResources/MostRequestedResourcesContracts.cs
@@ -1,12 +1,9 @@
 namespace Aquifer.API.Modules.Reports.RequestedResources;
 
-/// <summary>
-///     Represents the response for the most requested resources.
-/// </summary>
 public class MostRequestedResourcesResponse
 {
-    public string DisplayName { get; set; } = null!;
+    public string Resource { get; set; } = null!;
     public int Count { get; set; } = 0;
-    public string EnglishDisplay { get; set; } = null!;
-    public string EnglishLabel { get; set; } = null!;
+    public string Language { get; set; } = null!;
+    public string Label { get; set; } = null!;
 }
