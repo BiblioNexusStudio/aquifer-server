b455e20b50e69faf3bb18d269397c6b6c9874725
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-02-05 15:30:56 -0700

    address more feedback

--- src/Aquifer.API/Utilities/BibleUtilities.cs
+++ src/Aquifer.API/Utilities/BibleUtilities.cs
@@ -1,4 +1,5 @@
-﻿using Aquifer.Data.Enums;
+﻿using Aquifer.API.Common;
+using Aquifer.Data.Enums;
 
 namespace Aquifer.API.Utilities;
 
@@ -31,5 +32,24 @@ public static class BibleUtilities
     public static int UpperBoundOfChapter(BookId bookId, int chapter)
     {
         return ((int)bookId * 1000000) + ((chapter + 1) * 1000) - 1 + 1000000000;
+    }
+
+    public static List<(int, int)> VerseRangesForBookAndChapters(string? bookCode, int[]? chapters)
+    {
+        if (bookCode is null)
+        {
+            return [];
+        }
+
+        if (chapters is null || chapters.Length == 0)
+        {
+            var bookId = BookCodes.IdFromCode(bookCode);
+            return [(LowerBoundOfBook(bookId), UpperBoundOfBook(bookId))];
+        }
+        else
+        {
+            var bookId = BookCodes.IdFromCode(bookCode);
+            return chapters.Select(c => (LowerBoundOfChapter(bookId, c), UpperBoundOfChapter(bookId, c))).ToList();
+        }
     }
 }
