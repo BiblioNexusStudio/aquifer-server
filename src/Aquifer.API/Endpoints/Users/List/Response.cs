3b05c60f5b6f5933b756737b7ccafeb43d6850c1
Jesse Winston <jesse.winston@biblionexus.org>
2024-02-08 09:01:06 -0500

    Add to users list if email and verified

--- src/Aquifer.API/Endpoints/Users/List/Response.cs
+++ src/Aquifer.API/Endpoints/Users/List/Response.cs
@@ -6,6 +6,8 @@ public class Response
 {
     public int Id { get; set; }
     public required string Name { get; set; }
+    public required string Email { get; set; }
     public required UserRole Role { get; set; }
     public required string? CompanyName { get; set; }
+    public bool IsEmailVerified { get; set; }
 }
