61950855e352040353ca6be9669c934c779a170b
Jesse Winston <jesse.winston@biblionexus.org>
2024-02-12 13:56:36 -0500

    checking in

---
+++ src/Aquifer.Data/EventHandlers/UpdatedTimestampHandler.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Aquifer.Data.EventHandlers;
+
+public static class UpdatedTimestampHandler
+{
+    public static void SetUpdatedTimestamp(object sender, EntityEntryEventArgs e)
+    {
+        if (e.Entry is { Entity: IHasUpdatedTimestamp entity, State: EntityState.Modified })
+        {
+            entity.Updated = DateTime.UtcNow;
+        }
+    }
+}
+
+public interface IHasUpdatedTimestamp
+{
+    public DateTime Updated { get; set; }
+}
