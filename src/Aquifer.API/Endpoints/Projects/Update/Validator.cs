288a490e6aa7b836ca373ffaa73f9dc59ebf9055
Jesse Winston <jesse.winston@biblionexus.org>
2024-02-19 12:21:27 -0500

    Allow sending single project value in update

--- src/Aquifer.API/Endpoints/Projects/Update/Validator.cs
+++ src/Aquifer.API/Endpoints/Projects/Update/Validator.cs
@@ -9,5 +9,14 @@ public class Validator : Validator<Request>
     {
         RuleFor(x => x.QuotedCost)
             .PrecisionScale(int.MaxValue, 2, true);
+
+        RuleFor(x => x).Must(x =>
+                x.QuotedCost is not null ||
+                x.ProjectManagerUserId is not null ||
+                x.ProjectedDeliveryDate is not null ||
+                x.ProjectedPublishDate is not null ||
+                x.CompanyLeadUserId is not null ||
+                x.EffectiveWordCount is not null)
+            .WithMessage("No values set");
     }
 }
