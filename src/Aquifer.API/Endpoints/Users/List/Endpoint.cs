6856ba7863b3d514a8b5b9b4566b8b3302018d61
Elisabeth <shakespearee04@gmail.com>
2024-02-20 10:40:22 -0600

    forgot companyId being the same for readUserDetails permission

--- src/Aquifer.API/Endpoints/Users/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Users/List/Endpoint.cs
@@ -16,23 +16,25 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService) : En
 
     public override async Task HandleAsync(CancellationToken ct)
     {
+        var self = await userService.GetUserFromJwtAsync(ct);
         if (!userService.HasPermission(PermissionName.ReadAllUsers) || !userService.HasPermission(PermissionName.ReadUserDetails))
         {
             await SendForbiddenAsync(ct);
             return;
         }
 
-        var users = await dbContext.Users.Where(x => userService.HasPermission(PermissionName.ReadUserDetails) ||
-                                                     userService.HasPermission(PermissionName.ReadAllUsers)).OrderBy(x => x.FirstName).Select(user => new Response
-                                                     {
-                                                         Id = user.Id,
-                                                         Name = $"{user.FirstName} {user.LastName}",
-                                                         Role = user.Role,
-                                                         CompanyName = user.Company.Name,
-                                                         Company = new CompanyResponse { Id = user.CompanyId, Name = user.Company.Name },
-                                                         Email = user.Email,
-                                                         IsEmailVerified = user.EmailVerified
-                                                     }).ToListAsync(ct);
+        var users = await dbContext.Users.Where(x =>
+            (userService.HasPermission(PermissionName.ReadUserDetails) && self.CompanyId == x.CompanyId) ||
+            userService.HasPermission(PermissionName.ReadAllUsers)).OrderBy(x => x.FirstName).Select(user => new Response
+        {
+            Id = user.Id,
+            Name = $"{user.FirstName} {user.LastName}",
+            Role = user.Role,
+            CompanyName = user.Company.Name,
+            Company = new CompanyResponse { Id = user.CompanyId, Name = user.Company.Name },
+            Email = user.Email,
+            IsEmailVerified = user.EmailVerified
+        }).ToListAsync(ct);
 
         await SendOkAsync(users, ct);
     }
