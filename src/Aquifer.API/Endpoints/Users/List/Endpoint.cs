a0f9b1c48a94d5bb03b9b182dbcbb24d4b7cabf5
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-02-20 13:06:44 -0500

    sort users and restrict result on roles (#196)

--- src/Aquifer.API/Endpoints/Users/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Users/List/Endpoint.cs
@@ -1,11 +1,12 @@
 ﻿using Aquifer.API.Common;
+using Aquifer.API.Services;
 using Aquifer.Data;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.API.Endpoints.Users.List;
 
-public class Endpoint(AquiferDbContext dbContext) : EndpointWithoutRequest<List<Response>>
+public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<List<Response>>
 {
     public override void Configure()
     {
@@ -15,16 +16,20 @@ public class Endpoint(AquiferDbContext dbContext) : EndpointWithoutRequest<List<
 
     public override async Task HandleAsync(CancellationToken ct)
     {
-        var users = await dbContext.Users.Select(user => new Response
-        {
-            Id = user.Id,
-            Name = $"{user.FirstName} {user.LastName}",
-            Role = user.Role,
-            CompanyName = user.Company.Name,
-            Company = new CompanyResponse { Id = user.CompanyId, Name = user.Company.Name },
-            Email = user.Email,
-            IsEmailVerified = user.EmailVerified
-        }).ToListAsync(ct);
+        var self = await userService.GetUserFromJwtAsync(ct);
+
+        var users = await dbContext.Users.Where(x =>
+            userService.HasPermission(PermissionName.ReadAllUsers) || (userService.HasPermission(PermissionName.ReadUsers) && self.CompanyId == x.CompanyId)
+            ).OrderBy(x => x.FirstName).Select(user => new Response
+            {
+                Id = user.Id,
+                Name = $"{user.FirstName} {user.LastName}",
+                Role = user.Role,
+                CompanyName = user.Company.Name,
+                Company = new CompanyResponse { Id = user.CompanyId, Name = user.Company.Name },
+                Email = user.Email,
+                IsEmailVerified = user.EmailVerified
+            }).ToListAsync(ct);
 
         await SendOkAsync(users, ct);
     }
