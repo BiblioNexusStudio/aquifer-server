d5acb1b6376afb3a920e858e9ae696929fe0a696
Frank Flood <frank.flood@biblionexus.org>
2024-02-22 10:09:51 -0500

    Switched from int to long

---
+++ src/Aquifer.Data/Migrations/20240222150726_UpdateWordIdIntToLong.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWordIdIntToLong : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "WordIdentifier",
+                table: "GreekNewTestamentWords",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "WordIdentifier",
+                table: "BibleVersionWords",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "WordIdentifier",
+                table: "GreekNewTestamentWords",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "WordIdentifier",
+                table: "BibleVersionWords",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+    }
+}
