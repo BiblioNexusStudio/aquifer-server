709524aab64588c4f8a169668348b9cabacacdbc
Carlos Morales <carlos.rey.morales@gmail.com>
2024-03-07 09:54:17 -0500

    Adding ResourceContentVersionSnapshots Table (#221)
    Co-authored-by: Jesse Winston <jesse.winston@biblionexus.org>

---
+++ src/Aquifer.Data/Entities/ResourceContentVersionSnapshotsEntity.cs
@@ -0,0 +1,18 @@
+namespace Aquifer.Data.Entities;
+
+public class ResourceContentVersionSnapshotsEntity
+{
+    public int Id { get; set; }
+    public int ResourceContentVersionId { get; set; }
+    public string DisplayName { get; set; } = null!;
+    public string Content { get; set; } = null!;
+    public int? WordCount { get; set; }
+    public int? UserId { get; set; }
+    public int Status { get; set; }
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public ResourceContentVersionEntity ResourceContentVersion { get; set; } = null!;
+    public UserEntity? User { get; set; }
+}
