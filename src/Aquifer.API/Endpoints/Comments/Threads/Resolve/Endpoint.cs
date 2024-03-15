737c7e49a9d7669ad2d4c9a6b46893936c2a75d3
Jesse Winston <jesse.winston@biblionexus.org>
2024-03-15 15:37:46 -0400

    Resolve thread endpoint

---
+++ src/Aquifer.API/Endpoints/Comments/Threads/Resolve/Endpoint.cs
@@ -0,0 +1,30 @@
+﻿using Aquifer.API.Helpers;
+using Aquifer.API.Services;
+using Aquifer.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Comments.Threads.Resolve;
+
+public class Endpoint(AquiferDbContext dbContext, IUserService userService) : Endpoint<Request>
+{
+    public override void Configure()
+    {
+        Patch("/comments/threads/resolve/{threadId}");
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var thread = await dbContext.CommentThreads.SingleOrDefaultAsync(x => x.Id == req.ThreadId && !x.Resolved, ct);
+        EndpointHelpers.ThrowErrorIfNull<Request>(thread, x => x.ThreadId, "No unresolved thread found for given id.", 404);
+
+        var user = await userService.GetUserFromJwtAsync(ct);
+
+        thread!.Resolved = true;
+        thread.ResolvedByUserId = user.Id;
+
+        await dbContext.SaveChangesAsync(ct);
+
+        await SendNoContentAsync(ct);
+    }
+}
