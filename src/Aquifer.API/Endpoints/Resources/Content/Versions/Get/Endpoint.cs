44c19d6b2e5c4be6797ea4237aac70b92a6fecc3
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-04-03 14:38:22 -0600

    add versions to resource content response and a new versions endpoint to pull the content for one (#246)

---
+++ src/Aquifer.API/Endpoints/Resources/Content/Versions/Get/Endpoint.cs
@@ -0,0 +1,36 @@
+using Aquifer.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Resources.Content.Versions.Get;
+
+public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, Response>
+{
+    public override void Configure()
+    {
+        Get("/resources/content/versions/{VersionId}");
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken ct)
+    {
+        var version = await dbContext.ResourceContentVersions.SingleOrDefaultAsync(rcvs => rcvs.Id == request.VersionId, ct);
+
+        if (version is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        await SendOkAsync(
+            new Response
+            {
+                Id = version.Id,
+                DisplayName = version.DisplayName,
+                WordCount = version.WordCount,
+                Created = version.Created,
+                IsPublished = version.IsPublished,
+                Version = version.Version,
+                ContentValue = version.Content
+            }, ct);
+    }
+}
