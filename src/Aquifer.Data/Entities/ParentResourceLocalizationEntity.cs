220c9c97d079a87075c646d5027b2f3a081144d9
Jesse Winston <jesse.winston@biblionexus.org>
2024-04-10 15:17:05 -0400

    Add ParentResourceLocalizations table

---
+++ src/Aquifer.Data/Entities/ParentResourceLocalizationEntity.cs
@@ -0,0 +1,21 @@
+﻿using Aquifer.Data.EventHandlers;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.Data.Entities;
+
+[PrimaryKey(nameof(ParentResourceId), nameof(LanguageId))]
+public class ParentResourceLocalizationEntity : IHasUpdatedTimestamp
+{
+    public int ParentResourceId { get; set; }
+    public int LanguageId { get; set; }
+    public string DisplayName { get; set; } = null!;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public ParentResourceEntity ParentResource { get; set; } = null!;
+    public LanguageEntity Language { get; set; } = null!;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
+}
---
+++ src/Aquifer.Data/Migrations/20240410191544_AddParentResourceLocalizationsTable.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddParentResourceLocalizationsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ParentResourceLocalizations",
+                columns: table => new
+                {
+                    ParentResourceId = table.Column<int>(type: "int", nullable: false),
+                    LanguageId = table.Column<int>(type: "int", nullable: false),
+                    DisplayName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ParentResourceLocalizations", x => new { x.ParentResourceId, x.LanguageId });
+                    table.ForeignKey(
+                        name: "FK_ParentResourceLocalizations_Languages_LanguageId",
+                        column: x => x.LanguageId,
+                        principalTable: "Languages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ParentResourceLocalizations_ParentResources_ParentResourceId",
+                        column: x => x.ParentResourceId,
+                        principalTable: "ParentResources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ParentResourceLocalizations");
+        }
+    }
+}
