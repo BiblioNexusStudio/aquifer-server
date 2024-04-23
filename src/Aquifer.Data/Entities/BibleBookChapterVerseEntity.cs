8564556dfce5dad87ee7aa0be3c276f8999d744e
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-04-23 14:19:52 -0400

    Add Bible book/chapter/verse tables (#260)

---
+++ src/Aquifer.Data/Entities/BibleBookChapterVerseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Aquifer.Data.Entities;
+
+public class BibleBookChapterVerseEntity
+{
+    public int Id { get; set; }
+    public int BibleBookChapterId { get; set; }
+    public int Number { get; set; }
+    public string Text { get; set; } = null!;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public BibleBookChapterEntity BibleBookChapter { get; set; } = null!;
+}
---
+++ src/Aquifer.Data/Entities/BibleBookEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Aquifer.Data.Enums;
+
+namespace Aquifer.Data.Entities;
+
+public class BibleBookEntity
+{
+    public int Id { get; set; }
+    public int BibleId { get; set; }
+    public BookId Number { get; set; }
+
+    [MaxLength(5)]
+    public string Code { get; set; } = null!;
+
+    [MaxLength(64)]
+    public string LocalizedName { get; set; } = null!;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public BibleEntity Bible { get; set; } = null!;
+    public ICollection<BibleBookChapterEntity> Chapters { get; set; } = [];
+}
