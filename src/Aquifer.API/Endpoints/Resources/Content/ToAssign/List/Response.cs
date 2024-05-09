062d65dd5f3fdf9c595e329c082368c3c9e7b93f
Jesse Winston <jesse.winston@biblionexus.org>
2024-05-09 14:06:53 -0400

    Remove comment

--- src/Aquifer.API/Endpoints/Resources/Content/ToAssign/List/Response.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/ToAssign/List/Response.cs
@@ -6,7 +6,7 @@ public class Response
 {
     public required int Id { get; set; }
     public required string EnglishLabel { get; set; }
-    public required string ParentResourceName { get; set; } // don't think we really need this
+    public required string ParentResourceName { get; set; }
     public required string LanguageEnglishDisplay { get; set; }
     public required int WordCount { get; set; }
     public required string ProjectName { get; set; }
