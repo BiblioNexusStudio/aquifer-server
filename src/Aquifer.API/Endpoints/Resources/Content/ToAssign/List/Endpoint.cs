b3f8f772c93fc5be63a466b93c13e3ad3d78cfca
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-05-09 14:58:50 -0400

    Add to-assign endpoint for managers (#278)

---
+++ src/Aquifer.API/Endpoints/Resources/Content/ToAssign/List/Endpoint.cs
@@ -0,0 +1,40 @@
+﻿using Aquifer.API.Common;
+using Aquifer.API.Services;
+using Aquifer.Data;
+using Aquifer.Data.Entities;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Resources.Content.ToAssign.List;
+
+public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<List<Response>>
+{
+    public override void Configure()
+    {
+        Get("/resources/content/to-assign");
+        Permissions(PermissionName.AssignOverride);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var user = await userService.GetUserFromJwtAsync(ct);
+        var query = $"""
+                     SELECT RCV.ResourceContentId AS Id, R.EnglishLabel, PR.DisplayName AS ParentResourceName,
+                            L.EnglishDisplay AS LanguageEnglishDisplay, RCV.WordCount, P.Name AS ProjectName,
+                            P.ProjectedDeliveryDate AS ProjectProjectedDeliveryDate
+                     FROM ResourceContentVersions AS RCV
+                              INNER JOIN ResourceContents AS RC ON RCV.ResourceContentId = RC.Id
+                              INNER JOIN Resources AS R ON RC.ResourceId = R.Id
+                              INNER JOIN ParentResources AS PR ON R.ParentResourceId = PR.Id
+                              INNER JOIN Languages AS L ON RC.LanguageId = L.Id
+                              INNER JOIN ProjectResourceContents PRC ON PRC.ResourceContentId = RC.Id
+                              INNER JOIN Projects P ON P.Id = PRC.ProjectId
+                     WHERE RCV.IsDraft = 1 AND RCV.AssignedUserId = {user.Id}
+                       AND RC.Status IN ({(int)ResourceContentStatus.New}, {(int)ResourceContentStatus.TranslationNotStarted})
+                     """;
+
+        var resources = await dbContext.Database.SqlQueryRaw<Response>(query).ToListAsync(ct);
+
+        await SendOkAsync(resources, ct);
+    }
+}
---
+++ src/Aquifer.API/Endpoints/Resources/Content/ToAssign/List/Response.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Aquifer.API.Endpoints.Resources.Content.ToAssign.List;
+
+public class Response
+{
+    public required int Id { get; set; }
+    public required string EnglishLabel { get; set; }
+    public required string ParentResourceName { get; set; }
+    public required string LanguageEnglishDisplay { get; set; }
+    public required int WordCount { get; set; }
+    public required string ProjectName { get; set; }
+
+    public int? DaysUntilProjectDeadline =>
+        ProjectProjectedDeliveryDate == null
+            ? null
+            : (ProjectProjectedDeliveryDate.Value.ToDateTime(new TimeOnly(23, 59)) - DateTime.UtcNow).Days;
+
+    [JsonIgnore]
+    public DateOnly? ProjectProjectedDeliveryDate { get; set; }
+}
