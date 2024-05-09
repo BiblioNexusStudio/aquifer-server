5d215e2e2b998dadc1179a187e2e7c82f76c7a97
Jesse Winston <jesse.winston@biblionexus.org>
2024-05-09 14:46:22 -0400

    Use status enum in query

--- src/Aquifer.API/Endpoints/Resources/Content/ToAssign/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/ToAssign/List/Endpoint.cs
@@ -1,28 +1,14 @@
 ﻿using Aquifer.API.Common;
 using Aquifer.API.Services;
 using Aquifer.Data;
+using Aquifer.Data.Entities;
 using FastEndpoints;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.API.Endpoints.Resources.Content.ToAssign.List;
 
 public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<List<Response>>
 {
-    private const string Query = """
-                                 SELECT RCV.ResourceContentId AS Id, R.EnglishLabel, PR.DisplayName AS ParentResourceName,
-                                        L.EnglishDisplay AS LanguageEnglishDisplay, RCV.WordCount, P.Name AS ProjectName,
-                                        P.ProjectedDeliveryDate AS ProjectProjectedDeliveryDate
-                                 FROM ResourceContentVersions AS RCV
-                                          INNER JOIN ResourceContents AS RC ON RCV.ResourceContentId = RC.Id
-                                          INNER JOIN Resources AS R ON RC.ResourceId = R.Id
-                                          INNER JOIN ParentResources AS PR ON R.ParentResourceId = PR.Id
-                                          INNER JOIN Languages AS L ON RC.LanguageId = L.Id
-                                          INNER JOIN ProjectResourceContents PRC ON PRC.ResourceContentId = RC.Id
-                                          INNER JOIN Projects P ON P.Id = PRC.ProjectId
-                                 WHERE RCV.IsDraft = 1 AND RCV.AssignedUserId = @UserId AND RC.Status IN (1, 6)
-                                 """;
-
     public override void Configure()
     {
         Get("/resources/content/to-assign");
@@ -32,7 +18,22 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService) : En
     public override async Task HandleAsync(CancellationToken ct)
     {
         var user = await userService.GetUserFromJwtAsync(ct);
-        var resources = await dbContext.Database.SqlQueryRaw<Response>(Query, new SqlParameter("UserId", user.Id)).ToListAsync(ct);
+        var query = $"""
+                     SELECT RCV.ResourceContentId AS Id, R.EnglishLabel, PR.DisplayName AS ParentResourceName,
+                            L.EnglishDisplay AS LanguageEnglishDisplay, RCV.WordCount, P.Name AS ProjectName,
+                            P.ProjectedDeliveryDate AS ProjectProjectedDeliveryDate
+                     FROM ResourceContentVersions AS RCV
+                              INNER JOIN ResourceContents AS RC ON RCV.ResourceContentId = RC.Id
+                              INNER JOIN Resources AS R ON RC.ResourceId = R.Id
+                              INNER JOIN ParentResources AS PR ON R.ParentResourceId = PR.Id
+                              INNER JOIN Languages AS L ON RC.LanguageId = L.Id
+                              INNER JOIN ProjectResourceContents PRC ON PRC.ResourceContentId = RC.Id
+                              INNER JOIN Projects P ON P.Id = PRC.ProjectId
+                     WHERE RCV.IsDraft = 1 AND RCV.AssignedUserId = {user.Id}
+                       AND RC.Status IN ({(int)ResourceContentStatus.New}, {(int)ResourceContentStatus.TranslationNotStarted})
+                     """;
+
+        var resources = await dbContext.Database.SqlQueryRaw<Response>(query).ToListAsync(ct);
 
         await SendOkAsync(resources, ct);
     }
