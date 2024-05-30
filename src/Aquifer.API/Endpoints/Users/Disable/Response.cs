31e53437c0f2455e664b63865a05b8f730e529a9
Jesse Winston <jesse.winston@biblionexus.org>
2024-05-30 12:10:00 -0400

    Don't disable user if something's assigned to them.

---
+++ src/Aquifer.API/Endpoints/Users/Disable/Response.cs
@@ -0,0 +1,14 @@
+﻿namespace Aquifer.API.Endpoints.Users.Disable;
+
+public class Response
+{
+    public bool HasError { get; set; }
+    public string? Error { get; set; }
+    public IEnumerable<DisableUserAssignedResourcesResponse>? AssignedResources { get; set; }
+}
+
+public class DisableUserAssignedResourcesResponse
+{
+    public int ResourceContentId { get; set; }
+    public string DisplayName { get; set; } = null!;
+}
