8af4ac4e5c49aa5bba6d37bce6278d4714c75704
Jesse Winston <jesse.winston@biblionexus.org>
2024-05-30 14:24:06 -0400

    Add sort order column to Resources

---
+++ src/Aquifer.Data/Migrations/20240530182332_AddSortOrderToResources.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSortOrderToResources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "SortOrder",
+                table: "Resources",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SortOrder",
+                table: "Resources");
+        }
+    }
+}
