23118478cc5ec8160a5884c2a5676359d0c8f660
Frank Flood <frank.flood@biblionexus.org>
2024-06-05 07:53:51 -0400

    Initial Review
    Tables added, but need review on the relationships.

---
+++ src/Aquifer.Data/Entities/GreekNewTestamentWordSenses.cs
@@ -0,0 +1,10 @@
+﻿namespace Aquifer.Data.Entities;
+public class GreekNewTestamentWordSenseEntity
+{
+    public int GreekNewTestamentWordId { get; set; }
+    public int GreekSenseId { get; set; }
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    public ICollection<GreekNewTestamentWordEntity> GreekNewTestamentWords { get; set; } = new List<GreekNewTestamentWordEntity>();  //  Ask Jesse about this
+    public ICollection<GreekSenseGlossEntity> GreekSenses { get; set; } = new List<GreekSenseGlossEntity>();
+}
