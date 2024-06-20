5b29f37e546067c06b5843fb6c130e4ca047f649
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-06-20 10:20:13 -0400

    Manager review statuses for assigned to company (#331)

--- src/Aquifer.API/Endpoints/Resources/Content/AssignedToOwnCompany/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/AssignedToOwnCompany/List/Endpoint.cs
@@ -1,6 +1,7 @@
 using Aquifer.API.Common;
 using Aquifer.API.Services;
 using Aquifer.Data;
+using Aquifer.Data.Entities;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,22 +9,6 @@ namespace Aquifer.API.Endpoints.Resources.Content.AssignedToOwnCompany.List;
 
 public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<List<Response>>
 {
-    private const string Query = """
-                                 SELECT RCV.ResourceContentId AS Id, R.EnglishLabel, PR.DisplayName AS ParentResourceName, U.Id AS UserId,
-                                        U.FirstName AS UserFirstName, U.LastName AS UserLastName,
-                                        L.EnglishDisplay AS LanguageEnglishDisplay, RCV.WordCount, P.Name AS ProjectName,
-                                        P.ProjectedDeliveryDate AS ProjectProjectedDeliveryDate, R.SortOrder, RC.ContentUpdated
-                                 FROM ResourceContentVersions AS RCV
-                                          INNER JOIN Users AS U ON RCV.AssignedUserId = U.Id
-                                          INNER JOIN ResourceContents AS RC ON RCV.ResourceContentId = RC.Id
-                                          INNER JOIN Resources AS R ON RC.ResourceId = R.Id
-                                          INNER JOIN ParentResources AS PR ON R.ParentResourceId = PR.Id
-                                          INNER JOIN Languages AS L ON RC.LanguageId = L.Id
-                                          INNER JOIN ProjectResourceContents PRC ON PRC.ResourceContentId = RC.Id
-                                          INNER JOIN Projects P ON P.Id = PRC.ProjectId
-                                 WHERE RCV.IsDraft = 1 AND U.CompanyId = {0} AND RC.Status IN (1, 6, 2, 7)
-                                 """;
-
     public override void Configure()
     {
         Get("/resources/content/assigned-to-own-company");
@@ -33,11 +18,38 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService) : En
     public override async Task HandleAsync(CancellationToken ct)
     {
         var user = await userService.GetUserFromJwtAsync(ct);
-        var userCompanyId = user.CompanyId;
+        List<ResourceContentStatus> statuses =
+        [
+            ResourceContentStatus.New,
+            ResourceContentStatus.AquiferizeInProgress,
+            ResourceContentStatus.TranslationNotStarted,
+            ResourceContentStatus.TranslationInProgress,
+            ResourceContentStatus.AquiferizeManagerReview,
+            ResourceContentStatus.TranslationManagerReview
+        ];
 
-        var queryResponse = (await dbContext.Database.SqlQueryRaw<Response>(Query, userCompanyId).ToListAsync(ct))
-            .OrderBy(x => x.DaysUntilProjectDeadline).ThenBy(x => x.ProjectName).ThenBy(x => x.ParentResourceName)
-            .ThenBy(x => x.SortOrder).ThenBy(x => x.EnglishLabel).ToList();
+        var query = $"""
+                     SELECT RCV.ResourceContentId AS Id, R.EnglishLabel, PR.DisplayName AS ParentResourceName, U.Id AS UserId,
+                            U.FirstName AS UserFirstName, U.LastName AS UserLastName, RC.Status AS StatusValue,
+                            L.EnglishDisplay AS LanguageEnglishDisplay, RCV.WordCount, P.Name AS ProjectName,
+                            P.ProjectedDeliveryDate AS ProjectProjectedDeliveryDate, R.SortOrder, RC.ContentUpdated
+                     FROM ResourceContentVersions AS RCV
+                              INNER JOIN Users AS U ON RCV.AssignedUserId = U.Id
+                              INNER JOIN ResourceContents AS RC ON RCV.ResourceContentId = RC.Id
+                              INNER JOIN Resources AS R ON RC.ResourceId = R.Id
+                              INNER JOIN ParentResources AS PR ON R.ParentResourceId = PR.Id
+                              INNER JOIN Languages AS L ON RC.LanguageId = L.Id
+                              INNER JOIN ProjectResourceContents PRC ON PRC.ResourceContentId = RC.Id
+                              INNER JOIN Projects P ON P.Id = PRC.ProjectId
+                     WHERE RCV.IsDraft = 1 AND U.CompanyId = {user.CompanyId} AND RC.Status IN ({string.Join(',', statuses.Select(x => (int)x))})
+                     """;
+
+        var queryResponse = (await dbContext.Database.SqlQueryRaw<Response>(query).ToListAsync(ct)).OrderBy(x => x.DaysUntilProjectDeadline)
+            .ThenBy(x => x.ProjectName)
+            .ThenBy(x => x.ParentResourceName)
+            .ThenBy(x => x.SortOrder)
+            .ThenBy(x => x.EnglishLabel)
+            .ToList();
 
         await SendOkAsync(queryResponse, ct);
     }
--- src/Aquifer.API/Endpoints/Resources/Content/AssignedToOwnCompany/List/Response.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/AssignedToOwnCompany/List/Response.cs
@@ -1,5 +1,7 @@
 using System.Text.Json.Serialization;
 using Aquifer.API.Common.Dtos;
+using Aquifer.Common.Extensions;
+using Aquifer.Data.Entities;
 
 namespace Aquifer.API.Endpoints.Resources.Content.AssignedToOwnCompany.List;
 
@@ -12,13 +14,19 @@ public class Response
     public required int? WordCount { get; set; }
     public required string ProjectName { get; set; }
     public required int SortOrder { get; set; }
+    public required ResourceContentStatus StatusValue { get; set; }
+    public string StatusDisplayName => StatusValue.GetDisplayName();
 
     public int? DaysUntilProjectDeadline =>
         ProjectProjectedDeliveryDate == null
             ? null
             : (ProjectProjectedDeliveryDate.Value.ToDateTime(new TimeOnly(23, 59)) - DateTime.UtcNow).Days;
 
-    public UserDto AssignedUser => new() { Id = UserId, Name = $"{UserFirstName} {UserLastName}" };
+    public UserDto AssignedUser => new()
+    {
+        Id = UserId,
+        Name = $"{UserFirstName} {UserLastName}"
+    };
 
     public int? DaysSinceContentUpdated => ContentUpdated == null ? null : (DateTime.UtcNow - (DateTime)ContentUpdated).Days;
 
