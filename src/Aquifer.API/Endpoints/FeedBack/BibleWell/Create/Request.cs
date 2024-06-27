e55f9986f8a9d19aec5b636e207b64227550765b
Carlos Morales <carlos.morales@biblionexus.org>
2024-06-27 14:59:42 -0400

    fixingLinterError

--- src/Aquifer.API/Endpoints/FeedBack/BibleWell/Create/Request.cs
+++ src/Aquifer.API/Endpoints/FeedBack/BibleWell/Create/Request.cs
@@ -2,8 +2,8 @@ namespace Aquifer.API.Endpoints.FeedBack.BibleWell.Create;
 
 public class Request
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = null!;
     public string? Email { get; set; }
     public string? Phone { get; set; }
-    public string Feedback { get; set; }
+    public string Feedback { get; set; } = null!;
 }
