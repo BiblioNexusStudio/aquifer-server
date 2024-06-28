5b7561ef77ec1437148eb5ebd5df44d261c25493
Jesse Winston <jesse.winston@biblionexus.org>
2024-06-28 16:52:24 -0400

    Hacking it till it works

--- src/Aquifer.API/Clients/Http/OpenAI/OpenAiContracts.cs
+++ src/Aquifer.API/Clients/Http/OpenAI/OpenAiContracts.cs
@@ -7,25 +7,26 @@ public class OpenAiChatCompletionRequest
     public string Model { get; set; } = "gpt-4o";
     public float Temperature { get; set; } = 0.2f;
     public List<OpenAiChatCompletionRequestMessage> Messages { get; set; } = [];
+    public bool Stream { get; set; } = true;
 }
 
 public class OpenAiChatCompletionRequestMessage
 {
-    public required string Role { get; set; }
-    public required string Content { get; set; }
+    public string? Role { get; set; }
+    public string? Content { get; set; }
 }
 
 public class OpenAiChatCompletionResponse
 {
     // unless request specifies n > 1, this will always be length 1
     public List<OpenAiChatCompletionResponseChoices> Choices { get; set; } = [];
-    public OpenAiChatCompletionResponseUsage Usage { get; set; } = null!;
+    // public OpenAiChatCompletionResponseUsage Usage { get; set; } = null!;
 }
 
 public class OpenAiChatCompletionResponseChoices
 {
     public int Index { get; set; }
-    public OpenAiChatCompletionRequestMessage Message { get; set; } = null!;
+    public OpenAiChatCompletionRequestMessage Delta { get; set; } = null!;
 
     [JsonPropertyName("finish_reason")]
     public string FinishReason { get; set; } = null!;
--- src/Aquifer.API/Endpoints/AI/Simplify/Endpoint.cs
+++ src/Aquifer.API/Endpoints/AI/Simplify/Endpoint.cs
@@ -1,7 +1,6 @@
 ﻿using Aquifer.API.Clients.Http.OpenAI;
 using Aquifer.API.Common;
 using Aquifer.API.Configuration;
-using Aquifer.Common.Utilities;
 using FastEndpoints;
 using Microsoft.Extensions.Options;
 
@@ -18,59 +17,59 @@ public class Endpoint(IOpenAiHttpClient openAiClient, IOptions<ConfigurationOpti
 
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        string? error = null;
-        var chunks = HtmlUtilities.GetChunks(req.Content);
-        List<string> newChunks = [];
-        var prompt = $"{options.Value.OpenAiSettings.HtmlSimplifyBasePrompt} {req.Prompt}";
-
-        foreach (var (name, html) in chunks)
-        {
-            if (name.StartsWith('h'))
-            {
-                newChunks.Add(html);
-                continue;
-            }
-
-            var clientResponse = await openAiClient.PostChatCompletionsAsync(prompt, html, ct);
-            var clientResponseContent = await clientResponse.Content.ReadAsStringAsync(ct);
-
-            if (!clientResponse.IsSuccessStatusCode)
-            {
-                error = "Bad response code from OpenAI.";
-                LogError(error, req, clientResponseContent);
-                newChunks.Add(html);
-                continue;
-            }
-
-            var chatCompletion = JsonUtilities.DefaultDeserialize<OpenAiChatCompletionResponse>(clientResponseContent);
-            if (chatCompletion.Choices.Count == 0)
-            {
-                error = "No choices returned by OpenAI";
-                LogError(error, req, clientResponseContent);
-                newChunks.Add(html);
-                continue;
-            }
-
-            var choice = chatCompletion.Choices[0];
-            if (choice.FinishReason == "stop")
-            {
-                newChunks.Add(choice.Message.Content);
-            }
-            else
-            {
-                error = "Bad finish reason from OpenAI";
-                LogError(error, req, clientResponseContent);
-                newChunks.Add(html);
-            }
-        }
-
-        var response = new Response
-        {
-            Content = string.Join("", newChunks),
-            Error = error
-        };
-
-        await SendAsync(response, 200, ct);
+        // string? error = null;
+        // var chunks = HtmlUtilities.GetChunks(req.Content);
+        // List<string> newChunks = [];
+        // var prompt = $"{options.Value.OpenAiSettings.HtmlSimplifyBasePrompt} {req.Prompt}";
+        //
+        // foreach (var (name, html) in chunks)
+        // {
+        //     if (name.StartsWith('h'))
+        //     {
+        //         newChunks.Add(html);
+        //         continue;
+        //     }
+        //
+        //     var clientResponse = await openAiClient.PostChatCompletionsAsync(prompt, html, ct);
+        //     var clientResponseContent = await clientResponse.Content.ReadAsStringAsync(ct);
+        //
+        //     if (!clientResponse.IsSuccessStatusCode)
+        //     {
+        //         error = "Bad response code from OpenAI.";
+        //         LogError(error, req, clientResponseContent);
+        //         newChunks.Add(html);
+        //         continue;
+        //     }
+        //
+        //     var chatCompletion = JsonUtilities.DefaultDeserialize<OpenAiChatCompletionResponse>(clientResponseContent);
+        //     if (chatCompletion.Choices.Count == 0)
+        //     {
+        //         error = "No choices returned by OpenAI";
+        //         LogError(error, req, clientResponseContent);
+        //         newChunks.Add(html);
+        //         continue;
+        //     }
+        //
+        //     var choice = chatCompletion.Choices[0];
+        //     if (choice.FinishReason == "stop")
+        //     {
+        //         newChunks.Add(choice.Message.Content);
+        //     }
+        //     else
+        //     {
+        //         error = "Bad finish reason from OpenAI";
+        //         LogError(error, req, clientResponseContent);
+        //         newChunks.Add(html);
+        //     }
+        // }
+        //
+        // var response = new Response
+        // {
+        //     Content = string.Join("", newChunks),
+        //     Error = error
+        // };
+        //
+        // await SendAsync(response, 200, ct);
     }
 
     private void LogError(string message, Request req, string responseContent)
