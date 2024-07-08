b693927ce46dbda71360458d205f9301d2b1cc8f
Carlos Morales <carlos.rey.morales@gmail.com>
2024-07-08 09:03:41 -0400

    Send-To-Manager Now Returns A List Of Users Assigned To ContentIds (#343)

---
+++ src/Aquifer.API/Endpoints/Resources/Content/SendForManagerReview/Response.cs
@@ -0,0 +1,12 @@
+namespace Aquifer.API.Endpoints.Resources.Content.SendForManagerReview;
+
+public class Response
+{
+    public List<UserAssignment> Assignments { get; set; } = [];
+}
+
+public class UserAssignment
+{
+    public int ResourceContentId { get; set; }
+    public int AssignedUserId { get; set; }
+}
