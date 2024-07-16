4ffa7d29473896296964ace1a176fafed55c6a7f
Carlos Morales <carlos.morales@biblionexus.org>
2024-07-16 10:07:16 -0400

    addedLastAssigedUserToMyWorkEndPoint

--- src/Aquifer.API/Endpoints/Resources/Content/AssignedToSelf/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/AssignedToSelf/List/Endpoint.cs
@@ -1,4 +1,6 @@
+using Aquifer.API.Common.Dtos;
 using Aquifer.API.Services;
+using Aquifer.Common.Extensions;
 using Aquifer.Data;
 using Aquifer.Data.Entities;
 using FastEndpoints;
@@ -6,10 +8,10 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.API.Endpoints.Resources.Content.AssignedToSelf.List;
 
-public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<List<Response>>
+public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<IEnumerable<Response>>
 {
     private const string Query = """
-                                 SELECT RCV.ResourceContentId AS Id, R.EnglishLabel, PR.DisplayName AS ParentResourceName,
+                                 SELECT RCV.ResourceContentId AS ResourceContentId, RCV.Id AS ResourceContentVersionId, R.EnglishLabel, PR.DisplayName AS ParentResourceName,
                                         L.EnglishDisplay AS LanguageEnglishDisplay, RCV.WordCount, RC.Status AS StatusValue,
                                         P.Name AS ProjectName, P.ProjectedDeliveryDate, History.Created AS HistoryCreated, R.SortOrder, RC.ContentUpdated
                                  FROM ResourceContentVersions AS RCV
@@ -36,10 +38,61 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService) : En
     public override async Task HandleAsync(CancellationToken ct)
     {
         var user = await userService.GetUserFromJwtAsync(ct);
-        var resourceContents = (await dbContext.Database
-                .SqlQueryRaw<Response>(Query, user.Id, (int)ResourceContentStatus.TranslationNotStarted).ToListAsync(ct))
-            .OrderByDescending(x => x.DaysSinceAssignment).ThenBy(x => x.ProjectName).ThenBy(x => x.EnglishLabel).ToList();
+        var queryResults = await dbContext.Database
+            .SqlQueryRaw<SqlQueryResult>(Query, user.Id, (int)ResourceContentStatus.TranslationNotStarted).ToListAsync(ct);
 
-        await SendOkAsync(resourceContents, ct);
+        var resourceContentVersionIds = queryResults.Select(x => x.ResourceContentVersionId);
+
+        var assignmentHistory = await dbContext.ResourceContentVersionAssignedUserHistory
+            .Where(x => resourceContentVersionIds.Contains(x.ResourceContentVersionId)).OrderByDescending(x => x.Id)
+            .Include(x => x.AssignedUser).ToListAsync(ct);
+
+        var groupedAssignmentsHistories = assignmentHistory.GroupBy(x => x.ResourceContentVersionId);
+
+        List<(int resourceContentVersionIds, UserDto? user)> lastAssignments = [];
+        foreach (var versionHistory in groupedAssignmentsHistories)
+        {
+            var lastAssignment = versionHistory.OrderByDescending(x => x.Id).Skip(1).Take(1).FirstOrDefault();
+            if (lastAssignment is not null)
+            {
+                lastAssignments.Add((lastAssignment.ResourceContentVersionId, UserDto.FromUserEntity(lastAssignment.AssignedUser)));
+            }
+        }
+
+        Response = queryResults.Select(x => new Response
+        {
+            Id = x.ResourceContentId,
+            EnglishLabel = x.EnglishLabel,
+            ParentResourceName = x.ParentResourceName,
+            LanguageEnglishDisplay = x.LanguageEnglishDisplay,
+            WordCount = x.WordCount,
+            StatusValue = x.StatusValue,
+            SortOrder = x.SortOrder,
+            ProjectName = x.ProjectName,
+            Status = x.StatusValue.GetDisplayName(),
+            StatusDisplayName = x.StatusValue.GetDisplayName(),
+            DaysSinceAssignment = (DateTime.UtcNow - x.HistoryCreated).Days,
+            DaysUntilProjectDeadline = x.ProjectedDeliveryDate == null
+                ? null
+                : (x.ProjectedDeliveryDate.Value.ToDateTime(new TimeOnly(23, 59)) - DateTime.UtcNow).Days,
+            DaysSinceContentUpdated = x.ContentUpdated == null ? null : (DateTime.UtcNow - (DateTime)x.ContentUpdated).Days,
+            LastAssignedUser = lastAssignments.FirstOrDefault(a => a.resourceContentVersionIds == x.ResourceContentVersionId).user
+        }).OrderByDescending(x => x.DaysSinceAssignment).ThenBy(x => x.ProjectName).ThenBy(x => x.EnglishLabel);
     }
+}
+
+internal class SqlQueryResult
+{
+    public required int ResourceContentId { get; set; }
+    public required int ResourceContentVersionId { get; set; }
+    public required string EnglishLabel { get; set; }
+    public required string ParentResourceName { get; set; }
+    public required string LanguageEnglishDisplay { get; set; }
+    public required int? WordCount { get; set; }
+    public required ResourceContentStatus StatusValue { get; set; }
+    public required int SortOrder { get; set; }
+    public string? ProjectName { get; set; }
+    public DateTime? ContentUpdated { get; set; }
+    public DateOnly? ProjectedDeliveryDate { get; set; }
+    public DateTime HistoryCreated { get; set; }
 }
