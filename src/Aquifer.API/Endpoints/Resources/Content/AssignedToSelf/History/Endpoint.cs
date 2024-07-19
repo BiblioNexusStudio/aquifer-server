a8e610e59911c2a1bde1e7f6bf003865b850af40
Jesse Winston <jesse.winston@biblionexus.org>
2024-07-19 11:48:46 -0400

    Add history endpoint

---
+++ src/Aquifer.API/Endpoints/Resources/Content/AssignedToSelf/History/Endpoint.cs
@@ -0,0 +1,35 @@
+﻿using Aquifer.API.Services;
+using Aquifer.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Resources.Content.AssignedToSelf.History;
+
+public class Endpoint(AquiferDbContext dbContext, IUserService userService) : EndpointWithoutRequest<IEnumerable<Response>>
+{
+    public override void Configure()
+    {
+        Get("/resources/content/assigned-to-self/history");
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var user = await userService.GetUserFromJwtAsync(ct);
+        var queryResults = await dbContext.ResourceContentVersionAssignedUserHistory
+            .Where(x => x.ChangedByUserId == user.Id &&
+                x.Created > DateTime.UtcNow.AddDays(-45) &&
+                x.ResourceContentVersion.AssignedUserId != user.Id)
+            .Select(x => new Response
+            {
+                Id = x.ResourceContentVersion.ResourceContentId,
+                EnglishLabel = x.ResourceContentVersion.ResourceContent.Resource.EnglishLabel,
+                LastActionTime = x.Created,
+                ParentResourceName = x.ResourceContentVersion.ResourceContent.Resource.ParentResource.DisplayName,
+                SortOrder = x.ResourceContentVersion.ResourceContent.Resource.SortOrder,
+                SourceWords = Random.Shared.Next() // TODO
+            })
+            .ToListAsync(ct);
+
+        Response = queryResults.GroupBy(g => g.Id).Select(g => g.OrderByDescending(r => r.LastActionTime).First());
+    }
+}
---
+++ src/Aquifer.API/Endpoints/Resources/Content/AssignedToSelf/History/Response.cs
@@ -0,0 +1,11 @@
+﻿namespace Aquifer.API.Endpoints.Resources.Content.AssignedToSelf.History;
+
+public class Response
+{
+    public required int Id { get; set; }
+    public required string EnglishLabel { get; set; }
+    public required string ParentResourceName { get; set; }
+    public required DateTime LastActionTime { get; set; }
+    public required int SourceWords { get; set; }
+    public required int SortOrder { get; set; }
+}
