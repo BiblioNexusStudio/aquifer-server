71161f97f117ef91a8d79fe7317c6c5442b3274d
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-07-30 14:19:22 -0600

    add SortOrder to response for unaquiferized/untranslated endpoints

--- src/Aquifer.API/Endpoints/Resources/Unaquiferized/List/Response.cs
+++ src/Aquifer.API/Endpoints/Resources/Unaquiferized/List/Response.cs
@@ -3,6 +3,7 @@ namespace Aquifer.API.Endpoints.Resources.Unaquiferized.List;
 public record Response
 {
     public required string Title { get; set; }
+    public required int SortOrder { get; set; }
     public required int ResourceId { get; set; }
     public required int WordCount { get; set; }
 }
--- src/Aquifer.API/Endpoints/Resources/Untranslated/List/Response.cs
+++ src/Aquifer.API/Endpoints/Resources/Untranslated/List/Response.cs
@@ -3,6 +3,7 @@ namespace Aquifer.API.Endpoints.Resources.Untranslated.List;
 public record Response
 {
     public required string Title { get; set; }
+    public required int SortOrder { get; set; }
     public required int ResourceId { get; set; }
     public required int WordCount { get; set; }
     public required bool IsBeingAquiferized { get; set; }
