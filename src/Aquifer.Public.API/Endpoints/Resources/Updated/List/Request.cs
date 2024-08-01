d3d2a68d63a635420207b28642c6bb32678ffae5
Jesse Winston <jesse.winston@biblionexus.org>
2024-08-01 13:14:23 -0400

    Basic updated lookup

---
+++ src/Aquifer.Public.API/Endpoints/Resources/Updated/List/Request.cs
@@ -0,0 +1,14 @@
+﻿namespace Aquifer.Public.API.Endpoints.Resources.Updated.List;
+
+public class Request
+{
+    /// <summary>
+    ///     UTC timestamp indicating how far back to check for updates. The maximum look-back is 90 days.
+    /// </summary>
+    public DateTime Timestamp { get; set; }
+
+    /// <summary>
+    ///     Optional LanguageId to search on. If none is provided, updates for all languages will be returned.
+    /// </summary>
+    public int? LanguageId { get; set; }
+}
---
+++ src/Aquifer.Public.API/Endpoints/Resources/Updated/List/Response.cs
@@ -0,0 +1,21 @@
+﻿namespace Aquifer.Public.API.Endpoints.Resources.Updated.List;
+
+public class Response
+{
+    public int ReturnedItemCount => Items.Count;
+    public List<ResponseContent> Items { get; set; } = [];
+}
+
+public class ResponseContent
+{
+    public required string UpdateType { get; set; }
+    public required int LanguageId { get; set; }
+    public required int ResourceId { get; set; }
+    public required DateTime Timestamp { get; set; }
+}
+
+public enum ResponseContentUpdateType
+{
+    New = 0,
+    Updated = 1
+}
