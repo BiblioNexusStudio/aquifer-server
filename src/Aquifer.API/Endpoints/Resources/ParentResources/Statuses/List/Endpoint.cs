beaef1d9b0d8582136ece1e918a65e5099d33c66
Elisabeth <shakespearee04@gmail.com>
2024-08-05 13:25:58 -0400

    Make sql querying a little faster, and cleanup

--- src/Aquifer.API/Endpoints/Resources/ParentResources/Statuses/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/ParentResources/Statuses/List/Endpoint.cs
@@ -4,7 +4,6 @@ using Aquifer.Common.Utilities;
 using Aquifer.Data;
 using Aquifer.Data.Entities;
 using FastEndpoints;
-using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.API.Endpoints.Resources.ParentResources.Statuses.List;
@@ -13,43 +12,39 @@ public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, IEnumerabl
 {
     private const int EnglishLanguageId = 1;
 
-    private const string Query = """
-                                 SELECT PR.DisplayName AS Title,
-                                        PR.Id AS ResourceId,
-                                        COUNT(RC.Id) AS TotalResources,
-                                        OtherLanguage.Total AS TotalLanguageResources,
-                                        OtherLanguage.LastPublished AS LastPublished,
-                                        PR.LicenseInfo AS LicenseInfoValue,
-                                        PR.ResourceType AS ResourceType
-                                 FROM ResourceContents RC
-                                          INNER JOIN Resources R ON R.Id = RC.ResourceId
-                                          INNER JOIN ParentResources PR ON PR.Id = R.ParentResourceId
-                                          CROSS APPLY (SELECT COUNT(RC2.Id) AS Total, MAX(RC2.Updated) AS LastPublished
-                                                       FROM ResourceContents RC2
-                                                                INNER JOIN ResourceContentVersions RCV ON RCV.ResourceContentId = RC2.Id AND RCV.IsPublished = 1
-                                                                INNER JOIN Resources R2 ON R2.Id = RC2.ResourceId
-                                                       WHERE R2.ParentResourceId = PR.Id
-                                                         AND RC2.LanguageId = @LanguageId) OtherLanguage
-                                 WHERE RC.LanguageId = @EnglishLanguageId
-                                 GROUP BY PR.DisplayName, OtherLanguage.Total, OtherLanguage.LastPublished, PR.LicenseInfo, PR.ResourceType, PR.Id
-                                 ORDER BY PR.DisplayName
-                                 """;
-
     public override void Configure()
     {
-        Get("/resources/parent-resources/statuses/{LanguageId}");
+        Get("/resources/parent-resources/statuses");
         Options(EndpointHelpers.SetCacheOption(60));
-        ResponseCache(EndpointHelpers.OneDayInSeconds, varyByQueryKeys:["*"]);
+        ResponseCache(EndpointHelpers.OneDayInSeconds, varyByQueryKeys: ["languageId"]);
         AllowAnonymous();
     }
 
     public override async Task HandleAsync(Request request, CancellationToken ct)
     {
-        var rows = await dbContext.Database
-            .SqlQueryRaw<ParentAndLanguageRow>(Query,
-                new SqlParameter("LanguageId", request.LanguageId),
-                new SqlParameter("EnglishLanguageId", EnglishLanguageId))
+        var rows = await dbContext.Database.SqlQueryRaw<ParentAndLanguageRow>($"""
+                                                                               SELECT PR.DisplayName AS Title,
+                                                                                      PR.Id AS ResourceId,
+                                                                                      COUNT(RC.Id) AS TotalResources,
+                                                                                      OtherLanguage.Total AS TotalLanguageResources,
+                                                                                      OtherLanguage.LastPublished AS LastPublished,
+                                                                                      PR.LicenseInfo AS LicenseInfoValue,
+                                                                                      PR.ResourceType AS ResourceType
+                                                                               FROM ResourceContents RC
+                                                                                        INNER JOIN Resources R ON R.Id = RC.ResourceId
+                                                                                        INNER JOIN ParentResources PR ON PR.Id = R.ParentResourceId
+                                                                                        CROSS APPLY (SELECT COUNT(RC2.Id) AS Total, MAX(RC2.Updated) AS LastPublished
+                                                                                                     FROM ResourceContents RC2
+                                                                                                              INNER JOIN ResourceContentVersions RCV ON RCV.ResourceContentId = RC2.Id AND RCV.IsPublished = 1
+                                                                                                              INNER JOIN Resources R2 ON R2.Id = RC2.ResourceId
+                                                                                                     WHERE R2.ParentResourceId = PR.Id
+                                                                                                       AND RC2.LanguageId = {request.LanguageId}) OtherLanguage
+                                                                               WHERE RC.LanguageId = {EnglishLanguageId}
+                                                                               GROUP BY PR.DisplayName, OtherLanguage.Total, OtherLanguage.LastPublished, PR.LicenseInfo, PR.ResourceType, PR.Id
+                                                                               ORDER BY PR.DisplayName
+                                                                               """)
             .ToListAsync(ct);
+
         Response = rows.Select(x => new Response
         {
             ResourceId = x.ResourceId,
@@ -84,9 +79,9 @@ public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, IEnumerabl
 
 internal class ParentAndLanguageRow
 {
-    public required ResourceType ResourceType { get; set; }
-    public required string Title { get; set; }
-    public required int ResourceId { get; set; }
+    public ResourceType ResourceType { get; set; }
+    public string Title { get; set; } = null!;
+    public int ResourceId { get; set; }
     public string? LicenseInfoValue { get; set; }
     public int TotalResources { get; set; }
     public int TotalLanguageResources { get; set; }
