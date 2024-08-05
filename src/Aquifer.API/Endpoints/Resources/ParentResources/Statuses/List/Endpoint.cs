09bde0c46f4f4c52ed07eb3794ecf831c016fa2e
Elisabeth <shakespearee04@gmail.com>
2024-08-05 14:20:42 -0400

    use SqlQuery instead of SqlQueryRaw

--- src/Aquifer.API/Endpoints/Resources/ParentResources/Statuses/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/ParentResources/Statuses/List/Endpoint.cs
@@ -22,27 +22,27 @@ public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, IEnumerabl
 
     public override async Task HandleAsync(Request request, CancellationToken ct)
     {
-        var rows = await dbContext.Database.SqlQueryRaw<ParentAndLanguageRow>($"""
-                                                                               SELECT PR.DisplayName AS Title,
-                                                                                      PR.Id AS ResourceId,
-                                                                                      COUNT(RC.Id) AS TotalResources,
-                                                                                      OtherLanguage.Total AS TotalLanguageResources,
-                                                                                      OtherLanguage.LastPublished AS LastPublished,
-                                                                                      PR.LicenseInfo AS LicenseInfoValue,
-                                                                                      PR.ResourceType AS ResourceType
-                                                                               FROM ResourceContents RC
-                                                                                        INNER JOIN Resources R ON R.Id = RC.ResourceId
-                                                                                        INNER JOIN ParentResources PR ON PR.Id = R.ParentResourceId
-                                                                                        CROSS APPLY (SELECT COUNT(RC2.Id) AS Total, MAX(RC2.Updated) AS LastPublished
-                                                                                                     FROM ResourceContents RC2
-                                                                                                              INNER JOIN ResourceContentVersions RCV ON RCV.ResourceContentId = RC2.Id AND RCV.IsPublished = 1
-                                                                                                              INNER JOIN Resources R2 ON R2.Id = RC2.ResourceId
-                                                                                                     WHERE R2.ParentResourceId = PR.Id
-                                                                                                       AND RC2.LanguageId = {request.LanguageId}) OtherLanguage
-                                                                               WHERE RC.LanguageId = {EnglishLanguageId}
-                                                                               GROUP BY PR.DisplayName, OtherLanguage.Total, OtherLanguage.LastPublished, PR.LicenseInfo, PR.ResourceType, PR.Id
-                                                                               ORDER BY PR.DisplayName
-                                                                               """)
+        var rows = await dbContext.Database.SqlQuery<ParentAndLanguageRow>($"""
+                                                                            SELECT PR.DisplayName AS Title,
+                                                                                   PR.Id AS ResourceId,
+                                                                                   COUNT(RC.Id) AS TotalResources,
+                                                                                   OtherLanguage.Total AS TotalLanguageResources,
+                                                                                   OtherLanguage.LastPublished AS LastPublished,
+                                                                                   PR.LicenseInfo AS LicenseInfoValue,
+                                                                                   PR.ResourceType AS ResourceType
+                                                                            FROM ResourceContents RC
+                                                                                     INNER JOIN Resources R ON R.Id = RC.ResourceId
+                                                                                     INNER JOIN ParentResources PR ON PR.Id = R.ParentResourceId
+                                                                                     CROSS APPLY (SELECT COUNT(RC2.Id) AS Total, MAX(RC2.Updated) AS LastPublished
+                                                                                                  FROM ResourceContents RC2
+                                                                                                           INNER JOIN ResourceContentVersions RCV ON RCV.ResourceContentId = RC2.Id AND RCV.IsPublished = 1
+                                                                                                           INNER JOIN Resources R2 ON R2.Id = RC2.ResourceId
+                                                                                                  WHERE R2.ParentResourceId = PR.Id
+                                                                                                    AND RC2.LanguageId = {request.LanguageId}) OtherLanguage
+                                                                            WHERE RC.LanguageId = {EnglishLanguageId}
+                                                                            GROUP BY PR.DisplayName, OtherLanguage.Total, OtherLanguage.LastPublished, PR.LicenseInfo, PR.ResourceType, PR.Id
+                                                                            ORDER BY PR.DisplayName
+                                                                            """)
             .ToListAsync(ct);
 
         Response = rows.Select(x => new Response
