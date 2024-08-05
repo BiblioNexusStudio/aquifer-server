56c61dfaa2fe0b0b9c74dddf5b248b5e4424c6ae
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-08-05 14:57:16 -0400

    add ExternalVersion to ResourceContents

---
+++ src/Aquifer.Data/Migrations/20240806163342_AddExternalVersionToResources.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddExternalVersionToResources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalVersion",
+                table: "ResourceContents",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExternalVersion",
+                table: "ResourceContents");
+        }
+    }
+}
