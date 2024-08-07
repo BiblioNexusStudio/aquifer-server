9cc0c6011c5f4f280cd38523cd8629da8d424baa
Jesse Winston <jesse.winston@biblionexus.org>
2024-08-07 13:18:59 -0400

    Remove required from null props because the serializer might remove it

--- src/Aquifer.Common/Jobs/Messages/TrackResourceContentRequestMessage.cs
+++ src/Aquifer.Common/Jobs/Messages/TrackResourceContentRequestMessage.cs
@@ -4,8 +4,8 @@ public record TrackResourceContentRequestMessage
 {
     public required string IpAddress { get; set; }
     public required IEnumerable<int> ResourceContentIds { get; set; }
-    public required string? Source { get; set; }
-    public required string? SubscriptionName { get; set; }
-    public required string? EndpointId { get; set; }
-    public required string? UserId { get; set; }
+    public string? Source { get; set; }
+    public string? SubscriptionName { get; set; }
+    public string? EndpointId { get; set; }
+    public string? UserId { get; set; }
 }
