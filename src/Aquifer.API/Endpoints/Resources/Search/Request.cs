40bde90041abe23a68cb09dd5401a1c346bb2f12
Carlos Morales <carlos.morales@biblionexus.org>
2024-08-08 11:27:22 -0400

    madeQueryNullable

--- src/Aquifer.API/Endpoints/Resources/Search/Request.cs
+++ src/Aquifer.API/Endpoints/Resources/Search/Request.cs
@@ -1,4 +1,3 @@
-using System.ComponentModel;
 using Aquifer.Data.Entities;
 
 namespace Aquifer.API.Endpoints.Resources.Search;
@@ -6,19 +5,11 @@ namespace Aquifer.API.Endpoints.Resources.Search;
 public record Request
 {
     public int LanguageId { get; set; }
-    public string Query { get; set; } = null!;
+    public string? Query { get; set; }
     public List<ResourceType> ResourceTypes { get; set; } = [];
     public string? BookCode { get; init; }
-
-    [DefaultValue(0)]
     public int StartChapter { get; init; }
-
-    [DefaultValue(0)]
     public int EndChapter { get; init; }
-
-    [DefaultValue(0)]
     public int StartVerse { get; init; }
-
-    [DefaultValue(0)]
     public int EndVerse { get; init; }
 }
