1b21bf82cf6d9428095429ead57d88a8b816d58f
Carlos Morales <carlos.morales@biblionexus.org>
2024-08-09 13:32:52 -0400

    copiedValidatorsFromPublicApi

--- src/Aquifer.API/Endpoints/Resources/Search/Validator.cs
+++ src/Aquifer.API/Endpoints/Resources/Search/Validator.cs
@@ -1,3 +1,5 @@
+using Aquifer.Common.Utilities;
+using Aquifer.Data.Enums;
 using FastEndpoints;
 using FluentValidation;
 
@@ -7,8 +9,28 @@ public class Validator : Validator<Request>
 {
     public Validator()
     {
+        RuleFor(x => x).Must(x => x.Query is not null || x.BookCode is not null)
+            .WithMessage("Either search query or bookCode is required.");
+        RuleFor(x => x.BookCode).Must(x => BibleBookCodeUtilities.IdFromCode(x!) != BookId.None)
+            .WithMessage("Invalid book code {PropertyValue}. Get a valid list from /bible-books endpoint.")
+            .When(x => x.BookCode is not null);
+        RuleFor(x => x.BookCode).NotNull().When(x => x.StartChapter > 0);
+        RuleFor(x => x.Query).MinimumLength(3).When(x => x.Query is not null);
         RuleFor(x => x.LanguageId).NotEmpty();
         RuleFor(x => x.ResourceTypes).NotEmpty();
         RuleForEach(x => x.ResourceTypes).IsInEnum();
+
+        RuleFor(x => x.StartChapter).InclusiveBetween(0, 150);
+        RuleFor(x => x.EndChapter).InclusiveBetween(0, 150);
+        RuleFor(x => x.StartVerse).InclusiveBetween(0, 200);
+        RuleFor(x => x.EndVerse).InclusiveBetween(0, 200);
+
+        RuleFor(x => x.StartChapter).GreaterThan(0).When(x => x.EndChapter > 0 || x.StartVerse > 0);
+        RuleFor(x => x.EndChapter).GreaterThan(0).When(x => x.StartChapter > 0);
+        RuleFor(x => x.StartVerse).GreaterThan(0).When(x => x.EndVerse > 0);
+        RuleFor(x => x.EndVerse).GreaterThan(0).When(x => x.StartVerse > 0);
+
+        RuleFor(x => x).Must(x => x.StartChapter <= x.EndChapter)
+            .WithMessage("startChapter cannot be greater than endChapter");
     }
 }
