d4ade47a7bd3592d4ad81bf0eb24772d2d677490
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-08-13 11:28:27 -0600

    add endpoints for creating and deleting Bible references

---
+++ src/Aquifer.API/Endpoints/Resources/BibleReferences/Create/Endpoint.cs
@@ -0,0 +1,69 @@
+using Aquifer.API.Common;
+using Aquifer.Data;
+using Aquifer.Data.Entities;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Resources.BibleReferences.Create;
+
+public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request>
+{
+    public override void Configure()
+    {
+        Post("/resources/bible-references");
+        Permissions(PermissionName.EditContentBibleReferences);
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken ct)
+    {
+        var resourceContent = await dbContext.ResourceContents
+            .Include(rc => rc.Resource)
+            .ThenInclude(r => r.VerseResources)
+            .Include(rc => rc.Resource)
+            .ThenInclude(r => r.PassageResources)
+            .ThenInclude(r => r.Passage)
+            .SingleOrDefaultAsync(rc => rc.Id == request.ResourceContentId, ct);
+
+        if (resourceContent == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        if (resourceContent.LanguageId != 1)
+        {
+            ThrowError("Only English content can be used for updating Bible references.");
+            return;
+        }
+
+        if (request.StartVerseId == request.EndVerseId)
+        {
+            if (!resourceContent.Resource.VerseResources.Any(vr => vr.VerseId == request.StartVerseId))
+            {
+                await dbContext.VerseResources.AddAsync(
+                    new VerseResourceEntity { ResourceId = resourceContent.ResourceId, VerseId = request.StartVerseId }, ct);
+            }
+        }
+        else
+        {
+            var existingPassage = await dbContext.Passages
+                .SingleOrDefaultAsync(p => p.StartVerseId == request.StartVerseId && p.EndVerseId == request.EndVerseId, ct);
+
+            if (existingPassage == null)
+            {
+                existingPassage = new PassageEntity { StartVerseId = request.StartVerseId, EndVerseId = request.EndVerseId };
+                await dbContext.Passages.AddAsync(existingPassage, ct);
+            }
+
+            if (!resourceContent.Resource.PassageResources.Any(pr =>
+                    pr.Passage.StartVerseId == request.StartVerseId && pr.Passage.EndVerseId == request.EndVerseId))
+            {
+                await dbContext.PassageResources.AddAsync(
+                    new PassageResourceEntity { ResourceId = resourceContent.ResourceId, PassageId = existingPassage.Id }, ct);
+            }
+        }
+
+        await dbContext.SaveChangesAsync(ct);
+        await SendOkAsync(ct);
+    }
+}
