660182184c5e6d0a013835d6a2033765cb9af087
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-08-14 09:12:34 -0600

    remove old scripts

--- src/Aquifer.Data/Scripts/Initial_Resource_Types_Insert.sql
+++
@@ -1,8 +0,0 @@
-SET IDENTITY_INSERT ParentResources ON;
-
-INSERT INTO ParentResources (Id, ShortName, DisplayName) VALUES
-(1, 'CBBTER', 'SRV CBBT-ER (Church-Based Bible Translation Exegetical Resources)'),
-(2, 'TyndaleBibleDictionary', 'Bible Dictionary (Tyndale)'),
-(3, 'UbsImages', 'Images (UBS)');
-
-SET IDENTITY_INSERT ParentResources OFF;
