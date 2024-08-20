8bd6f3d645a8579d790a1014c1ee7fdcce07b920
Carlos Morales <carlos.morales@biblionexus.org>
2024-08-20 09:01:04 -0400

    addedCompanyReviewersTableMigration

---
+++ src/Aquifer.Data/Migrations/20240819190814_AddCompanyReviewersTable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCompanyReviewersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CompanyReviewers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CompanyId = table.Column<int>(type: "int", nullable: false),
+                    LanguageId = table.Column<int>(type: "int", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CompanyReviewers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CompanyReviewers_UserId_CompanyId",
+                table: "CompanyReviewers",
+                columns: new[] { "UserId", "CompanyId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CompanyReviewers");
+        }
+    }
+}
