58f30321734f697c70261696b31b2c1bf39460e6
Jesse Winston <jesse.winston@biblionexus.org>
2024-08-21 12:18:39 -0400

    Available languages endpoint

--- src/Aquifer.Public.API/Endpoints/Resources/Get/AvailableLanguages/Endpoint.cs
+++ src/Aquifer.Public.API/Endpoints/Resources/Get/AvailableLanguages/Endpoint.cs
@@ -1,9 +1,10 @@
 ﻿using Aquifer.Data;
 using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.Public.API.Endpoints.Resources.Get.AvailableLanguages;
 
-public class Endpoint(AquiferDbContext dbContext) : EndpointWithoutRequest<Response>
+public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, IEnumerable<Response>>
 {
     public override void Configure()
     {
@@ -11,13 +12,38 @@ public class Endpoint(AquiferDbContext dbContext) : EndpointWithoutRequest<Respo
         Description(d => d.ProducesProblemFE(404));
         Summary(s =>
         {
-            s.Summary = "For a given resource id, see in what other languages it is available.";
-            s.Description = "For a given resource id, return a list of all other languages that are available for this resource.";
+            s.Summary = "For a given resource content id, see in what other languages it is available.";
+            s.Description = "For a given resource content id, return a list of all other languages that are available for this resource.";
         });
     }
 
-    public override async Task HandleAsync(CancellationToken ct)
+    public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        await Task.Delay(1, ct);
+        var foundContent = await dbContext.ResourceContentVersions.Where(x => x.ResourceContentId == req.ContentId && x.IsPublished)
+            .Select(x => new ResourceContentQueryResponse(x.ResourceContent.ResourceId, x.ResourceContent.LanguageId))
+            .FirstOrDefaultAsync(ct);
+
+        if (foundContent is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        Response = await dbContext.ResourceContentVersions
+            .Where(x => x.ResourceContent.ResourceId == foundContent.ResourceId &&
+                x.IsPublished &&
+                x.ResourceContent.LanguageId != foundContent.LanguageId)
+            .Select(x => new Response
+            {
+                ContentId = x.ResourceContentId,
+                ContentDisplayName = x.DisplayName,
+                LanguageId = x.ResourceContent.LanguageId,
+                LanguageDisplayName = x.ResourceContent.Language.DisplayName,
+                LanguageCode = x.ResourceContent.Language.ISO6393Code,
+                LanguageEnglishDisplayName = x.ResourceContent.Language.EnglishDisplay
+            })
+            .ToListAsync(ct);
     }
+
+    private record ResourceContentQueryResponse(int ResourceId, int LanguageId);
 }
--- src/Aquifer.Public.API/Endpoints/Resources/Get/AvailableLanguages/Response.cs
+++ src/Aquifer.Public.API/Endpoints/Resources/Get/AvailableLanguages/Response.cs
@@ -2,5 +2,10 @@
 
 public class Response
 {
-    
+    public int ContentId { get; set; }
+    public string ContentDisplayName { get; set; } = null!;
+    public int LanguageId { get; set; }
+    public string LanguageDisplayName { get; set; } = null!;
+    public string LanguageEnglishDisplayName { get; set; } = null!;
+    public string LanguageCode { get; set; } = null!;
 }
