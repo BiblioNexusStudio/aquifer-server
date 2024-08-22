97120fb985378fa119facdf54a575882c33dc40d
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-08-22 12:07:57 -0600

    add endpoints for creating and deleting resource references

---
+++ src/Aquifer.API/Endpoints/Resources/ResourceReferences/Delete/Endpoint.cs
@@ -0,0 +1,48 @@
+using Aquifer.API.Common;
+using Aquifer.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Resources.ResourceReferences.Delete;
+
+public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request>
+{
+    public override void Configure()
+    {
+        Delete("/resources/resource-references");
+        Permissions(PermissionName.EditContentResourceReferences);
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken ct)
+    {
+        var resourceContent = await dbContext.ResourceContents
+            .Include(rc => rc.Resource)
+            .ThenInclude(r => r.AssociatedResourceChildren)
+            .SingleOrDefaultAsync(rc => rc.Id == request.ResourceContentId, ct);
+
+        if (resourceContent == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        var referenceResource = await dbContext.Resources.FindAsync([request.ReferenceResourceId, ct], ct);
+
+        if (referenceResource == null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        var associatedResource = resourceContent.Resource.AssociatedResourceChildren
+            .FirstOrDefault(r => r.Id == request.ReferenceResourceId);
+
+        if (associatedResource != null)
+        {
+            resourceContent.Resource.AssociatedResourceChildren.Remove(associatedResource);
+        }
+
+        await dbContext.SaveChangesAsync(ct);
+        await SendOkAsync(ct);
+    }
+}
