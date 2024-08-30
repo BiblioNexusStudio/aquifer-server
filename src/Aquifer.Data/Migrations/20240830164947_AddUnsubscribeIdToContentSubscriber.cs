690157f4d0612251198075c66e146f5aa6815b71
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-08-30 13:31:28 -0400

    Create EmailTemplates table, add Enabled and UnsubscribeId columns to ContentSubscriber (#412)

---
+++ src/Aquifer.Data/Migrations/20240830164947_AddUnsubscribeIdToContentSubscriber.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUnsubscribeIdToContentSubscriber : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UnsubscribeId",
+                table: "ContentSubscribers",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UnsubscribeId",
+                table: "ContentSubscribers");
+        }
+    }
+}
