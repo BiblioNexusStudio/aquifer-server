a060288fa6e43268ee72cf36d4220aae7686d7bc
Elisabeth <shakespearee04@gmail.com>
2024-09-04 10:36:49 -0400

    take out try/catch

--- src/Aquifer.API/Endpoints/Marketing/Unsubscribe/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Marketing/Unsubscribe/Endpoint.cs
@@ -1,5 +1,4 @@
-﻿using Aquifer.Common.Utilities;
-using Aquifer.Data;
+﻿using Aquifer.Data;
 using FastEndpoints;
 
 namespace Aquifer.API.Endpoints.Marketing.Unsubscribe;
@@ -14,22 +13,12 @@ public class Endpoint(AquiferDbContext dbContext, ILogger<Endpoint> logger) : En
 
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        try
+        var subscriberToDisable = dbContext.ContentSubscribers.SingleOrDefault(s => s.UnsubscribeId == req.UnsubscribeId);
+        if (subscriberToDisable is not null)
         {
-            var subscriberToDisable = dbContext.ContentSubscribers.SingleOrDefault(s => s.UnsubscribeId == req.UnsubscribeId);
-            if (subscriberToDisable is not null)
-            {
-                subscriberToDisable.Enabled = false;
-                await dbContext.SaveChangesAsync(ct);
-            }
+            subscriberToDisable.Enabled = false;
+            await dbContext.SaveChangesAsync(ct);
         }
-        catch(Exception e)
-        {
-            logger.LogError(e, "Failed to handle subscriber request: {requestContent}", JsonUtilities.DefaultSerialize(req));
-        }
-        finally
-        {
-            await SendOkAsync("You have been successfully unsubscribed.", ct);
-        }
+        await SendOkAsync("You have been successfully unsubscribed.", ct);
     }
 }
