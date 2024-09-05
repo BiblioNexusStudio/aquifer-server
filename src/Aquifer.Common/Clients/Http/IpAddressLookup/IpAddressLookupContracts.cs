0dae9fdc7882887eaa19c197f0d5f45b0f017f7b
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-09-05 11:21:09 -0400

    Save IP location data in request handler (#416)

---
+++ src/Aquifer.Common/Clients/Http/IpAddressLookup/IpAddressLookupContracts.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Aquifer.Common.Clients.Http.IpAddressLookup;
+
+public class IpAddressLookupResponse
+{
+    public string? City { get; set; }
+    public string? Region { get; set; }
+
+    [JsonPropertyName("country_name")]
+    public string? Country { get; set; }
+}
---
+++ src/Aquifer.Common/Clients/Http/IpAddressLookup/IpAddressLookupHttpClient.cs
@@ -0,0 +1,29 @@
+﻿using Aquifer.Common.Utilities;
+
+namespace Aquifer.Common.Clients.Http.IpAddressLookup;
+
+public interface IIpAddressLookupHttpClient
+{
+    Task<IpAddressLookupResponse> LookupIpAddressAsync(string ipAddress, CancellationToken ct);
+}
+
+public class IpAddressLookupHttpClient : IIpAddressLookupHttpClient
+{
+    private const string BaseUri = "https://ipapi.co";
+    private readonly HttpClient _httpClient;
+
+    public IpAddressLookupHttpClient(HttpClient httpClient)
+    {
+        _httpClient = httpClient;
+        _httpClient.BaseAddress = new Uri(BaseUri);
+        // request will fail without User-Agent
+        _httpClient.DefaultRequestHeaders.Add("User-Agent", "bn-user");
+    }
+
+    public async Task<IpAddressLookupResponse> LookupIpAddressAsync(string ipAddress, CancellationToken ct)
+    {
+        var response = await _httpClient.GetAsync($"{ipAddress}/json", ct);
+        var responseContent = await response.Content.ReadAsStringAsync(ct);
+        return JsonUtilities.DefaultDeserialize<IpAddressLookupResponse>(responseContent);
+    }
+}
