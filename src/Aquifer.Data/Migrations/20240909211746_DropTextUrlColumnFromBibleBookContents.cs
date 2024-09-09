37cff0ed9575c975dc07cafa3e3c65c358dc4164
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-09-09 15:18:47 -0600

    drop TextUrl column from BibleBookContents table

---
+++ src/Aquifer.Data/Migrations/20240909211746_DropTextUrlColumnFromBibleBookContents.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DropTextUrlColumnFromBibleBookContents : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TextUrl",
+                table: "BibleBookContents");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TextUrl",
+                table: "BibleBookContents",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
