f1af02f08bee338ab43bb26b7e126a7b54941e00
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-09-13 14:03:24 -0600

    add display name to machine translations table and endpoint

--- src/Aquifer.API/Endpoints/Resources/Content/MachineTranslation/Create/Validator.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/MachineTranslation/Create/Validator.cs
@@ -8,6 +8,8 @@ public class Validator : Validator<Request>
     public Validator()
     {
         RuleFor(x => x.ResourceContentVersionId).GreaterThan(0);
+        // TODO: enable this after the deploy so we don't break translations for users with open pages
+        // RuleFor(x => x.DisplayName).NotEmpty();
         RuleFor(x => x.Content).NotEmpty();
         RuleFor(x => x.SourceId).IsInEnum();
     }
