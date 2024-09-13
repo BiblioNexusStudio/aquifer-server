41192bef61b9b1f133fc785358f8bc3044e2ae10
Jesse Winston <jesse.winston@biblionexus.org>
2024-09-13 12:13:26 -0400

    Fix long loading times on Projects page, but makes the others a bit slower, because they don't have so many items so the extra joins actually make it longer.

--- src/Aquifer.API/Endpoints/Projects/List/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Projects/List/Endpoint.cs
@@ -1,6 +1,7 @@
 ﻿using Aquifer.API.Common;
 using Aquifer.API.Services;
 using Aquifer.Data;
+using Aquifer.Data.Entities;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 
@@ -25,28 +26,37 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService) : En
         var self = await userService.GetUserFromJwtAsync(ct);
 
         return await dbContext.Projects
-            .Where(x =>
-                (x.ActualPublishDate == null || x.ActualPublishDate.Value > DateOnly.FromDateTime(DateTime.UtcNow).AddDays(-30)) &&
+            .Where(x => (x.ActualPublishDate == null || x.ActualPublishDate.Value > DateOnly.FromDateTime(DateTime.UtcNow).AddDays(-30)) &&
                 (userService.HasPermission(PermissionName.ReadProject) ||
-                    (userService.HasPermission(PermissionName.ReadProjectsInCompany) && self.CompanyId == x.CompanyId))).Select(x =>
-                new Response
+                    (userService.HasPermission(PermissionName.ReadProjectsInCompany) && self.CompanyId == x.CompanyId)))
+            .Select(x => new Response
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Company = x.Company.Name,
+                Language = x.Language.EnglishDisplay,
+                ProjectPlatform = x.ProjectPlatform.Name,
+                ProjectLead = $"{x.ProjectManagerUser.FirstName} {x.ProjectManagerUser.LastName}",
+                Manager = x.CompanyLeadUser != null ? $"{x.CompanyLeadUser.FirstName} {x.CompanyLeadUser.LastName}" : null,
+                Resource = x.ResourceContents.First().Resource.ParentResource.DisplayName,
+                ItemCount = x.ResourceContents.Count,
+                WordCount = x.SourceWordCount,
+                IsStarted = x.Started != null,
+                Days =
+                    x.ProjectedDeliveryDate.HasValue
+                        ? x.ProjectedDeliveryDate.Value.DayNumber - DateOnly.FromDateTime(DateTime.UtcNow).DayNumber
+                        : null,
+                Counts = new ProjectResourceStatusCounts
                 {
-                    Id = x.Id,
-                    Name = x.Name,
-                    Company = x.Company.Name,
-                    Language = x.Language.EnglishDisplay,
-                    ProjectPlatform = x.ProjectPlatform.Name,
-                    ProjectLead = $"{x.ProjectManagerUser.FirstName} {x.ProjectManagerUser.LastName}",
-                    Manager = x.CompanyLeadUser != null ? $"{x.CompanyLeadUser.FirstName} {x.CompanyLeadUser.LastName}" : null,
-                    Resource = x.ResourceContents.First().Resource.ParentResource.DisplayName,
-                    ItemCount = x.ResourceContents.Count,
-                    WordCount = x.SourceWordCount,
-                    IsStarted = x.Started != null,
-                    Days =
-                        x.ProjectedDeliveryDate.HasValue
-                            ? x.ProjectedDeliveryDate.Value.DayNumber - DateOnly.FromDateTime(DateTime.UtcNow).DayNumber
-                            : null,
-                    Counts = new ProjectResourceStatusCounts(x.ResourceContents)
-                }).ToListAsync(ct);
+                    NotStarted = x.ResourceContents.Count(rc => ProjectResourceStatusCounts.NotStartedStatuses.Contains(rc.Status)),
+                    InProgress = x.ResourceContents.Count(rc => ProjectResourceStatusCounts.InProgressStatuses.Contains(rc.Status)),
+                    InManagerReview =
+                        x.ResourceContents.Count(rc => ProjectResourceStatusCounts.InManagerReviewStatuses.Contains(rc.Status)),
+                    InPublisherReview =
+                        x.ResourceContents.Count(rc => ProjectResourceStatusCounts.InPublisherReviewStatuses.Contains(rc.Status)),
+                    Completed = x.ResourceContents.Count(rc => rc.Status == ResourceContentStatus.Complete)
+                }
+            })
+            .ToListAsync(ct);
     }
 }
