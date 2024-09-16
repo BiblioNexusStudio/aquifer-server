48940c5a5abc8e4ef15950d289345965723e1313
Nate Merritt <nate.merritt@biblionexus.org>
2024-09-16 14:38:41 -0400

    Add List Bibles endpoint.
    Bibles list squash

---
+++ src/Aquifer.Public.API/Endpoints/Bibles/List/Endpoint.cs
@@ -0,0 +1,49 @@
+﻿using Aquifer.Data;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.Public.API.Endpoints.Bibles.List;
+
+public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, IReadOnlyList<Response>>
+{
+    private readonly AquiferDbContext _dbContext = dbContext;
+
+    public override void Configure()
+    {
+        Get("/bibles");
+        Description(d => d.ProducesProblemFE());
+        Summary(s =>
+        {
+            s.Summary = "Get a list of Bibles.";
+            s.Description =
+                "For a given optional language ID, returns the Bibles in the system including language information, abbreviation codes, whether or not the Bible supports a Greek alignment, and license information.";
+        });
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken ct)
+    {
+        var bibles = await _dbContext.Bibles
+            .Where(b =>
+                b.Enabled &&
+                !b.RestrictedLicense &&
+                (!request.LanguageId.HasValue || b.LanguageId == request.LanguageId) &&
+                (request.LanguageCode == null || b.Language.ISO6393Code == request.LanguageCode) &&
+                (!request.IsLanguageDefault.HasValue || b.LanguageDefault == request.IsLanguageDefault) &&
+                (!request.HasGreekAlignment.HasValue || b.GreekAlignment == request.HasGreekAlignment) &&
+                (!request.HasAudio.HasValue || b.BibleBookContents.Any(bbc => bbc.AudioUrls != null) == request.HasAudio))
+            .Select(bible => new Response
+            {
+                Name = bible.Name,
+                Abbreviation = bible.Abbreviation,
+                Id = bible.Id,
+                SerializedLicenseInfo = bible.LicenseInfo,
+                LanguageId = bible.LanguageId,
+                IsLanguageDefault = bible.LanguageDefault,
+                HasGreekAlignment = bible.GreekAlignment,
+                HasAudio = bible.BibleBookContents.Any(bbc => bbc.AudioUrls != null),
+            })
+            .ToListAsync(ct);
+
+        await SendOkAsync(bibles, ct);
+    }
+}
---
+++ src/Aquifer.Public.API/Endpoints/Bibles/List/Validator.cs
@@ -0,0 +1,24 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Aquifer.Public.API.Endpoints.Bibles.List;
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.LanguageId)
+            .Empty()
+            .When(x => x.LanguageCode is not null)
+            .WithMessage(x => "'{PropertyName}' must be empty when 'Language Code' is specified.");
+        RuleFor(x => x.LanguageCode)
+            .Empty()
+            .When(x => x.LanguageId is not null)
+            .WithMessage(x => "'{PropertyName}' must be empty when 'Language Id' is specified.");
+
+        RuleFor(x => x.LanguageId)
+            .InclusiveBetween(1, 32);
+        RuleFor(x => x.LanguageCode)
+            .Length(3);
+    }
+}
