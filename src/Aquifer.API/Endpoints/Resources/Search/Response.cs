05c7f4f77758971e1c148f549e7e3d8364216d27
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-09-16 11:16:23 -0600

    add sort order to library search endpoint (#437)

--- src/Aquifer.API/Endpoints/Resources/Search/Response.cs
+++ src/Aquifer.API/Endpoints/Resources/Search/Response.cs
@@ -3,11 +3,13 @@ namespace Aquifer.API.Endpoints.Resources.Search;
 public record Response
 {
     public required int Id { get; set; }
+
     // If the resource is a non-text media type (like Audio) this id helps link it to its text counterpart
     public required int? DependentOnId { get; set; }
     public required string DisplayName { get; set; }
     public required string MediaType { get; set; }
     public required int ParentResourceId { get; set; }
     public required int Version { get; set; }
+    public required int SortOrder { get; set; }
     public required string ResourceType { get; set; }
 }
