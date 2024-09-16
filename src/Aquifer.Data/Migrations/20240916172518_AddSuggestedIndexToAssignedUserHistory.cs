f3272aa7a58981526a003461635656ce32ede197
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-09-16 11:34:09 -0600

    add suggested index to assigned user history (#439)

---
+++ src/Aquifer.Data/Migrations/20240916172518_AddSuggestedIndexToAssignedUserHistory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSuggestedIndexToAssignedUserHistory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceContentVersionAssignedUserHistory_ChangedByUserId_Created",
+                table: "ResourceContentVersionAssignedUserHistory",
+                columns: new[] { "ChangedByUserId", "Created" })
+                .Annotation("SqlServer:Include", new[] { "ResourceContentVersionId" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResourceContentVersionAssignedUserHistory_ChangedByUserId_Created",
+                table: "ResourceContentVersionAssignedUserHistory");
+        }
+    }
+}
