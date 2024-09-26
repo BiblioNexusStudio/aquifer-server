8a3116753dfe546c52c7220f56fd985ce694b992
kaseywright <kasey.wright@biblionexus.org>
2024-09-26 15:51:48 -0400

    Ensure that snapshots and assigned user history is recorded for community user translations creation

--- src/Aquifer.API/Endpoints/Resources/Content/CreateTranslation/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/CreateTranslation/Endpoint.cs
@@ -100,6 +100,11 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService, IRes
             ct
         );
 
+        if (isCommunityUser) {
+            await historyService.AddSnapshotHistoryAsync(newResourceContentVersion, user.Id, ResourceContentStatus.TranslationInProgress, ct);
+            await historyService.AddAssignedUserHistoryAsync(newResourceContentVersion, user.Id, user.Id, ct);
+        }
+
         await dbContext.SaveChangesAsync(ct);
         await SendOkAsync(new Response(newResourceContent.Id), ct);
     }
