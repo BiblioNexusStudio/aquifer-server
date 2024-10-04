343ab1a001457264d3b609cddb6fa5556020dfaf
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-10-04 15:09:39 -0600

    add AcceptsCompany option to reports table

---
+++ src/Aquifer.Data/Migrations/20241004205123_AddAcceptsCompanyToReportsTable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAcceptsCompanyToReportsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AcceptsCompany",
+                table: "Reports",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AcceptsCompany",
+                table: "Reports");
+        }
+    }
+}
