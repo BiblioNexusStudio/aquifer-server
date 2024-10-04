129186f53f83dbacb929184d03fed2c188e85e70
Nate Merritt <nate.merritt@biblionexus.org>
2024-10-04 10:58:40 -0400

    Add missing indices for greek alignment query (#466)

---
+++ src/Aquifer.Data/Migrations/20241004145335_AddMissingIndicesForGreekAlignmentQuery.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMissingIndicesForGreekAlignmentQuery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_BibleVersionWordGroupWords_BibleVersionWordId",
+                table: "BibleVersionWordGroupWords",
+                column: "BibleVersionWordId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_BibleVersionWordGroupWords_BibleVersionWordId",
+                table: "BibleVersionWordGroupWords");
+        }
+    }
+}
