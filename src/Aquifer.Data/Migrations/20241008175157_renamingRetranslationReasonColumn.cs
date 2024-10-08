e0b1c6135a3dadc0fad5e801df5a180563d83515
Carlos Morales <carlos.morales@biblionexus.org>
2024-10-08 13:52:44 -0400

    renamingRetranslationReasonColumn

---
+++ src/Aquifer.Data/Migrations/20241008175157_renamingRetranslationReasonColumn.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class renamingRetranslationReasonColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ReasonForReTranslation",
+                table: "ResourceContentVersionMachineTranslations",
+                newName: "RetranslationReason");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "RetranslationReason",
+                table: "ResourceContentVersionMachineTranslations",
+                newName: "ReasonForReTranslation");
+        }
+    }
+}
