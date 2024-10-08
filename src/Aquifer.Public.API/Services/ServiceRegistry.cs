7840eec6d16061bdaa5df3372958e021cb2cbb02
Nate Merritt <nate.merritt@biblionexus.org>
2024-10-08 14:23:31 -0400

    Improve public API resource search route performance. (#471)

---
+++ src/Aquifer.Public.API/Services/ServiceRegistry.cs
@@ -0,0 +1,13 @@
+﻿using Aquifer.Common.Services.Caching;
+
+namespace Aquifer.Public.API.Services;
+
+public static class ServiceRegistry
+{
+    public static IServiceCollection AddCachingServices(this IServiceCollection services)
+    {
+        services.AddScoped<ICachingLanguageService, CachingLanguageService>();
+
+        return services;
+    }
+}
