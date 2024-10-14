49124f96c85ea15c7ac6fbfa76ea96649dac6617
kaseywright <kasey.wright@biblionexus.org>
2024-10-14 10:50:30 -0400

    Add HelpDocumentEntity

---
+++ src/Aquifer.Data/Entities/HelpDocumentEntity.cs
@@ -0,0 +1,24 @@
+
+
+namespace Aquifer.Data.Entities;
+
+public class HelpDocumentEntity
+{
+    public int Id { get; set; }
+    public required HelpDocumentType Type { get; set; }
+    public required string Title { get; set; }
+    public required string Url { get; set; }
+    
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; }
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Updated { get; set; }
+}
+
+public enum HelpDocumentType
+{
+    None = 0,
+    Release = 1,
+    HowTo = 2
+}
