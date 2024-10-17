b5efdd42765c9c0e6f77a012e516487a96eb90d5
kaseywright <kasey.wright@biblionexus.org>
2024-10-17 10:51:30 -0400

    Sort how to docuemnts alphabetically by title.

--- src/Aquifer.API/Endpoints/Help/AquiferCms/Documents/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Help/AquiferCms/Documents/Endpoint.cs
@@ -20,7 +20,9 @@ public class Endpoint(AquiferDbContext dbContext) : EndpointWithoutRequest<Respo
         var response = new Response
         {
             Releases = helpDocuments.Where(x => x.Type == HelpDocumentType.Release),
-            HowTos = helpDocuments.Where(x => x.Type == HelpDocumentType.HowTo)
+            HowTos = helpDocuments
+                .Where(x => x.Type == HelpDocumentType.HowTo)
+                .OrderBy(x => x.Title)
         };
         await SendOkAsync(response, ct);
     }
