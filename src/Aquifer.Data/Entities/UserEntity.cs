5420f32e986c157aa5cdddedd3129420e07cf9c0
Nate Merritt <nate.merritt@biblionexus.org>
2024-10-23 10:17:22 -0400

    Create migration to add AquiferNotificationsEnabled column to Users table.

--- src/Aquifer.Data/Entities/UserEntity.cs
+++ src/Aquifer.Data/Entities/UserEntity.cs
@@ -16,7 +16,7 @@ public class UserEntity : IHasUpdatedTimestamp
     public bool EmailVerified { get; set; }
     public UserRole Role { get; set; }
     public bool Enabled { get; set; }
-    public bool AquiferNotificationsEnabled { get; set; } = true;
+    public bool AquiferNotificationsEnabled { get; set; }
 
     public int CompanyId { get; set; }
     public CompanyEntity Company { get; set; } = null!;
@@ -36,6 +36,7 @@ public class UserEntityConfiguration : IEntityTypeConfiguration<UserEntity>
     public void Configure(EntityTypeBuilder<UserEntity> builder)
     {
         builder.Property(u => u.Enabled).HasDefaultValue(true);
+        builder.Property(u => u.AquiferNotificationsEnabled).HasDefaultValue(true);
     }
 }
 
