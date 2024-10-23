128c26e7ffb4f74ab28e375ceb04d7cc9394ed38
Nate Merritt <nate.merritt@biblionexus.org>
2024-10-23 09:59:06 -0400

    Create JobHistory table migration.

--- src/Aquifer.Common/Jobs/QueueClientFactory.cs
+++ src/Aquifer.Common/Jobs/QueueClientFactory.cs
@@ -31,7 +31,7 @@ public sealed class QueueClientFactory(QueueConfigurationOptions _queueConfigura
         {
             return await queueClient.Value;
         }
-        catch (Exception e)
+        catch (Exception)
         {
             // ensure the dictionary doesn't get stuck with a faulted task
             _queueClientByQueueNameMap.TryRemove(queueName, out _);
