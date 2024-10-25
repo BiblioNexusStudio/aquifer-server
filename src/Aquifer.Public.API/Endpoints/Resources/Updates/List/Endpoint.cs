ecd3030ae68e27cd9f9cced63886218f60dc3818
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-10-25 14:16:04 -0400

    add limit/offset, resourceCollectionCode and LanguageCode params (#502)

--- src/Aquifer.Public.API/Endpoints/Resources/Updates/List/Endpoint.cs
+++ src/Aquifer.Public.API/Endpoints/Resources/Updates/List/Endpoint.cs
@@ -1,10 +1,12 @@
-﻿using Aquifer.Data;
+﻿using Aquifer.Common.Services.Caching;
+using Aquifer.Data;
+using Aquifer.Data.Entities;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 
 namespace Aquifer.Public.API.Endpoints.Resources.Updates.List;
 
-public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, Response>
+public class Endpoint(AquiferDbContext dbContext, ICachingLanguageService cachingLanguageService) : Endpoint<Request, Response>
 {
     public override void Configure()
     {
@@ -16,23 +18,83 @@ public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, Response>
         {
             s.Summary = "Get resource ids that are new or updated since the given UTC timestamp.";
             s.Description =
-                "For a given UTC timestamp and optional language id, get a list of resource ids that are new or have been updated since the provided timestamp. This is intended for users who are storing Aquifer data locally and want to fetch new content.";
+                "For a given UTC timestamp, get a list of resource ids that are new or have been updated since the provided timestamp. This is intended for users who are storing Aquifer data locally and want to fetch new content.";
         });
     }
 
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
-        Response.Items = await dbContext.ResourceContentVersions
-            .Where(x => (!req.LanguageId.HasValue || x.ResourceContent.LanguageId == req.LanguageId) &&
-                x.IsPublished &&
-                x.Updated >= req.Timestamp)
+        var query = await GetQuery(req, ct);
+        var totalCount = await GetTotalResourceCountAsync(req, query, ct);
+
+        if (totalCount == 0)
+        {
+            await SendOkAsync(new Response
+            {
+                Offset = req.Offset
+            }, ct);
+            return;
+        }
+
+        var items = await GetResourceUpdatesAsync(req, query, ct);
+        Response = new Response
+        {
+            Items = items,
+            TotalItemCount = totalCount,
+            Offset = req.Offset
+        };
+    }
+
+    private async Task<List<ResponseContent>> GetResourceUpdatesAsync(Request req, IQueryable<ResourceContentVersionEntity> query,
+        CancellationToken ct)
+    {
+        var languageCodeByIdMap = await cachingLanguageService.GetLanguageCodeByIdMapAsync(ct);
+        return await query
+            .OrderByDescending(r => r.Updated)
+            .Skip(req.Offset)
+            .Take(req.Limit)
             .Select(x => new ResponseContent
             {
                 ResourceId = x.ResourceContentId,
                 LanguageId = x.ResourceContent.LanguageId,
+                LanguageCode = languageCodeByIdMap[x.ResourceContent.LanguageId],
                 UpdateType = x.Version == 1 ? ResponseContentUpdateType.New : ResponseContentUpdateType.Updated,
                 Timestamp = x.Updated
             })
             .ToListAsync(ct);
+    }
+
+    private async Task<int> GetTotalResourceCountAsync(Request req, IQueryable<ResourceContentVersionEntity> query, CancellationToken ct)
+    {
+        var totalCount = await query.CountAsync(ct);
+        if (req.Offset >= totalCount && req.Offset != 0)
+        {
+            ThrowError($"Offset of {req.Offset} equals or exceeds total of {totalCount}");
+        }
+
+        return totalCount;
+    }
+
+    private async Task<IQueryable<ResourceContentVersionEntity>> GetQuery(Request req, CancellationToken ct)
+    {
+        var (isValidLanguageId, isValidLanguageCode, validLanguageId) =
+            await cachingLanguageService.ValidateLanguageIdOrCodeAsync(req.LanguageId, req.LanguageCode, false, ct);
+
+        if (!isValidLanguageCode)
+        {
+            ThrowError(x => x.LanguageCode, $"Invalid LanguageCode:  {req.LanguageCode}");
+        }
+
+        if (!isValidLanguageId)
+        {
+            ThrowError(x => x.LanguageId, $"Invalid LanguageId: {req.LanguageId}");
+        }
+
+        return dbContext.ResourceContentVersions
+            .Where(x => (!validLanguageId.HasValue || x.ResourceContent.LanguageId == validLanguageId) &&
+                        x.IsPublished &&
+                        x.Updated >= req.Timestamp &&
+                        (req.ResourceCollectionCode == null ||
+                         x.ResourceContent.Resource.ParentResource.Code.ToLower() == req.ResourceCollectionCode.ToLower()));
     }
 }
--- src/Aquifer.Public.API/Endpoints/Resources/Updates/List/Request.cs
+++ src/Aquifer.Public.API/Endpoints/Resources/Updates/List/Request.cs
@@ -1,14 +1,39 @@
-﻿namespace Aquifer.Public.API.Endpoints.Resources.Updates.List;
+﻿using System.ComponentModel;
+
+namespace Aquifer.Public.API.Endpoints.Resources.Updates.List;
 
 public class Request
 {
     /// <summary>
     ///     UTC timestamp (e.g. 07/20/2024) indicating how far back to check for updates. The maximum look-back is 90 days.
     /// </summary>
-    public DateTime Timestamp { get; set; }
+    public DateTime Timestamp { get; init; }
 
     /// <summary>
     ///     Optional LanguageId to search on. If none is provided, updates for all languages will be returned.
     /// </summary>
-    public int? LanguageId { get; set; }
+    public int? LanguageId { get; init; }
+
+    /// <summary>
+    ///     Optional ISO 639-3 code that is 3 characters in length (e.g. "eng").
+    /// </summary>
+    public string? LanguageCode { get; init; }
+
+    /// <summary>
+    ///     The number of results to return. Max is 1000.
+    /// </summary>
+    [DefaultValue(1000)]
+    public int Limit { get; init; } = 1000;
+
+    /// <summary>
+    ///     Records to skip if paging through results.
+    /// </summary>
+    [DefaultValue(0)]
+    public int Offset { get; init; }
+
+    /// <summary>
+    ///     Optional resource collection code to search for. Search by code retrieved from /resources/collections endpoint such
+    ///     as "TyndaleBibleDictionary".
+    /// </summary>
+    public string? ResourceCollectionCode { get; init; }
 }
