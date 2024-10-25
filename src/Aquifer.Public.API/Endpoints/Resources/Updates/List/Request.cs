8c86c974e3e4b57b7a4bfd4a007f071da631ccad
Elisabeth <shakespearee04@gmail.com>
2024-10-25 08:44:55 -0400

    use init for consistency

--- src/Aquifer.Public.API/Endpoints/Resources/Updates/List/Request.cs
+++ src/Aquifer.Public.API/Endpoints/Resources/Updates/List/Request.cs
@@ -7,12 +7,12 @@ public class Request
     /// <summary>
     ///     UTC timestamp (e.g. 07/20/2024) indicating how far back to check for updates. The maximum look-back is 90 days.
     /// </summary>
-    public DateTime Timestamp { get; set; }
+    public DateTime Timestamp { get; init; }
 
     /// <summary>
     ///     Optional LanguageId to search on. If none is provided, updates for all languages will be returned.
     /// </summary>
-    public int? LanguageId { get; set; }
+    public int? LanguageId { get; init; }
 
     /// <summary>
     ///     Optional ISO 639-3 code that is 3 characters in length (e.g. "eng").
