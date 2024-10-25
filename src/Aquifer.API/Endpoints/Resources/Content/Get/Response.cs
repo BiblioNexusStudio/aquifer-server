7e265d67b8fb71bc10e06c84877427a9cae2cfb4
kaseywright <kasey.wright@biblionexus.org>
2024-10-25 15:06:16 -0400

    Rervert changes to allow pull back to CompanyReview

--- src/Aquifer.API/Endpoints/Resources/Content/Get/Response.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/Get/Response.cs
@@ -22,6 +22,7 @@ public class Response
     public int ResourceContentVersionId { get; set; }
     public bool IsDraft { get; set; }
     public ResourceContentVersionReviewLevel ReviewLevel { get; set; }
+    public bool CanPullBackToCompanyReview { get; set; }
     public object Content => JsonUtilities.DefaultDeserialize(ContentValue);
     public int ContentSize { get; set; }
     public string DisplayName { get; set; } = null!;
