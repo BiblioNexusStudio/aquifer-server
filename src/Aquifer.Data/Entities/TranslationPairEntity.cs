1c84b510c448527192501dd1a0e98c59540812f6
Carlos Morales <carlos.morales@biblionexus.org>
2024-11-12 11:14:26 -0500

    addressingPrConcerns

--- src/Aquifer.Data/Entities/TranslationPairEntity.cs
+++ src/Aquifer.Data/Entities/TranslationPairEntity.cs
@@ -1,28 +1,16 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
 namespace Aquifer.Data.Entities;
 
-[EntityTypeConfiguration(typeof(TranslationPairEntityConfiguration))]
 public class TranslationPairEntity
 {
     public int Id { get; set; }
-    public required LanguageEntity Language { get; set; }
-    public required int LanguageId { get; set; }
-    public required string Key { get; set; }
-    public required string Value { get; set; }
+    public LanguageEntity Language { get; set; } = null!;
+    public int LanguageId { get; set; }
+    public string Key { get; set; } = null!;
+    public string Value { get; set; } = null!;
 
     [SqlDefaultValue("getutcdate()")]
-    public required DateTime Created { get; set; }
+    public DateTime Created { get; set; }
 
     [SqlDefaultValue("getutcdate()")]
-    public required DateTime Updated { get; set; }
-}
-
-public class TranslationPairEntityConfiguration : IEntityTypeConfiguration<TranslationPairEntity>
-{
-    public void Configure(EntityTypeBuilder<TranslationPairEntity> builder)
-    {
-        builder.HasOne(x => x.Language).WithMany(l => l.TranslationPairs).HasForeignKey(tp => tp.LanguageId);
-    }
+    public DateTime Updated { get; set; }
 }
