b39e776c24eead75594f4b064427c7bf1f3359b1
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-11-14 13:14:35 -0500

    Add unique index for translation pair, up EF tools version (#555)

---
+++ src/Aquifer.Data/Migrations/20241114180837_AddTranslationPairIndex.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTranslationPairIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "TranslationPairs",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TranslationPairs_LanguageId_Key",
+                table: "TranslationPairs",
+                columns: new[] { "LanguageId", "Key" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_TranslationPairs_LanguageId_Key",
+                table: "TranslationPairs");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Key",
+                table: "TranslationPairs",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
