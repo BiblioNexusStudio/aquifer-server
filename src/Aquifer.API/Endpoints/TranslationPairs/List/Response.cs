3e1dc5ae1dbeb3a4d66e5ce9ff260c27770cf2f6
Carlos Morales <carlos.morales@biblionexus.org>
2024-11-15 10:01:58 -0500

    makingLanguageIdAnInt

--- src/Aquifer.API/Endpoints/TranslationPairs/List/Response.cs
+++ src/Aquifer.API/Endpoints/TranslationPairs/List/Response.cs
@@ -2,7 +2,7 @@ namespace Aquifer.API.Endpoints.TranslationPairs.List;
 
 public class Response
 {
-    public string LanguageId { get; set; } = null!;
+    public int LanguageId { get; set; }
     public int TranslationPairId { get; set; }
     public string LanguageEnglishDisplay { get; set; } = null!;
     public string TranslationPairKey { get; set; } = null!;
