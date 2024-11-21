98a28a15803406a362d5ca9465f7c6d5e040bec2
Jesse Winston <jesse.winston@biblionexus.org>
2024-11-21 12:25:52 -0500

    Add handler for bad translation pair keys

---
+++ src/Aquifer.Data/EventHandlers/BadTranslationPairHandler.cs
@@ -0,0 +1,25 @@
+﻿using Aquifer.Data.Entities;
+using Aquifer.Data.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Aquifer.Data.EventHandlers;
+
+public static class BadTranslationPairHandler
+{
+    private static readonly IReadOnlyList<string> s_KeyBlacklist = ["span", "div"];
+
+    public static void Handle(IEnumerable<EntityEntry> entityEntries)
+    {
+        entityEntries.Where(entry => entry is { Entity: TranslationPairEntity, State: EntityState.Modified or EntityState.Added })
+            .Select(x => x.Entity as TranslationPairEntity)
+            .ToList()
+            .ForEach(x =>
+            {
+                if (s_KeyBlacklist.Contains(x?.Key))
+                {
+                    throw new TranslationPairKeyNotAllowedException();
+                }
+            });
+    }
+}
