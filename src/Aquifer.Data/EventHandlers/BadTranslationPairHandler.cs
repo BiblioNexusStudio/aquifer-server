297d044fcd250bdc3d34c96b38303ec8e295b5dd
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-11-21 13:07:04 -0500

    Add handler for bad translation pair keys (#579)
    Originally was going to use an interceptor (hence the branch name), but
    just adding to the saving event was simpler. Noticed that the other
    method was misnamed, and it runs after the records are already saved (so
    that was a nice waste of time).

---
+++ src/Aquifer.Data/EventHandlers/BadTranslationPairHandler.cs
@@ -0,0 +1,35 @@
+﻿using Aquifer.Data.Entities;
+using Aquifer.Data.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Aquifer.Data.EventHandlers;
+
+public static class BadTranslationPairHandler
+{
+    private static readonly IReadOnlySet<string> s_KeyBlacklist = new HashSet<string>
+    {
+        "span",
+        "div"
+    };
+
+    public static void Handle(IEnumerable<EntityEntry> entityEntries)
+    {
+        entityEntries.Where(entry => entry is { Entity: TranslationPairEntity, State: EntityState.Modified or EntityState.Added })
+            .Select(x => x.Entity as TranslationPairEntity)
+            .ToList()
+            .ForEach(x =>
+            {
+                if (x is null)
+                {
+                    return;
+                }
+
+                if (x.Key.Length < 3 || s_KeyBlacklist.Contains(x.Key))
+                {
+                    throw new TranslationPairKeyNotAllowedException(
+                        $"The key ${x.Key} is not allowed. Keys 2 or fewer characters and some specific keys are not allowed.");
+                }
+            });
+    }
+}
