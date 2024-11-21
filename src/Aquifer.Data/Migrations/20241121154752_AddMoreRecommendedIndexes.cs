14d50d5baca85db9e52ea10e99b2395d3ae8f31d
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-11-21 08:50:14 -0700

    add more recommended indexes (#575)

---
+++ src/Aquifer.Data/Migrations/20241121154752_AddMoreRecommendedIndexes.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoreRecommendedIndexes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceContentVersionAssignedUserHistory_Created",
+                table: "ResourceContentVersionAssignedUserHistory",
+                column: "Created")
+                .Annotation("SqlServer:Include", new[] { "AssignedUserId", "ResourceContentVersionId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_ThreadId",
+                table: "Comments",
+                column: "ThreadId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResourceContentVersionAssignedUserHistory_Created",
+                table: "ResourceContentVersionAssignedUserHistory");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_ThreadId",
+                table: "Comments");
+        }
+    }
+}
