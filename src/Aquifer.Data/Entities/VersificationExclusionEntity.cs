6418fd2f5c377817ea83c18d71aeae4af5cf1e26
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-12-06 14:34:41 -0500

    Add tables for versification (#592)

---
+++ src/Aquifer.Data/Entities/VersificationExclusionEntity.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.Data.Entities;
+
+[PrimaryKey(nameof(BibleId), nameof(BibleVerseId))]
+public class VersificationExclusionEntity
+{
+    public int BibleId { get; set; }
+    public int BibleVerseId { get; set; }
+    public BibleEntity Bible { get; set; } = null!;
+    public VerseEntity Verse { get; set; } = null!;
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+}
