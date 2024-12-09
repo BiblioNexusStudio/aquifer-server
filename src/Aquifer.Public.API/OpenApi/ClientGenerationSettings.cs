b563802fb493636c6d466b1ab62a58eebbceb91a
Nate Merritt <nate.merritt@biblionexus.org>
2024-12-09 16:35:32 -0500

    Update client generation settings.

--- src/Aquifer.Public.API/OpenApi/ClientGenerationSettings.cs
+++ src/Aquifer.Public.API/OpenApi/ClientGenerationSettings.cs
@@ -3,6 +3,11 @@ using Kiota.Builder;
 
 namespace Aquifer.Public.API.OpenApi;
 
+/// <summary>
+/// Generate client code and make it available as a zip file per language at `/clients/language-id`.
+/// See https://fast-endpoints.com/docs/swagger-support#api-client-generation.
+/// We could also generate clients from the command line or on build if desired.
+/// </summary>
 public static class ClientGenerationSettings
 {
     public static IEndpointRouteBuilder ConfigureClientGeneration(
@@ -16,9 +21,10 @@ public static class ClientGenerationSettings
             {
                 c.SwaggerDocumentName = swaggerDocumentName;
                 c.Language = GenerationLanguage.CSharp;
-                c.ClientNamespaceName = "BiblioNexus.Aquifer";
+                c.ClientNamespaceName = "BiblioNexus.Aquifer.API.Client";
                 c.ClientClassName = "AquiferClient";
                 c.OutputPath = Path.Combine(c.OutputPath, "cs");
+                c.CleanOutput = true;
             },
             o =>
             {
@@ -32,9 +38,10 @@ public static class ClientGenerationSettings
             {
                 c.SwaggerDocumentName = swaggerDocumentName;
                 c.Language = GenerationLanguage.Java;
-                c.ClientNamespaceName = "org.biblionexus.aquifer";
+                c.ClientNamespaceName = "org.biblionexus.aquifer.api.client";
                 c.ClientClassName = "AquiferClient";
                 c.OutputPath = Path.Combine(c.OutputPath, "java");
+                c.CleanOutput = true;
             },
             o =>
             {
@@ -48,9 +55,10 @@ public static class ClientGenerationSettings
             {
                 c.SwaggerDocumentName = swaggerDocumentName;
                 c.Language = GenerationLanguage.Python;
-                c.ClientNamespaceName = "biblionexus_aquifer";
+                c.ClientNamespaceName = "biblionexus_aquifer_api_client";
                 c.ClientClassName = "AquiferClient";
                 c.OutputPath = Path.Combine(c.OutputPath, "py");
+                c.CleanOutput = true;
             },
             o =>
             {
@@ -64,9 +72,10 @@ public static class ClientGenerationSettings
             {
                 c.SwaggerDocumentName = swaggerDocumentName;
                 c.Language = GenerationLanguage.TypeScript;
-                c.ClientNamespaceName = "biblionexus-aquifer";
+                c.ClientNamespaceName = "biblionexus-aquifer-api-client";
                 c.ClientClassName = "AquiferClient";
                 c.OutputPath = Path.Combine(c.OutputPath, "ts");
+                c.CleanOutput = true;
             },
             o =>
             {
