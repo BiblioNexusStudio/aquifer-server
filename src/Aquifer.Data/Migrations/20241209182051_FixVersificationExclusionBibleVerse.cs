1e2dbb1021c145d726f70bb36db1bb2814bd1543
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-12-09 13:56:10 -0500

    Fix BibleVerse/VerseId problem (#596)

---
+++ src/Aquifer.Data/Migrations/20241209182051_FixVersificationExclusionBibleVerse.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixVersificationExclusionBibleVerse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VersificationExclusions_Verses_VerseId",
+                table: "VersificationExclusions");
+
+            migrationBuilder.DropColumn(
+                name: "VerseId",
+                table: "VersificationExclusions");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VersificationExclusions_Verses_BibleVerseId",
+                table: "VersificationExclusions",
+                column: "BibleVerseId",
+                principalTable: "Verses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_VersificationExclusions_Verses_BibleVerseId",
+                table: "VersificationExclusions");
+
+            migrationBuilder.AddColumn<int>(
+                name: "VerseId",
+                table: "VersificationExclusions",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_VersificationExclusions_Verses_VerseId",
+                table: "VersificationExclusions",
+                column: "VerseId",
+                principalTable: "Verses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
