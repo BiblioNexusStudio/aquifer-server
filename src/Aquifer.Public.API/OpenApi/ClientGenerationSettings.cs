d89225775db8541581f0e0e28b90321f420b17ee
Nate Merritt <nate.merritt@biblionexus.org>
2024-12-11 17:57:21 -0500

    Add TypeScript example of generated client usage.

--- src/Aquifer.Public.API/OpenApi/ClientGenerationSettings.cs
+++ src/Aquifer.Public.API/OpenApi/ClientGenerationSettings.cs
@@ -40,7 +40,8 @@ public static class ClientGenerationSettings
 
                     See also [this C# Kiota example](https://github.com/microsoft/kiota-samples/blob/main/get-started/quickstart/dotnet/src/Program.cs) for how to use a generated Kiota client.
                     
-                    Full example of a `Program.cs` file using the generated client and injecting an API key header:
+                    Full example of a `Program.cs` file using the generated client and injecting an API key header.
+                    The example code assumes that the downloaded client code files are contained in the same solution in the `BiblioNexus.Aquifer.API.Client` namespace.
                     ```
                     using System.Net.Http;
                     using BiblioNexus.Aquifer.API.Client;
@@ -55,14 +56,14 @@ public static class ClientGenerationSettings
                         new AnonymousAuthenticationProvider(),
                         httpClient: new HttpClient(httpMessageHandler!))
                     {
-                        BaseUrl = GetAquiferPublicApiBaseUri(environment),
+                        BaseUrl = "https://api.aquifer.bible",
                     };
                     
                     var aquiferClient = new AquiferClient(adapter);
                     
                     try
                     {
-                        // GET /biblebooks
+                        // GET /bibles/books
                         var bibleBooks = await aquiferClient.Bibles.Books.GetAsync();
                         
                         Console.WriteLine($"Retrieved {bibleBooks?.Count} Bible books.");
@@ -75,22 +76,49 @@ public static class ClientGenerationSettings
                         }))
                         ?.SingleOrDefault();
                         
-                        Console.WriteLine($"The English language default Bible is \"{englishDefaultBible?.Name}\" ({englishDefaultBible?.Abbreviation})");
+                        Console.WriteLine($"The English language default Bible is \"{englishDefaultBible?.Name}\" ({englishDefaultBible?.Abbreviation}).");
                         
                         // GET /resources/{id}
-                        var resource = await aquiferClient.Resources[1717].GetAsync();
+                        var resource = await aquiferClient.Resources[1717].GetAsync(b =>
+                        {
+                            b.QueryParameters.ContentTextType = "html";
+                        });
                         
                         Console.WriteLine($"""
                             Retrieved Resource:
-                              - ID: {resource?.Id}
-                              - Name: {resource?.Name}
-                              - Content: {await GetUntypedNodeOriginalJsonAsync(resource.Content!)}
+                              - ID: {resource!.Id}
+                              - Name: {resource.Name}
+                              - Content: {await GetUntypedNodeOriginalJsonAsync(resource.Content)}
                             """);
+                    }
+                    catch (ErrorResponse ex)
+                    {
+                        Console.WriteLine($"Error: {ex.Message}");
+                        Console.WriteLine($"Status Code: {ex.ResponseStatusCode}");
+                        if (ex.Errors != null)
+                        {
+                            foreach (var additionalData in ex.Errors.AdditionalData)
+                            {
+                                Console.WriteLine($"\"{additionalData.Key}\": \"{await GetAdditionalDataValueAsStringAsync(additionalData.Value)}\"");
+                            }
+                        }
+                        Console.WriteLine(ex.StackTrace);
                     }
                     catch (Exception ex)
                     {
-                        Console.WriteLine($"ERROR: {ex.Message}");
+                        Console.WriteLine($"Error: {ex.Message}");
                         Console.WriteLine(ex.StackTrace);
+                    }
+                    
+                    private static async Task<string?> GetAdditionalDataValueAsStringAsync(object value)
+                    {
+                        if (value is UntypedNode untypedNode)
+                        {
+                            return await GetUntypedNodeOriginalJsonAsync(untypedNode);
+                        }
+                        
+                        // TODO use your JSON serialization strategy of choice to render a better string
+                        return value.ToString();
                     }
                     
                     private static async Task<string> GetUntypedNodeOriginalJsonAsync(UntypedNode untypedNode)
@@ -98,7 +126,7 @@ public static class ClientGenerationSettings
                         return await KiotaJsonSerializer.SerializeAsStringAsync(untypedNode);
                     }
                     ```
-                    The following class is used above to inject the API Key into the header:
+                    The following class is also used above in order to inject the API Key into every request header:
                     ```
                     public sealed class SetApiKeyHeaderRequestHandler(string _apiKey) : DelegatingHandler
                     {
@@ -192,6 +220,95 @@ public static class ClientGenerationSettings
                       * `npm install @microsoft/kiota-bundle@1.0.0-preview.77 -SE` (or newer version)
 
                     See also [this TypeScript Kiota example](https://github.com/microsoft/kiota-samples/blob/main/get-started/quickstart/typescript/index.ts) for how to use a generated Kiota client.
+                    
+                    The following is a full example of an `index.tx` file using the generated client and injecting an API key header.
+                    The example code assumes that the downloaded client code files are unzipped into a subdirectory named `client`.
+                    ```
+                    import { AnonymousAuthenticationProvider, SerializationWriter, serializeToJsonAsString, serializeUntypedNode, UntypedNode } from "@microsoft/kiota-abstractions";
+                    import { FetchRequestAdapter, KiotaClientFactory, MiddlewareFactory } from "@microsoft/kiota-http-fetchlibrary";
+                    import { createAquiferClient } from "./client/aquiferClient.js";
+                    import { SetApiKeyHeaderRequestHandler } from "./SetApiKeyHeaderRequestHandler.js";
+                    
+                    // see https://learn.microsoft.com/en-us/openapi/kiota/middleware?tabs=typescript
+                    const handlers = MiddlewareFactory.getDefaultMiddlewares();
+                    handlers.unshift(new SetApiKeyHeaderRequestHandler("your-api-key-goes-here"));
+                    const httpClient = KiotaClientFactory.create(undefined, handlers);
+                    const adapter = new FetchRequestAdapter(new AnonymousAuthenticationProvider(), undefined, undefined, httpClient);
+                    adapter.baseUrl = "https://api.aquifer.bible";
+                    
+                    const client = createAquiferClient(adapter);
+                    
+                    async function main(): Promise<void> {
+                      try {
+                        // GET /bibles/books
+                        const bibleBooks = await client.bibles.books.get();
+                        console.log(`Retrieved ${bibleBooks?.length} Bible books.`);
+                    
+                        // GET /bibles
+                        const getBiblesResult = await client.bibles.get({
+                            queryParameters: {
+                                languageCode: "eng",
+                                isLanguageDefault: true,
+                            },
+                        });
+                        const englishLanguageDefaultBible = getBiblesResult?.[0];
+                        console.log(`The English language default Bible is "${englishLanguageDefaultBible?.name}" (${englishLanguageDefaultBible?.abbreviation}).`);
+                    
+                        // GET /resources/{id}
+                        const resource = await client.resources.byContentId(1717).get({
+                            queryParameters: {
+                                contentTextType: "html",
+                            },
+                        });
+                        console.log(`Retrieved Resource:\n  - ID: ${resource?.id}\n  - Name: ${resource?.name}\n  - Content: ${getUntypedNodeOriginalJson(resource?.content!)}.`);
+                    
+                      } catch (error) {
+                        console.log("Error:");
+                        console.log(error);
+                      }
+                    }
+                    
+                    function getUntypedNodeOriginalJson(untypedNode: UntypedNode): string {
+                        const serializeUntypedNodeToJson = (writer: SerializationWriter, value?: Partial<UntypedNode> | null): void => {
+                            serializeUntypedNode(writer, value!);
+                        }
+                        return serializeToJsonAsString(untypedNode, serializeUntypedNodeToJson);
+                    }
+                    
+                    main();
+                    ```
+                    The following class is also used above in order to inject the API Key into every request header:
+                    ```
+                    import { RequestOption } from "@microsoft/kiota-abstractions";
+                    import { FetchRequestInit, Middleware } from "@microsoft/kiota-http-fetchlibrary";
+                    
+                    export class SetApiKeyHeaderRequestHandler implements Middleware {
+                        public constructor(private readonly _apiKey: string) {}
+                    
+                        /** @inheritdoc */
+                        next: Middleware | undefined;
+                    
+                        private setRequestHeader(options: FetchRequestInit | undefined, key: string, value: string): void {
+                            if (options) {
+                                if (!options.headers) {
+                                    options.headers = {};
+                                }
+                                options.headers[key] = value;
+                            }
+                        };
+                    
+                        /** @inheritdoc */
+                        public async execute(url: string, requestInit: RequestInit, requestOptions?: Record<string, RequestOption>): Promise<Response> {
+                            this.setRequestHeader(requestInit as FetchRequestInit, "api-key", this._apiKey);
+                    
+                            const response = await this.next?.execute(url, requestInit, requestOptions);
+                            if (!response) {
+                                throw new Error("No response returned by the next middleware.");
+                            }
+                            return response;
+                        }
+                    }
+                    ```
                     """);
                 o.WithName("AquiferPublicAPIEndpointsClientsGetTsEndpoint");
                 o.WithOpenApi();
