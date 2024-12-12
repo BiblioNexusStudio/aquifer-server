46ad749c1f384b3cbf1377bac763a01df9342516
Nate Merritt <nate.merritt@biblionexus.org>
2024-12-12 12:03:44 -0500

    Shorten AppFixture to App.

--- tests/Aquifer.Public.API.IntegrationTests/Endpoints/Bibles/List/EndpointTests.cs
+++ tests/Aquifer.Public.API.IntegrationTests/Endpoints/Bibles/List/EndpointTests.cs
@@ -5,12 +5,12 @@ using FastEndpoints;
 
 namespace Aquifer.Public.API.IntegrationTests.Endpoints.Bibles.List;
 
-public sealed class EndpointTests(AppFixture _appFixture) : TestBase<AppFixture>
+public sealed class EndpointTests(App _app) : TestBase<App>
 {
     [Fact]
     public async Task ValidRequest_ShouldReturnSuccess()
     {
-        var (response, result) = await _appFixture.Client.GETAsync<Endpoint, Request, IReadOnlyList<Response>>(
+        var (response, result) = await _app.Client.GETAsync<Endpoint, Request, IReadOnlyList<Response>>(
             new Request
             {
                 IsLanguageDefault = true,
