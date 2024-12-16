7f924a633199788c920e400ab19c7f8cda4cb34f
Elisabeth <shakespearee04@gmail.com>
2024-12-16 15:55:26 -0500

    translate mappedVerseId to a startChapter/Verse/bookNumber format before getting the BibleText

--- src/Aquifer.Common/Utilities/BibleTextUtilities.cs
+++ src/Aquifer.Common/Utilities/BibleTextUtilities.cs
@@ -12,9 +12,11 @@ public static class BibleTextUtilities
         var verseId = BibleUtilities.GetVerseId(bookNumber, chapter, verse);
 
         var mappedVerseId = await cachingService.GetVersificationMapping(bibleId, verseId, ct);
+        var (mappedBookNumber, mappedChapter, mappedVerse) = BibleUtilities.TranslateVerseId(mappedVerseId);
 
-        return await dbContext.BibleTexts
-            .Where(bt => bt.VerseNumber == mappedVerseId && bt.ChapterNumber == chapter && bt.BibleId == bibleId).Select(b => new Chapter
+        var bibleTexts = await dbContext.BibleTexts
+            .Where(bt => bt.VerseNumber == mappedVerse && bt.ChapterNumber == mappedChapter && bt.BookId == mappedBookNumber &&
+                         bt.BibleId == bibleId).Select(b => new Chapter
             {
                 Number = b.ChapterNumber,
                 Verse = new ChapterVerse
@@ -23,6 +25,7 @@ public static class BibleTextUtilities
                     Text = b.Text
                 }
             }).ToListAsync(ct);
+        return bibleTexts;
     }
 }
 
