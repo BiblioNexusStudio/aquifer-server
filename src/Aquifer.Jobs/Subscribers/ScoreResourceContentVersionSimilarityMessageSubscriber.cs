5c29b6418089fbca8c3f245bc8ffedf091ef30fd
kaseywright <kasey.wright@biblionexus.org>
2024-12-18 17:18:56 -0500

    WIP Add ScoreResourceContentVersionSimilarityMessageSubscriber

---
+++ src/Aquifer.Jobs/Subscribers/ScoreResourceContentVersionSimilarityMessageSubscriber.cs
@@ -0,0 +1,217 @@
+using Aquifer.Common.Messages;
+using Aquifer.Common.Messages.Models;
+using Aquifer.Common.Tiptap;
+using Aquifer.Common.Utilities;
+using Aquifer.Data;
+using Azure.Storage.Queues.Models;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Aquifer.Jobs.Subscribers;
+
+public sealed class ScoreResourceContentVersionSimilarityMessageSubscriber(
+    AquiferDbContext _dbContext,
+    ILogger<ScoreResourceContentVersionSimilarityMessageSubscriber> _logger)
+{
+    [Function(nameof(ScoreResourceContentVersionSimilarity))]
+    public async Task ScoreResourceContentVersionSimilarity(
+        [QueueTrigger(Queues.GenerateResourceContentSimilarityScore)]
+        QueueMessage queueMessage,
+        CancellationToken ct)
+    {
+        await queueMessage.ProcessAsync<ScoreResourceContentVersionSimilarityMessage, ScoreResourceContentVersionSimilarityMessageSubscriber>(
+            _logger,
+            nameof(ScoreResourceContentVersionSimilarity),
+            ProcessAsync,
+            ct);
+    }
+
+    private async Task ProcessAsync(QueueMessage queueMessage, ScoreResourceContentVersionSimilarityMessage message, CancellationToken ct)
+    {
+        _logger.LogInformation("Scoring machine translation...");
+
+        switch (message.ComparisonType)
+        {
+            case ResourceContentVersionSimilarityComparisonTypes.MachineTranslationToResourceContentVersion:
+                await ScoreMachineTranslationToResourceContentVersionSimilarityAsync(
+                    message.BaseVersionId,
+                    message.CompareVersionId,
+                    message.Status,
+                    ct);
+                break;
+            case ResourceContentVersionSimilarityComparisonTypes.MachineTranslationToSnapshot:
+                await ScoreMachineTranslationToSnapshotSimilarityAsync(
+                    message.BaseVersionId,
+                    message.CompareVersionId,
+                    message.Status,
+                    ct);
+                break;
+            case ResourceContentVersionSimilarityComparisonTypes.ResourceContentVersionToSnapshot:
+                await ScoreResourceContentVersionToSnapshotSimilarityAsync(
+                    message.BaseVersionId,
+                    message.CompareVersionId,
+                    message.Status,
+                    ct);
+                break;
+            case ResourceContentVersionSimilarityComparisonTypes.SnapshotToSnapshot:
+                await ScoreSnapshotToSnapshotSimilarityAsync(
+                    message.BaseVersionId,
+                    message.CompareVersionId,
+                    message.Status,
+                    ct);
+                break;
+            default:
+                throw new InvalidOperationException($"Unknown similarity comparison type: {message.ComparisonType}");
+        }
+    }
+
+    private async Task ScoreMachineTranslationToResourceContentVersionSimilarityAsync(
+        int machineId, 
+        int contentVersionId, 
+        ScoreResourceContentVersionSimilarityStatus status,
+        CancellationToken ct)
+    {
+        var machineContentResource = await _dbContext
+            .ResourceContentVersionMachineTranslations
+            .SingleOrDefaultAsync(x => x.Id == machineId, ct) 
+                ?? throw new InvalidOperationException($"Machine translation with id {machineId} not found");
+        
+        var machineText = HtmlUtilities.GetPlainText(machineContentResource.Content);
+        
+        
+        var compResource = await _dbContext
+            .ResourceContentVersions
+            .SingleOrDefaultAsync(x => x.Id == contentVersionId, ct)
+                ?? throw new InvalidOperationException($"Content version with id {contentVersionId} not found");
+        
+        var compHtmlItems = TiptapConverter.ConvertJsonToHtmlItems(compResource.Content);
+        var compText = HtmlUtilities.GetPlainText(
+            string.Join(string.Empty, compHtmlItems)
+        );
+        
+        var (similarity, executionTime) = StringSimilarityUtilities
+            .ComputeLevenshteinSimilarity(
+                machineText,
+                compText
+            );
+        _logger.LogInformation($"Similarity: {similarity}, execution time: {executionTime}, status: {status}");
+        
+        // todo: success, store results, job success
+        // store results on Machintrx table? or make new table with foreign keys and other info we care about
+        // await _dbContext.SaveChangesAsync(ct);
+    }
+
+    private async Task ScoreMachineTranslationToSnapshotSimilarityAsync(
+        int machineId, 
+        int compareSnapshotId, 
+        ScoreResourceContentVersionSimilarityStatus status,
+        CancellationToken ct)
+    {
+        var machineContentResource = await _dbContext
+                                         .ResourceContentVersionMachineTranslations
+                                         .SingleOrDefaultAsync(x => x.Id == machineId, ct) 
+                                     ?? throw new InvalidOperationException($"Machine translation with id {machineId} not found");
+        
+        var machineText = HtmlUtilities.GetPlainText(machineContentResource.Content);
+        
+        
+        var compSnapshotResource = await _dbContext
+                                       .ResourceContentVersionSnapshots
+                                       .SingleOrDefaultAsync(x => x.Id == compareSnapshotId, ct)
+                                   ?? throw new InvalidOperationException($"Snapshot version with id {compareSnapshotId} not found");
+        
+        var compHtmlItems = TiptapConverter.ConvertJsonToHtmlItems(compSnapshotResource.Content);
+        var compText = HtmlUtilities.GetPlainText(
+            string.Join(string.Empty, compHtmlItems)
+        );
+        
+        var (similarity, executionTime) = StringSimilarityUtilities
+            .ComputeLevenshteinSimilarity(
+                machineText,
+                compText
+            );
+        _logger.LogInformation($"Similarity: {similarity}, execution time: {executionTime}, status: {status}");
+        
+        // todo: success, store results, job success
+        // store results on Machintrx table? or make new table with foreign keys and other info we care about
+        // await _dbContext.SaveChangesAsync(ct);
+    }
+    
+    private async Task ScoreResourceContentVersionToSnapshotSimilarityAsync(
+        int baseContentVersionId, 
+        int compareSnapshotId, 
+        ScoreResourceContentVersionSimilarityStatus status,
+        CancellationToken ct)
+    {
+        var baseContentResource = await _dbContext
+                                       .ResourceContentVersions
+                                       .SingleOrDefaultAsync(x => x.Id == baseContentVersionId, ct) 
+                                  ?? throw new InvalidOperationException($"Resource Content Version with id {baseContentVersionId} not found");
+        
+        var baseContentHtmlItems = TiptapConverter.ConvertJsonToHtmlItems(baseContentResource.Content);
+        var baseText = HtmlUtilities.GetPlainText(
+            string.Join(string.Empty, baseContentHtmlItems)    
+        );
+        
+        var compSnapshotResource = await _dbContext
+                                       .ResourceContentVersionSnapshots
+                                       .SingleOrDefaultAsync(x => x.Id == compareSnapshotId, ct)
+                                   ?? throw new InvalidOperationException($"Snapshot version with id {compareSnapshotId} not found");
+        
+        var compHtmlItems = TiptapConverter.ConvertJsonToHtmlItems(compSnapshotResource.Content);
+        var compText = HtmlUtilities.GetPlainText(
+            string.Join(string.Empty, compHtmlItems)
+        );
+        
+        var (similarity, executionTime) = StringSimilarityUtilities
+            .ComputeLevenshteinSimilarity(
+                baseText,
+                compText
+            );
+        _logger.LogInformation($"Similarity: {similarity}, execution time: {executionTime}, status: {status}");
+        
+        // todo: success, store results, job success
+        // store results on Machintrx table? or make new table with foreign keys and other info we care about
+        // await _dbContext.SaveChangesAsync(ct);
+    }
+    
+    private async Task ScoreSnapshotToSnapshotSimilarityAsync(
+        int baseSnapshotId, 
+        int compareSnapshotId, 
+        ScoreResourceContentVersionSimilarityStatus status,
+        CancellationToken ct)
+    {
+        var baseSnapshotResource = await _dbContext
+                                         .ResourceContentVersionSnapshots
+                                         .SingleOrDefaultAsync(x => x.Id == baseSnapshotId, ct) 
+                                     ?? throw new InvalidOperationException($"Snapshot with id {baseSnapshotId} not found");
+        
+        var baseSnapshotHtmlItems = TiptapConverter.ConvertJsonToHtmlItems(baseSnapshotResource.Content);
+        var baseText = HtmlUtilities.GetPlainText(
+            string.Join(string.Empty, baseSnapshotHtmlItems)    
+        );
+        
+        var compSnapshotResource = await _dbContext
+                                       .ResourceContentVersionSnapshots
+                                       .SingleOrDefaultAsync(x => x.Id == compareSnapshotId, ct)
+                                   ?? throw new InvalidOperationException($"Snapshot version with id {compareSnapshotId} not found");
+        
+        var compHtmlItems = TiptapConverter.ConvertJsonToHtmlItems(compSnapshotResource.Content);
+        var compText = HtmlUtilities.GetPlainText(
+            string.Join(string.Empty, compHtmlItems)
+        );
+        
+        var (similarity, executionTime) = StringSimilarityUtilities
+            .ComputeLevenshteinSimilarity(
+                baseText,
+                compText
+            );
+        _logger.LogInformation($"Similarity: {similarity}, execution time: {executionTime}, status: {status}");
+        
+        // todo: success, store results, job success
+        // store results on Machintrx table? or make new table with foreign keys and other info we care about
+        // await _dbContext.SaveChangesAsync(ct);
+    }
+}
+
