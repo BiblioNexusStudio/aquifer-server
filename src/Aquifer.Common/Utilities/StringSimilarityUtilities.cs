25a9a743cd63689f30ba6f9e0438c8fd23371769
kaseywright <kasey.wright@biblionexus.org>
2024-12-18 17:06:08 -0500

    Add StringSimilarityUtilities and Unit tests

---
+++ src/Aquifer.Common/Utilities/StringSimilarityUtilities.cs
@@ -0,0 +1,117 @@
+using System.Diagnostics;
+
+namespace Aquifer.Common.Utilities;
+
+public static class StringSimilarityUtilities
+{
+    public static (double similarity, double executionTime) ComputeLevenshteinSimilarity(string textA, string textB, int limit = 15000)
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        var textASubstrings = GetSubstrings(textA, limit);
+        var textBSubstrings = GetSubstrings(textB, limit);
+        var distance = ComputeTotalStringDistance(textASubstrings, textBSubstrings);
+
+        var similarity = 1 - ((double)distance / Math.Max(textA.Length, textB.Length));
+
+        stopwatch.Stop();
+        var executionTime = stopwatch.Elapsed.TotalMilliseconds;
+        
+        return (similarity, executionTime);
+    }
+    
+    public static int LevenshteinDistance(string textA, string textB)
+    {
+        var aLength = textA.Length;
+        var bLength = textB.Length;
+        var matrix = new int[bLength + 1];
+
+        for (var j = 0; j <= bLength; j++)
+        {
+            matrix[j] = j;
+        }
+
+        for (var i = 1; i <= aLength; i++)
+        {
+            var prev = matrix[0];
+            matrix[0] = i;
+            for (var j = 1; j <= bLength; j++)
+            {
+                var cost = (textA[i - 1] == textB[j - 1]) ? 0 : 1;
+                var temp = matrix[j];
+                matrix[j] = Math.Min(
+                    Math.Min(
+                        prev + cost, 
+                        matrix[j] + 1
+                    ), 
+                    matrix[j - 1] + 1
+                );
+                prev = temp;
+            }
+        }
+
+        return matrix[bLength];
+    }
+    
+    public static List<string> GetSubstrings(string text, int limit)
+    {
+        var result = new List<string>();
+        
+        while (text.Length > limit)
+        {
+            string? substring;
+            
+            if (text.LastIndexOf(' ', limit) == -1)
+            { 
+                substring = text[..limit];
+                text = text[limit..];
+            }
+            else
+            {
+                substring = text[..text.LastIndexOf(' ', limit)];
+                text = text[(text.LastIndexOf(' ', limit) + 1)..];
+            }
+            
+            result.Add(substring);
+        }
+        result.Add(text);
+        
+        return result;
+    }
+
+    public static int ComputeTotalStringDistance(List<string> listA, List<string> listB)
+    {
+        var totalDistance = 0;
+        List<string> longestList;
+        List<string> shortestList;
+        
+        if (listA.Count > listB.Count)
+        {
+            longestList = listA;
+            shortestList = listB;
+        }
+        else
+        {
+            longestList = listB;
+            shortestList = listA;
+        }
+        
+        // We have to account for lists that are not the same length.
+        // Levenshtein distance is cumulative, so the distance of a string
+        // compared to an empty string is the character count of that non-empty string 
+        // i.e. "123" and "" is a distance of 3
+        for (var i = 0; i < longestList.Count; i++)
+        {
+            if (i >= shortestList.Count)
+            {
+                totalDistance += LevenshteinDistance(longestList[i], string.Empty);
+            }
+            else
+            {
+                totalDistance += LevenshteinDistance(longestList[i], shortestList[i]);
+            }
+        }
+        
+        return totalDistance;
+    }
+}
---
+++ tests/Aquifer.Common.UnitTests/StringSimilarityUtilitiesTests.cs
@@ -0,0 +1,157 @@
+using Aquifer.Common.Utilities;
+
+namespace Aquifer.Common.UnitTests;
+
+public sealed class StringSimilarityUtilitiesTests
+{
+    [Theory]
+    [ClassData(typeof(StringDistanceTestData))]
+    public void GivenTwoStringsGetLevenshteinDistance_ShouldReturnCorrectDistance( StringPair strings, int expectedDistance)
+    {
+        var distance = StringSimilarityUtilities.LevenshteinDistance(strings.First, strings.Second);
+        
+        Assert.Equal(expectedDistance, distance);
+    }
+
+    [Theory]
+    [ClassData(typeof(SubStringSplitTestData))]
+    public void LongStringsShouldBeSplitOnLimit_ShouldReturnCorrectSubstrings(SubStringLimit strings, List<string> expectedSubstrings)
+    {
+        var subStrings = StringSimilarityUtilities.GetSubstrings(strings.First, strings.Limit);
+        
+        Assert.Equal(expectedSubstrings, subStrings);
+    }
+    
+    [Theory]
+    [ClassData(typeof(TotalStingDistanceTestData))]
+    public void SplitStringDistanceShouldEqualStringDistance_ShouldReturnCorrectDistance(
+        List<string> substringsListA, 
+        List<string> substringsListB,
+        int expectedDistance)
+    {
+        var totalDistance = StringSimilarityUtilities.ComputeTotalStringDistance(substringsListA, substringsListB);
+        
+        Assert.Equal(expectedDistance, totalDistance);
+    }
+
+    [Theory]
+    [ClassData(typeof(StringSimilarityTestData))]
+    public void StringSimilarity_ShouldReturnCorrectSimilarityAndExecutionTimeMs(
+        StringPair strings, IComparable<double> expectedSimilarity)
+    {
+        var (similarity, _) = StringSimilarityUtilities.ComputeLevenshteinSimilarity(
+            strings.First, 
+            strings.Second, 
+            strings.Limit);
+        
+        Assert.Equal(expectedSimilarity, similarity);
+    }
+}
+
+public class StringPair
+{
+    public required string First { get; set; }
+    public required string Second { get; set; }
+    public int Limit { get; set; }
+}
+
+public class StringDistanceTestData : TheoryData<StringPair, int>
+{
+    public StringDistanceTestData()
+    {
+        Add( new StringPair { First = "abc", Second = "abc" }, 0);
+        Add( new StringPair { First = "abc", Second = "abd" }, 1);
+        Add( new StringPair { First = "This big car", Second = "this big cat" }, 2);
+        Add( new StringPair { First = "This is longer than two", Second = "This is longer t" }, 7);
+    }
+}
+
+public class SubStringLimit
+{
+    public required string First { get; set; }
+    public required int Limit { get; set; }
+}
+
+public class SubStringSplitTestData : TheoryData<SubStringLimit, List<string>>
+{
+    public SubStringSplitTestData()
+    {
+        Add( 
+            new SubStringLimit { First = "This is longer than two words.", Limit = 50 },
+            ["This is longer than two words."]
+        );
+        Add( 
+            new SubStringLimit { First = "This is longer than two words.", Limit = 20 },
+            ["This is longer than", "two words."]
+        );
+        Add( 
+            new SubStringLimit { First = "This is longer than two words.", Limit = 17 },
+            ["This is longer", "than two words."]
+        );
+        Add( 
+            new SubStringLimit { First = "This is longer than two words.", Limit = 5 },
+            ["This", "is", "longe", "r", "than", "two", "words", "."]
+        );
+    }
+}
+
+public class TotalStingDistanceTestData : TheoryData<List<string>, List<string>, int>
+{
+    public TotalStingDistanceTestData()
+    {
+        Add( 
+            ["This is longer than two words."],
+            ["This is longer than two words."],
+            0
+        );
+        Add( 
+            ["This is longer", "than two words."],
+            ["This is longer", "than two words."],
+            0
+        );
+        Add( 
+            ["This is", "longer than two words."],
+            ["This is", "longer than two words"],
+            1
+        );
+        Add( 
+            ["This is", "longer than", "two words."],
+            ["This is", "longer than", "two"],
+            7
+        );
+    }
+}
+
+public class StringSimilarityTestData : TheoryData<StringPair, IComparable<double>>
+{
+    public StringSimilarityTestData()
+    {
+        Add(
+            new StringPair
+            {
+                First = "This is longer than two words.",
+                Second = "This is longer than two words.",
+                Limit = 17
+            },
+            1.0
+        );
+        Add(
+            new StringPair
+            {
+                First = "This is longer than two words.",
+                Second = "This is longer than two words.",
+                Limit = 5
+            },
+            1.0
+        );
+        Add(
+            new StringPair
+            {
+                First = "This is longer than two words.",
+                Second = "This is longer .",
+                Limit = 5
+            },
+            0.5
+        );
+    }
+}
