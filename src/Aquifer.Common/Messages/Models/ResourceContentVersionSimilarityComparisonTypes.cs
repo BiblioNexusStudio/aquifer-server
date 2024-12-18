1c1493b7c7481bab745dcea56e67e47caa376334
kaseywright <kasey.wright@biblionexus.org>
2024-12-18 17:41:32 -0500

    Drop custom statuses for ResourceContentStatus

--- src/Aquifer.Common/Messages/Models/ResourceContentVersionSimilarityComparisonTypes.cs
+++ src/Aquifer.Common/Messages/Models/ResourceContentVersionSimilarityComparisonTypes.cs
@@ -1,14 +1,5 @@
 namespace Aquifer.Common.Messages.Models;
 
-public enum ScoreResourceContentVersionSimilarityStatus
-{
-    None = 0,
-    Publish = 1,
-    EditorReview = 2,
-    PublisherReview = 3,
-    CompanyReview = 4, //?
-}
-
 public enum ResourceContentVersionSimilarityComparisonTypes
 {
     MachineTranslationToResourceContentVersion,
