2090108ab104f739f944df975109f4631ab9730b
kaseywright <kasey.wright@biblionexus.org>
2025-01-06 13:05:34 -0500

    Refactor to drop status check and clean up the user history check

--- src/Aquifer.API/Endpoints/Resources/Content/MachineTranslation/Update/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/MachineTranslation/Update/Endpoint.cs
@@ -1,6 +1,5 @@
 ﻿using Aquifer.API.Services;
 using Aquifer.Data;
-using Aquifer.Data.Entities;
 using FastEndpoints;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,47 +15,41 @@ public class Endpoint(AquiferDbContext dbContext, IUserService userService) : En
     public override async Task HandleAsync(Request req, CancellationToken ct)
     {
         var user = await userService.GetUserFromJwtAsync(ct);
-        var existingRcv = await dbContext
-            .ResourceContentVersionMachineTranslations
-            .AsTracking()
-            .Join(
-                dbContext.ResourceContentVersionAssignedUserHistory,
-                mt => mt.ResourceContentVersionId,
-                rah => rah.ResourceContentVersionId,
-                (mt, rah) => new{ mt, rah })
+        var existingMt = await dbContext.ResourceContentVersionMachineTranslations.AsTracking()
             .FirstOrDefaultAsync(x =>
-                    x.mt.Id == req.Id &&
-                    (
-                        (x.mt.ResourceContentVersion.AssignedUserId == user.Id && 
-                         x.mt.ResourceContentVersion.ResourceContent.Status == ResourceContentStatus.TranslationEditorReview)
-                     || x.rah.AssignedUserId == user.Id),
+                    x.Id == req.Id &&
+                    x.ResourceContentVersion
+                        .ResourceContentVersionAssignedUserHistories
+                        .Any(h => h.AssignedUserId == user.Id),
                 ct);
 
-        if (existingRcv!.mt is null)
+         
+        
+        if (existingMt is null)
         {
             ThrowError(x => x.Id, "No machine translation exists for user");
         }
 
-        existingRcv.mt.UserId = user.Id;
+        existingMt.UserId = user.Id;
 
         if (req.UserRating.HasValue)
         {
-            existingRcv.mt.UserRating = req.UserRating.Value;
+            existingMt.UserRating = req.UserRating.Value;
         }
 
         if (req.ImproveClarity.HasValue)
         {
-            existingRcv.mt.ImproveClarity = req.ImproveClarity.Value;
+            existingMt.ImproveClarity = req.ImproveClarity.Value;
         }
 
         if (req.ImproveConsistency.HasValue)
         {
-            existingRcv.mt.ImproveConsistency = req.ImproveConsistency.Value;
+            existingMt.ImproveConsistency = req.ImproveConsistency.Value;
         }
 
         if (req.ImproveTone.HasValue)
         {
-            existingRcv.mt.ImproveTone = req.ImproveTone.Value;
+            existingMt.ImproveTone = req.ImproveTone.Value;
         }
 
         await dbContext.SaveChangesAsync(ct);
