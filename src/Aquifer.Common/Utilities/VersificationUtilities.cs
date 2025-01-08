ad7ed4dd62d743f12a44738561fd502dd2135e99
Elisabeth <shakespearee04@gmail.com>
2025-01-08 10:18:25 -0500

    change from to source

--- src/Aquifer.Common/Utilities/VersificationUtilities.cs
+++ src/Aquifer.Common/Utilities/VersificationUtilities.cs
@@ -4,13 +4,13 @@ namespace Aquifer.Common.Utilities;
 
 public static class VersificationUtilities
 {
-    public static async Task<int> GetVersificationAsync(int verseId, int fromBibleId, int targetBibleId,
+    public static async Task<int> GetVersificationAsync(int sourceBibleVerseId, int sourceFromBibleId, int targetBibleId,
         ICachingVersificationService versificationService, CancellationToken ct)
     {
-        var baseVerseIdByFromBibleVerseIdMapping = await versificationService.GetBaseVerseIdByBibleVerseIdMapAsync(fromBibleId, ct);
+        var baseVerseIdByFromBibleVerseIdMapping = await versificationService.GetBaseVerseIdByBibleVerseIdMapAsync(sourceFromBibleId, ct);
         var targetBibleVerseIdByBaseVerseIdMapping = await versificationService.GetBibleVerseIdByBaseVerseIdMapAsync(targetBibleId, ct);
 
-        var baseVerseId = baseVerseIdByFromBibleVerseIdMapping.GetValueOrDefault(verseId, verseId);
+        var baseVerseId = baseVerseIdByFromBibleVerseIdMapping.GetValueOrDefault(sourceBibleVerseId, sourceBibleVerseId);
 
         return targetBibleVerseIdByBaseVerseIdMapping.GetValueOrDefault(baseVerseId, baseVerseId);
     }
