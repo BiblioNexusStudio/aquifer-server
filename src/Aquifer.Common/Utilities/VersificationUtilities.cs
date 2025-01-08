b4fea4f17fe9b70f4cde04c428f619952c2d8ea6
Elisabeth <shakespearee04@gmail.com>
2025-01-08 10:17:23 -0500

    var rename fix

--- src/Aquifer.Common/Utilities/VersificationUtilities.cs
+++ src/Aquifer.Common/Utilities/VersificationUtilities.cs
@@ -7,10 +7,10 @@ public static class VersificationUtilities
     public static async Task<int> GetVersificationAsync(int verseId, int fromBibleId, int targetBibleId,
         ICachingVersificationService versificationService, CancellationToken ct)
     {
-        var baseVerseIdFromBibleVerseIdMapping = await versificationService.GetBaseVerseIdByBibleVerseIdMapAsync(fromBibleId, ct);
+        var baseVerseIdByFromBibleVerseIdMapping = await versificationService.GetBaseVerseIdByBibleVerseIdMapAsync(fromBibleId, ct);
         var targetBibleVerseIdByBaseVerseIdMapping = await versificationService.GetBibleVerseIdByBaseVerseIdMapAsync(targetBibleId, ct);
 
-        var baseVerseId = baseVerseIdFromBibleVerseIdMapping.GetValueOrDefault(verseId, verseId);
+        var baseVerseId = baseVerseIdByFromBibleVerseIdMapping.GetValueOrDefault(verseId, verseId);
 
         return targetBibleVerseIdByBaseVerseIdMapping.GetValueOrDefault(baseVerseId, baseVerseId);
     }
