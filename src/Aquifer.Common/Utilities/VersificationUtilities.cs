888522b6c4a7686c2a7db7382b8f3b7ccc14b24f
Elisabeth <shakespearee04@gmail.com>
2025-01-08 10:20:11 -0500

    add comment, fix var names

--- src/Aquifer.Common/Utilities/VersificationUtilities.cs
+++ src/Aquifer.Common/Utilities/VersificationUtilities.cs
@@ -4,13 +4,15 @@ namespace Aquifer.Common.Utilities;
 
 public static class VersificationUtilities
 {
-    public static async Task<int> GetVersificationAsync(int sourceBibleVerseId, int sourceFromBibleId, int targetBibleId,
+    public static async Task<int> GetVersificationAsync(int sourceBibleVerseId, int sourceBibleId, int targetBibleId,
         ICachingVersificationService versificationService, CancellationToken ct)
     {
-        var baseVerseIdByFromBibleVerseIdMapping = await versificationService.GetBaseVerseIdByBibleVerseIdMapAsync(sourceFromBibleId, ct);
+        var baseVerseIdBySourceBibleVerseIdMapping = await versificationService.GetBaseVerseIdByBibleVerseIdMapAsync(sourceBibleId, ct);
         var targetBibleVerseIdByBaseVerseIdMapping = await versificationService.GetBibleVerseIdByBaseVerseIdMapAsync(targetBibleId, ct);
 
-        var baseVerseId = baseVerseIdByFromBibleVerseIdMapping.GetValueOrDefault(sourceBibleVerseId, sourceBibleVerseId);
+        // The dictionary only contains mappings where the key is different from the value.
+        // If the key verse ID is not present in the mapping (and is not in the exclusions list) then the value matches the key.
+        var baseVerseId = baseVerseIdBySourceBibleVerseIdMapping.GetValueOrDefault(sourceBibleVerseId, sourceBibleVerseId);
 
         return targetBibleVerseIdByBaseVerseIdMapping.GetValueOrDefault(baseVerseId, baseVerseId);
     }
