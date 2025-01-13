a992be932aea8dd3df156ef45a7f24e584c85f39
Elisabeth <shakespearee04@gmail.com>
2025-01-13 12:43:46 -0500

    Reviewer Id must be null when the skip editing flag is true

--- src/Aquifer.API/Endpoints/Resources/Content/SendForEditorReview/Validator.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/SendForEditorReview/Validator.cs
@@ -8,5 +8,6 @@ public class Validator : Validator<Request>
     public Validator()
     {
         RuleFor(x => x.ContentIds).NotNull().When(x => x.ContentId is null);
+        RuleFor(x => x.AssignedReviewerUserId).Null().When(x => x.SkipEditorStep);
     }
 }
