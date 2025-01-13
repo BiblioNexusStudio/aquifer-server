bc23a79ecab92ab770da7076b9e52b9e62251aaa
Nate Merritt <nate.merritt@biblionexus.org>
2025-01-13 16:30:17 -0500

    Rename BookChapters.VerseCount to MaxVerseNumber.

--- src/Aquifer.Data/Entities/BookChapterEntity.cs
+++ src/Aquifer.Data/Entities/BookChapterEntity.cs
@@ -7,7 +7,7 @@ public class BookChapterEntity
     public int Id { get; set; }
     public BookId BookId { get; set; }
     public int Number { get; set; }
-    public int VerseCount { get; set; }
+    public int MaxVerseNumber { get; set; }
 
     public BookEntity Book { get; set; } = null!;
 }
