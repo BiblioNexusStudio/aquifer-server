c6cfda3d283590b1446aa7751079cc1725d97490
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2025-01-14 12:30:35 -0500

    Update translation pair validation (#628)

--- src/Aquifer.API/Endpoints/TranslationPairs/Post/Validator.cs
+++ src/Aquifer.API/Endpoints/TranslationPairs/Post/Validator.cs
@@ -9,6 +9,6 @@ public class Validator : Validator<Request>
     {
         RuleFor(x => x.LanguageId).GreaterThan(0);
         RuleFor(x => x.Key).MinimumLength(3).NotEmpty();
-        RuleFor(x => x.Value).MinimumLength(3).NotEmpty();
+        RuleFor(x => x.Value).MinimumLength(2).NotEmpty();
     }
 }
