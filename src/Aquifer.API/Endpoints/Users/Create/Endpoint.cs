208f349f011e4dc2c3a73c1ff16ef739060b0902
Nate Merritt <nate.merritt@biblionexus.org>
2025-02-07 14:23:38 -0500

    Clean up the create user endpoint.

--- src/Aquifer.API/Endpoints/Users/Create/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Users/Create/Endpoint.cs
@@ -24,21 +24,10 @@ public class Endpoint(AquiferDbContext dbContext, IAuth0Service authService, ILo
 
     private async Task CreateUserAsync(Request req, CancellationToken ct)
     {
-        await ValidateCompanyIdAsync(req.CompanyId, ct);
-        var accessToken = await authService.GetAccessTokenAsync(ct);
-        var desiredUserAuth0RoleId = await GetRoleIdAsync(req, accessToken, ct);
-        var newAuth0UserId = await CreateAuth0UserAsync(req, accessToken, ct);
-        await AssignAuth0RoleAsync(accessToken, newAuth0UserId, desiredUserAuth0RoleId, ct);
-        await SendPasswordResetAsync(accessToken, req.Email, ct);
-
-        await SaveUserToDatabaseAsync(req, newAuth0UserId, ct);
-    }
-
-    private async Task ValidateCompanyIdAsync(int companyId, CancellationToken ct)
-    {
+        // validate company (must match the current user if not admin)
         var newUserCompany = await dbContext.Companies
             .AsTracking()
-            .SingleOrDefaultAsync(x => x.Id == companyId, ct);
+            .SingleOrDefaultAsync(x => x.Id == req.CompanyId, ct);
         if (newUserCompany is null)
         {
             ThrowError(x => x.CompanyId, "Invalid company id");
@@ -49,38 +38,29 @@ public class Endpoint(AquiferDbContext dbContext, IAuth0Service authService, ILo
         {
             ThrowError(x => x.CompanyId, "Not authorized to create user outside of company", StatusCodes.Status403Forbidden);
         }
-    }
 
-    private async Task<string> GetRoleIdAsync(Request req, string accessToken, CancellationToken ct)
-    {
-        var roleId = await authService.GetRoleIdForRoleNameAsync(accessToken, req.Role.ToString(), ct);
+        var accessToken = await authService.GetAccessTokenAsync(ct);
 
-        if (roleId is null)
+        // confirm the requested role exists
+        var desiredUserAuth0RoleId = await authService.GetRoleIdForRoleNameAsync(accessToken, req.Role.ToString(), ct);
+        if (desiredUserAuth0RoleId is null)
         {
             logger.LogWarning("Requested non-existent role: {requestedRole}.", req.Role);
             ThrowError("Requested role does not exist", 400);
         }
 
-        return roleId;
-    }
+        // create the Auth0 user and assign the Auth0 role
+        var newAuth0UserId = await authService.CreateUserAsync(accessToken, req.Email, $"{req.FirstName} {req.LastName}", ct);
+        await authService.AssignRoleToUserAsync(accessToken, newAuth0UserId, desiredUserAuth0RoleId, CancellationToken.None);
 
-    private async Task<string> CreateAuth0UserAsync(Request req, string accessToken, CancellationToken ct)
-    {
-        return await authService.CreateUserAsync(accessToken, req.Email, $"{req.FirstName} {req.LastName}", ct);
-    }
-
-    private async Task AssignAuth0RoleAsync(string accessToken, string userId, string roleId, CancellationToken ct)
-    {
-        await authService.AssignRoleToUserAsync(accessToken, userId, roleId, ct);
-    }
-
-    private async Task SendPasswordResetAsync(string accessToken, string email, CancellationToken ct)
-    {
         // Auth0 doesn't support creating a user account without a password and having the user
         // create a password as part of the email verification. So we have to create a password
         // as part of creating their account, and then immediately send them the reset email
         // which will act as a creation / set password flow.
-        await authService.ResetPasswordAsync(accessToken, email, ct);
+        await authService.ResetPasswordAsync(accessToken, req.Email, CancellationToken.None);
+
+        // create the DB user (and assign the DB role)
+        await SaveUserToDatabaseAsync(req, newAuth0UserId, CancellationToken.None);
     }
 
     private async Task SaveUserToDatabaseAsync(Request req, string providerUserId, CancellationToken ct)
