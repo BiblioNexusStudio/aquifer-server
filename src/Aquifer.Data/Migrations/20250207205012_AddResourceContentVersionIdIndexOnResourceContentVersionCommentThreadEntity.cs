67b0cf92113516b88b4e1543945cffd178c5401d
Nate Merritt <nate.merritt@biblionexus.org>
2025-02-10 09:49:50 -0500

    Add index on ResourceContentVersionId column to ResourceContentVersionCommentThread table. (#643)
    Suggested by [Azure SQL Performance
    Recommendations](https://portal.azure.com/#@biblionexus.org/resource/subscriptions/dacfa945-984c-4708-9f2d-7d8358f223d5/resourceGroups/BiblioNexus-Prod/providers/Microsoft.Sql/servers/biblionexus-prod/databases/biblionexus-prod-db/recommendations).

---
+++ src/Aquifer.Data/Migrations/20250207205012_AddResourceContentVersionIdIndexOnResourceContentVersionCommentThreadEntity.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResourceContentVersionIdIndexOnResourceContentVersionCommentThreadEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_ResourceContentVersionCommentThreads_ResourceContentVersionId",
+                table: "ResourceContentVersionCommentThreads",
+                column: "ResourceContentVersionId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ResourceContentVersionCommentThreads_ResourceContentVersionId",
+                table: "ResourceContentVersionCommentThreads");
+        }
+    }
+}
