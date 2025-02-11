9f7edf9a2470da3a0b10f3c40f121da9faf1009d
Nate Merritt <nate.merritt@biblionexus.org>
2025-02-11 10:45:28 -0500

    Add validation to ensure that IsRead is passed.

--- src/Aquifer.API/Endpoints/Notifications/Update/Validator.cs
+++ src/Aquifer.API/Endpoints/Notifications/Update/Validator.cs
@@ -8,6 +8,9 @@ public class Validator : Validator<Request>
     public Validator()
     {
         RuleFor(x => x.NotificationKind).IsInEnum();
-        RuleFor(x => x.NotificationEntityId).GreaterThan(0);
+        RuleFor(x => x.NotificationKindId).GreaterThan(0);
+
+        // if we ever add future updatable properties to the request then this validation rule should be removed
+        RuleFor(x => x.IsRead).NotNull();
     }
 }
