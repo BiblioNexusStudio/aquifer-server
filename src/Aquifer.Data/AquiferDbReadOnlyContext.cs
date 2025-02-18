5aafed099eb20d3fbdce833cb86693a6540fb6a5
Nate Merritt <nate.merritt@biblionexus.org>
2025-02-18 17:21:43 -0500

    WIP

---
+++ src/Aquifer.Data/AquiferDbReadOnlyContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.Data
+{
+    public sealed class AquiferDbReadOnlyContext : AquiferDbContext
+    {
+        public AquiferDbReadOnlyContext(DbContextOptions<AquiferDbContext> options) : base(options)
+        {
+        }
+    }
+}
