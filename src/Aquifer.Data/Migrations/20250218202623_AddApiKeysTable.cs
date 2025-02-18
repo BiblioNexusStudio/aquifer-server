d9fea08ff49ba25a247ff6535cfd5f83e8f62fee
Jesse Winston <jesse.winston@biblionexus.org>
2025-02-18 15:28:02 -0500

    ApiKey entity

---
+++ src/Aquifer.Data/Migrations/20250218202623_AddApiKeysTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddApiKeysTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ApiKeys",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ApiKey = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Scope = table.Column<int>(type: "int", nullable: false),
+                    Organization = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    ContactName = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: true),
+                    Phone = table.Column<string>(type: "nvarchar(32)", maxLength: 32, nullable: true),
+                    UseCase = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()"),
+                    Updated = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "getutcdate()")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApiKeys", x => x.Id);
+                    table.CheckConstraint("CK_Must_Provide_Contact_Info", "ContactName IS NOT NULL OR Organization IS NOT NULL");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApiKeys_ApiKey",
+                table: "ApiKeys",
+                column: "ApiKey",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApiKeys");
+        }
+    }
+}
