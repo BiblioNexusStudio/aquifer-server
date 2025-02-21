3f85ed8fc2d64c6c3efd6f53b37c37062a533efe
kaseywright <kasey.wright@biblionexus.org>
2025-02-21 14:48:38 -0500

    Add baseMapping to verses and debug VerseMap

--- src/Aquifer.API/Endpoints/Bibles/Texts/Get/Response.cs
+++ src/Aquifer.API/Endpoints/Bibles/Texts/Get/Response.cs
@@ -1,4 +1,6 @@
-﻿namespace Aquifer.API.Endpoints.Bibles.Texts.Get;
+﻿using System.Collections.ObjectModel;
+
+namespace Aquifer.API.Endpoints.Bibles.Texts.Get;
 
 public class Response
 {
@@ -9,6 +11,7 @@ public class Response
     public required string BookCode { get; set; }
     public required int BookNumber { get; set; }
     public IEnumerable<ResponseChapters> Chapters { get; set; } = null!;
+    public ReadOnlyDictionary<int, int?> VerseMap { get; set; } = null!; // todo remove
 }
 
 public class ResponseChapters
@@ -21,4 +24,5 @@ public class ResponseChapterVerses
 {
     public required int Number { get; set; }
     public required string Text { get; set; }
+    public string BaseMapping { get; set; } = null!;
 }
