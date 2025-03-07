0573485c59e652b2de6ff571bff9a5ac52c6ded5
Nate Merritt <nate.merritt@biblionexus.org>
2025-03-07 11:54:24 -0500

    Add migration.

--- src/Aquifer.Data/Migrations/20250307161744_AddUploadsTable.Designer.cs
+++ src/Aquifer.Data/Migrations/20250307161744_AddUploadsTable.Designer.cs
@@ -4,6 +4,7 @@ using Aquifer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Aquifer.Data.Migrations
 {
     [DbContext(typeof(AquiferDbContext))]
-    partial class AquiferDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250307161744_AddUploadsTable")]
+    partial class AddUploadsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -1327,10 +1330,6 @@ namespace Aquifer.Data.Migrations
 
                     b.Property<bool>("AcceptsParentResource")
                         .HasColumnType("bit");
-
-                    b.Property<string>("AllowedRoles")
-                        .HasMaxLength(32)
-                        .HasColumnType("nvarchar(32)");
 
                     b.Property<DateTime>("Created")
                         .ValueGeneratedOnAdd()
