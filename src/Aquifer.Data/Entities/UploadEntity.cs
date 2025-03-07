f011751886173b135907c8b92b2ad9c891d13003
Nate Merritt <nate.merritt@biblionexus.org>
2025-03-07 11:16:40 -0500

    Add UploadEntity.

---
+++ src/Aquifer.Data/Entities/UploadEntity.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Aquifer.Data.EventHandlers;
+
+namespace Aquifer.Data.Entities;
+
+public class UploadEntity : IHasUpdatedTimestamp
+{
+    public int Id { get; set; }
+
+    public UploadStatus Status { get; set; }
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Created { get; set; } = DateTime.UtcNow;
+
+    [SqlDefaultValue("getutcdate()")]
+    public DateTime Updated { get; set; } = DateTime.UtcNow;
+}
+
+public enum UploadStatus
+{
+    None = 0,
+
+    [Display(Name = "Pending")]
+    Pending = 1,
+
+    [Display(Name = "Started")]
+    Started = 2,
+
+    [Display(Name = "Completed")]
+    Completed = 3,
+}
