48f51792f8ec74564d221ab230dcdadedcf468c4
Jesse Winston <jesse.winston@biblionexus.org>
2025-03-11 10:13:20 -0400

    Add health endpoint to Jobs

---
+++ src/Aquifer.Jobs/Http/HealthTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+
+namespace Aquifer.Jobs.Http;
+
+public static class HealthTrigger
+{
+    [Function("Health")]
+    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    {
+        var response = req.CreateResponse(HttpStatusCode.OK);
+        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+        response.WriteString("Healthy");
+
+        return response;
+    }
+}
