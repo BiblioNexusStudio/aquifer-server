eb9e3d0adb6c27a9c83d215f3c234a2f3fb42143
Jesse Winston <jesse.winston@biblionexus.org>
2025-03-12 12:06:11 -0400

    Check that queues are being drained as part of health check

--- src/Aquifer.Jobs/Http/HealthTrigger.cs
+++ src/Aquifer.Jobs/Http/HealthTrigger.cs
@@ -1,17 +1,39 @@
 ﻿using System.Net;
+using System.Reflection;
+using Aquifer.Common.Messages;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Aquifer.Jobs.Http;
 
-public static class HealthTrigger
+public class HealthTrigger(IQueueClientFactory _queueClientFactory, ILogger<HealthTrigger> _logger)
 {
     [Function("Health")]
-    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req)
+    public async Task<HttpResponseData> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequestData req,
+        CancellationToken ct)
     {
-        var response = req.CreateResponse(HttpStatusCode.OK);
+        var queueNames = typeof(Queues).GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.FlattenHierarchy)
+            .Where(qn => qn is { IsLiteral: true, IsInitOnly: false } && qn.FieldType == typeof(string))
+            .Select(qn => (string)qn.GetRawConstantValue()!)
+            .ToList();
+
+        var isHealthy = true;
+        foreach (var queue in queueNames)
+        {
+            var client = await _queueClientFactory.GetQueueClientAsync(queue, ct);
+            var topMessage = await client.PeekMessageAsync(ct);
+            if (topMessage.Value?.InsertedOn?.UtcDateTime <= DateTime.UtcNow.AddHours(-1))
+            {
+                isHealthy = false;
+                _logger.LogError("Queue {queue} is not being drained.", queue);
+            }
+        }
+
+        var response = req.CreateResponse(isHealthy ? HttpStatusCode.OK : HttpStatusCode.ServiceUnavailable);
         response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        response.WriteString("Healthy");
+        await response.WriteStringAsync(isHealthy ? "Healthy" : "Unhealthy", ct);
 
         return response;
     }
