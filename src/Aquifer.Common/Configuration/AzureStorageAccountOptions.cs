f8a8273a84c7415f5a52dfafbe5c194ba9c28d76
Nate Merritt <nate.merritt@biblionexus.org>
2025-03-13 16:44:20 -0400

    Don't require nullable properties on config.

--- src/Aquifer.Common/Configuration/AzureStorageAccountOptions.cs
+++ src/Aquifer.Common/Configuration/AzureStorageAccountOptions.cs
@@ -2,7 +2,7 @@
 
 public sealed class AzureStorageAccountOptions
 {
-    public required Uri? BlobEndpoint { get; init; }
-    public required Uri? QueueEndpoint { get; init; }
-    public required string? ConnectionStringOverride { get; init; }
+    public Uri? BlobEndpoint { get; init; }
+    public Uri? QueueEndpoint { get; init; }
+    public string? ConnectionStringOverride { get; init; }
 }
