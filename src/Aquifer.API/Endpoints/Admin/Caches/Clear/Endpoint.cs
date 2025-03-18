ec6094c0f658277b651af10eeebc075db1d756a9
Nate Merritt <nate.merritt@biblionexus.org>
2025-03-18 17:12:17 -0400

    Add Admin ApiKeyScope and use it to gate admin routes.

--- src/Aquifer.API/Endpoints/Admin/Caches/Clear/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Admin/Caches/Clear/Endpoint.cs
@@ -1,11 +1,14 @@
 using Aquifer.API.Helpers;
+using Aquifer.Common.Services.Caching;
+using Aquifer.Data.Entities;
 using FastEndpoints;
 using Microsoft.AspNetCore.OutputCaching;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace Aquifer.API.Endpoints.Admin.Caches.Clear;
 
-public class Endpoint(IMemoryCache _memoryCache, IOutputCacheStore _outputCacheStore) : Endpoint<Request>
+public class Endpoint(ICachingApiKeyService _cachingApiKeyService, IMemoryCache _memoryCache, IOutputCacheStore _outputCacheStore)
+    : Endpoint<Request>
 {
     public override void Configure()
     {
@@ -15,6 +18,13 @@ public class Endpoint(IMemoryCache _memoryCache, IOutputCacheStore _outputCacheS
 
     public override async Task HandleAsync(Request request, CancellationToken ct)
     {
+        var apiKey = _cachingApiKeyService.CurrentApiKey;
+        if (!apiKey.HasScope(ApiKeyScope.Admin))
+        {
+            await SendUnauthorizedAsync(ct);
+            return;
+        }
+
         if (request.ShouldClearMemoryCache)
         {
             // The IMemoryCache interface is normally injected into constructors, but it doesn't support clearing the entire cache.
--- src/Aquifer.Public.API/Endpoints/Admin/Caches/Clear/Endpoint.cs
+++ src/Aquifer.Public.API/Endpoints/Admin/Caches/Clear/Endpoint.cs
@@ -1,3 +1,5 @@
+using Aquifer.Common.Services.Caching;
+using Aquifer.Data.Entities;
 using Aquifer.Public.API.Helpers;
 using Aquifer.Public.API.OpenApi;
 using FastEndpoints;
@@ -6,17 +8,24 @@ using Microsoft.Extensions.Caching.Memory;
 
 namespace Aquifer.Public.API.Endpoints.Admin.Caches.Clear;
 
-public class Endpoint(IMemoryCache _memoryCache, IOutputCacheStore _outputCacheStore) : Endpoint<Request>
+public class Endpoint(ICachingApiKeyService _cachingApiKeyService, IMemoryCache _memoryCache, IOutputCacheStore _outputCacheStore)
+    : Endpoint<Request>
 {
     public override void Configure()
     {
         Post("/admin/caches/clear");
         Tags(EndpointHelpers.EndpointTags.ExcludeFromSwaggerDocument);
-        AllowAnonymous();
     }
 
     public override async Task HandleAsync(Request request, CancellationToken ct)
     {
+        var apiKey = _cachingApiKeyService.CurrentApiKey;
+        if (!apiKey.HasScope(ApiKeyScope.Admin))
+        {
+            await SendUnauthorizedAsync(ct);
+            return;
+        }
+
         if (request.ShouldClearMemoryCache)
         {
             // The IMemoryCache interface is normally injected into constructors, but it doesn't support clearing the entire cache.
