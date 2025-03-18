0f1b883cdbf400d8c39416c5ff440fdd5a6d1304
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2025-03-18 09:25:33 -0400

    Add `NotApplicableReason` column to ResourceContents (#706)

---
+++ src/Aquifer.Data/Migrations/20250317143416_AddNaReasonToResourceContent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNaReasonToResourceContent : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NotApplicableReason",
+                table: "ResourceContents",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotApplicableReason",
+                table: "ResourceContents");
+        }
+    }
+}
