d5f62ef3949c2acfa06ac0b86e6b3eaca3c51c30
Jesse Winston <jesse.winston@biblionexus.org>
2025-03-18 10:28:20 -0400

    Require not applicable reason when marking

--- src/Aquifer.API/Endpoints/Resources/Content/NotApplicable/Update/Request.cs
+++ src/Aquifer.API/Endpoints/Resources/Content/NotApplicable/Update/Request.cs
@@ -3,4 +3,5 @@ namespace Aquifer.API.Endpoints.Resources.Content.NotApplicable.Update;
 public class Request
 {
     public int ContentId { get; set; }
+    public string NotApplicableReason { get; set; } = null!;
 }
---
+++ src/Aquifer.API/Endpoints/Resources/Content/NotApplicable/Update/Validator.cs
@@ -0,0 +1,13 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Aquifer.API.Endpoints.Resources.Content.NotApplicable.Update;
+
+public class Validator : Validator<Request>
+{
+    public Validator()
+    {
+        RuleFor(x => x.ContentId).NotEmpty().GreaterThan(0);
+        RuleFor(x => x.NotApplicableReason).NotEmpty();
+    }
+}
