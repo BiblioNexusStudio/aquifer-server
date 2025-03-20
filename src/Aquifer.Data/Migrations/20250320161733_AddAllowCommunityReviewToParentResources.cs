cecc1e80777fdcd3c3c709ad0fe4e6b853cdec3c
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2025-03-20 15:46:46 -0400

    Restrict Community Reviewer resources (#716)

---
+++ src/Aquifer.Data/Migrations/20250320161733_AddAllowCommunityReviewToParentResources.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAllowCommunityReviewToParentResources : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AllowCommunityReview",
+                table: "ParentResources",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AllowCommunityReview",
+                table: "ParentResources");
+        }
+    }
+}
