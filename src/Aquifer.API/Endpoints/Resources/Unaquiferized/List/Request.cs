963cd7b32bed3022f62ba260f37a2d3ba5fba774
Nate Merritt <nate.merritt@biblionexus.org>
2025-04-03 11:40:56 -0400

    Support passing source and target language IDs when getting content for creating a project.

--- src/Aquifer.API/Endpoints/Resources/Unaquiferized/List/Request.cs
+++ src/Aquifer.API/Endpoints/Resources/Unaquiferized/List/Request.cs
@@ -2,6 +2,7 @@ namespace Aquifer.API.Endpoints.Resources.Unaquiferized.List;
 
 public record Request
 {
+    public int LanguageId { get; set; }
     public int ParentResourceId { get; set; }
     public string? BookCode { get; set; }
     public int[]? Chapters { get; set; }
