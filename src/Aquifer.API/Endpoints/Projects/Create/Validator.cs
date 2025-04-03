13496ed0a75f94a4a77fc6017986913222c9002d
Nate Merritt <nate.merritt@biblionexus.org>
2025-04-03 15:57:08 -0400

    Support passing source and target language IDs when creating a new project.

--- src/Aquifer.API/Endpoints/Projects/Create/Validator.cs
+++ src/Aquifer.API/Endpoints/Projects/Create/Validator.cs
@@ -8,7 +8,9 @@ public class Validator : Validator<Request>
     public Validator()
     {
         RuleFor(x => x.Title).NotEmpty();
-        RuleFor(x => x.LanguageId).GreaterThan(0);
+        RuleFor(x => x.SourceLanguageId).GreaterThan(0);
+        RuleFor(x => x.TargetLanguageId).GreaterThan(0).When(x => !x.IsAlreadyTranslated);
+        RuleFor(x => x.TargetLanguageId).Null().When(x => x.IsAlreadyTranslated);
         RuleFor(x => x.ProjectManagerUserId).GreaterThan(0);
         RuleFor(x => x.CompanyId).GreaterThan(0);
         RuleFor(x => x.CompanyLeadUserId).GreaterThan(0);
