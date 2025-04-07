8caa00296413444a13f88833f641d7e5c49b4db4
Elisabeth <shakespearee04@gmail.com>
2025-04-07 13:00:01 -0400

    Add notificationOptions to tests

--- tests/Aquifer.API.IntegrationTests/ConfigurationOptions.cs
+++ tests/Aquifer.API.IntegrationTests/ConfigurationOptions.cs
@@ -8,6 +8,7 @@ public sealed class ConfigurationOptions
     public required Auth0Settings IntegrationTestAuth0Settings { get; init; }
     public required UserSettings IntegrationTestUserSettings { get; init; }
     public required string KeyVaultUri { get; init; }
+    public required NotificationOptions IntegrationNotificationOptions { get; init; }
     public required string InternalApiKey { get; init; }
 
     public sealed class ConnectionStringOptions
@@ -18,5 +19,10 @@ public sealed class ConfigurationOptions
     public sealed class UserSettings
     {
         public required string TestUserPassword { get; init; }
+    }
+
+    public class NotificationOptions
+    {
+        public string? NotifyIdsOnCommunityReviewerComment { get; init; }
     }
 }
