33537df99764be06ee1b62ee32e2d95bdc9f6783
Nate Merritt <nate.merritt@biblionexus.org>
2025-04-08 09:43:01 -0400

    Use a transaction when creating/updating notification DB records. (#744)

--- src/Aquifer.API/Endpoints/Notifications/Update/Endpoint.cs
+++ src/Aquifer.API/Endpoints/Notifications/Update/Endpoint.cs
@@ -1,4 +1,5 @@
-﻿using Aquifer.API.Services;
+﻿using System.Data;
+using Aquifer.API.Services;
 using Aquifer.Data;
 using Aquifer.Data.Entities;
 using FastEndpoints;
@@ -17,69 +18,76 @@ public class Endpoint(AquiferDbContext _dbContext, IUserService _userService) : 
     {
         var currentUserId = (await _userService.GetUserFromJwtAsync(ct)).Id;
 
-        if (req.NotificationKind == NotificationKind.Comment)
+        await _dbContext.Database.CreateExecutionStrategy().ExecuteAsync(async () =>
         {
-            var comment = await _dbContext.Comments
-                .Where(c => c.Id == req.NotificationKindId)
-                .FirstOrDefaultAsync(ct);
+            await using var transaction = await _dbContext.Database.BeginTransactionAsync(IsolationLevel.Serializable, ct);
 
-            if (comment is null)
+            if (req.NotificationKind == NotificationKind.Comment)
             {
-                await SendNotFoundAsync(ct);
-                return;
+                var comment = await _dbContext.Comments
+                    .Where(c => c.Id == req.NotificationKindId)
+                    .FirstOrDefaultAsync(ct);
+
+                if (comment is null)
+                {
+                    await SendNotFoundAsync(ct);
+                    return;
+                }
             }
-        }
-        else if (req.NotificationKind == NotificationKind.HelpDocument)
-        {
-            var helpDocument = await _dbContext.HelpDocuments
-                .Where(d => d.Id == req.NotificationKindId)
-                .FirstOrDefaultAsync(ct);
-
-            if (helpDocument is null)
+            else if (req.NotificationKind == NotificationKind.HelpDocument)
             {
-                await SendNotFoundAsync(ct);
-                return;
-            }
-        }
-        else
-        {
-            ThrowError(
-                x => x.NotificationKind,
-                $"Invalid {nameof(req.NotificationKind)}: \"{req.NotificationKind}\".",
-                StatusCodes.Status400BadRequest);
-        }
+                var helpDocument = await _dbContext.HelpDocuments
+                    .Where(d => d.Id == req.NotificationKindId)
+                    .FirstOrDefaultAsync(ct);
 
-        if (req.IsRead.HasValue)
-        {
-            // Note that only notifications that a user has interacted with are stored in the DB.
-            var existingNotification = await _dbContext.Notifications
-                .AsTracking()
-                .Where(n =>
-                    n.UserId == currentUserId &&
-                    n.Kind == req.NotificationKind &&
-                    n.NotificationKindId == req.NotificationKindId)
-                .SingleOrDefaultAsync(ct);
-
-            if (existingNotification is not null)
-            {
-                existingNotification.IsRead = req.IsRead.Value;
+                if (helpDocument is null)
+                {
+                    await SendNotFoundAsync(ct);
+                    return;
+                }
             }
             else
             {
-                var newNotification = new NotificationEntity
-                {
-                    UserId = currentUserId,
-                    Kind = req.NotificationKind,
-                    NotificationKindId = req.NotificationKindId,
-                    Created = DateTime.UtcNow,
-                    IsRead = req.IsRead.Value,
-                };
-
-                _dbContext.Notifications.Add(newNotification);
+                ThrowError(
+                    x => x.NotificationKind,
+                    $"Invalid {nameof(req.NotificationKind)}: \"{req.NotificationKind}\".",
+                    StatusCodes.Status400BadRequest);
             }
 
-            await _dbContext.SaveChangesAsync(ct);
-        }
+            if (req.IsRead.HasValue)
+            {
+                // Note that only notifications that a user has marked as read/unread are stored in the DB.
+                var existingNotification = await _dbContext.Notifications
+                    .AsTracking()
+                    .Where(
+                        n =>
+                            n.UserId == currentUserId &&
+                            n.Kind == req.NotificationKind &&
+                            n.NotificationKindId == req.NotificationKindId)
+                    .SingleOrDefaultAsync(ct);
+
+                if (existingNotification is not null)
+                {
+                    existingNotification.IsRead = req.IsRead.Value;
+                }
+                else
+                {
+                    var newNotification = new NotificationEntity
+                    {
+                        UserId = currentUserId,
+                        Kind = req.NotificationKind,
+                        NotificationKindId = req.NotificationKindId,
+                        Created = DateTime.UtcNow,
+                        IsRead = req.IsRead.Value,
+                    };
+
+                    _dbContext.Notifications.Add(newNotification);
+                }
+
+                await _dbContext.SaveChangesAsync(ct);
+                await transaction.CommitAsync(ct);
+            }
+        });
 
         await SendNoContentAsync(ct);
     }
