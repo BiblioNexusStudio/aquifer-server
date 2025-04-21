754ed5e35856aa9031feb1379f791eea29807c69
Nate Merritt <nate.merritt@biblionexus.org>
2025-04-21 10:11:44 -0400

    Don't allow invalid passage references to be created.

--- src/Aquifer.API/Endpoints/Resources/BibleReferences/Create/Validator.cs
+++ src/Aquifer.API/Endpoints/Resources/BibleReferences/Create/Validator.cs
@@ -10,5 +10,8 @@ public class Validator : Validator<Request>
         RuleFor(x => x.ResourceContentId).GreaterThan(0);
         RuleFor(x => x.StartVerseId).InclusiveBetween(1000000000, 1999999999);
         RuleFor(x => x.EndVerseId).InclusiveBetween(1000000000, 1999999999);
+        RuleFor(x => x.EndVerseId)
+            .GreaterThanOrEqualTo(x => x.StartVerseId)
+            .WithMessage("End verse ID must be greater than or equal to start verse ID.");
     }
 }
