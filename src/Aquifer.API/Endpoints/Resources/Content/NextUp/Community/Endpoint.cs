59ddac883411e77f02da84949d83708b2db76645
Carlos Morales <carlos.rey.morales@gmail.com>
2025-05-08 14:56:45 -0400

    Added Community Reviewer Next Up Endpoint (#755)

---
+++ src/Aquifer.API/Endpoints/Resources/Content/NextUp/Community/Endpoint.cs
@@ -0,0 +1,46 @@
+using Aquifer.API.Common;
+using Aquifer.Common;
+using Aquifer.Data;
+using Aquifer.Data.Entities;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aquifer.API.Endpoints.Resources.Content.NextUp.Community;
+
+public class Endpoint(AquiferDbContext dbContext) : Endpoint<Request, Response>
+{
+    public override void Configure()
+    {
+        Get("/resources/content/{Id}/next-up/community");
+        Permissions(PermissionName.SendReviewCommunityContent);
+    }
+
+    public override async Task HandleAsync(Request request, CancellationToken ct)
+    {
+        var resourceContentFromReq =
+            await dbContext.ResourceContents.Include(x => x.Resource).SingleOrDefaultAsync(x => x.Id == request.Id, ct);
+
+        if (resourceContentFromReq is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        var query = $"""
+                     SELECT TOP 1 RC.Id
+                     FROM Resources R
+                     INNER JOIN ResourceContents RC ON RC.ResourceId = R.Id AND RC.LanguageId = {Constants.EnglishLanguageId} AND RC.Status = {(int)ResourceContentStatus.Complete}
+                     LEFT OUTER JOIN ResourceContents RC2 ON RC2.ResourceId = R.Id AND RC2.LanguageId = {resourceContentFromReq.LanguageId}
+                     WHERE R.ParentResourceId = {resourceContentFromReq.Resource.ParentResourceId}
+                     AND R.SortOrder >= {resourceContentFromReq.Resource.SortOrder}
+                     AND RC2.Id IS NULL
+                     ORDER BY R.SortOrder, R.EnglishLabel
+                     """;
+
+        var result = await dbContext.Database.SqlQueryRaw<int>(query).ToListAsync(ct);
+
+        var response = new Response { NextUpResourceContentId = result.SingleOrDefault() };
+
+        await SendAsync(response, cancellation: ct);
+    }
+}
