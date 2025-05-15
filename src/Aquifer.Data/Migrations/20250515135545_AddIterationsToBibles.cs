9c4ce5d299306d19a5f4ed7e59e03aa8e8373cc3
jwinston <jesse.winston@biblionexus.org>
2025-05-15 10:01:57 -0400

    Add ContentIteration column to Bibles

---
+++ src/Aquifer.Data/Migrations/20250515135545_AddIterationsToBibles.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aquifer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIterationsToBibles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ContentIteration",
+                table: "Bibles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentIteration",
+                table: "Bibles");
+        }
+    }
+}
